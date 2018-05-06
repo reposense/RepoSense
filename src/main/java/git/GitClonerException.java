52d21b94e74e15ed2950861237f6fcc891018974
eugenepeh <e0012835@u.nus.edu>
2018-05-06 18:17:04 +0800

    Checkstyle: fix some violations 5

--- src/main/java/git/GitClonerException.java
+++ src/main/java/git/GitClonerException.java
@@ -1,10 +1,8 @@
 package git;
 
-/**
- * Created by matanghao1 on 13/3/18.
- */
+
 public class GitClonerException extends Exception {
-    public GitClonerException(Exception e){
+    public GitClonerException(Exception e) {
         super(e.getMessage());
     }
 }
