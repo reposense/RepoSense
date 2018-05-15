069dab932abb77e17bbf72b3cd89a31dbc31b538
Yong Hao TENG <yonghao.teng@gmail.com>
2018-05-15 14:26:45 +0800

    Stage missing files

--- src/main/java/reposense/ConfigParser/Argument.java
+++ src/main/java/reposense/ConfigParser/Argument.java
@@ -1,13 +1,52 @@
 package reposense.ConfigParser;
 
+import java.io.File;
 import java.util.Date;
 import java.util.Optional;
 
-public final class Arguments {
+public final class Argument {
     private static final String DEFAULT_FILE_ARG = ".";
 
+    private File configFile;
+    private File targetFile;
     private Optional<Date> sinceDate;
     private Optional<Date> untilDate;
-    private String outputFile = DEFAULT_FILE_ARG;
 
+    public Argument() {
+        sinceDate = Optional.empty();
+        untilDate = Optional.empty();
+        targetFile = new File(DEFAULT_FILE_ARG);
+    }
+
+    public File getConfigFile() {
+        return configFile;
+    }
+
+    public void setConfigFile(String configFile) {
+        this.configFile = new File(configFile);
+    }
+
+    public Optional<Date> getSinceDate() {
+        return sinceDate;
+    }
+
+    public File getTargetFile() {
+        return targetFile;
+    }
+
+    public void setTargetFile(String targetFile) {
+        this.targetFile = new File(targetFile);
+    }
+
+    public void setSinceDate(Date sinceDate) {
+        this.sinceDate = Optional.ofNullable(sinceDate);
+    }
+
+    public Optional<Date> getUntilDate() {
+        return untilDate;
+    }
+
+    public void setUntilDate(Date untilDate) {
+        this.untilDate = Optional.ofNullable(untilDate);
+    }
 }
--- src/main/java/reposense/ConfigParser/ConfigParser.java
+++ src/main/java/reposense/ConfigParser/ConfigParser.java
@@ -1,4 +1,21 @@
 package reposense.ConfigParser;
 
-public class ConfigParser {
+import reposense.dataobject.RepoConfiguration;
+import reposense.exceptions.ParseException;
+import reposense.system.CsvConfigurationBuilder;
+
+import java.util.Date;
+import java.util.List;
+
+public class ConfigParser extends Parser<List<RepoConfiguration>, Argument> {
+
+    @Override
+    public List<RepoConfiguration> parse(Argument argument) throws ParseException {
+        Date sinceDate = argument.getSinceDate().orElse(null);
+        Date untilDate = argument.getUntilDate().orElse(null);
+
+        List<RepoConfiguration> configs = CsvConfigurationBuilder.buildConfigs(argument.getConfigFile(), sinceDate, untilDate);
+
+        return configs;
+    }
 }
