ae11b01a7e63ae52d5c562f97af44760e8a122bd
Metta Ong <ongspxm@gmail.com>
2018-05-15 17:17:56 +0800

    chore: convert into multiple file format

---
+++ frontend/data/script.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+var data = require("./pretty.json");
+var objs = {};
+
+var attrs = "repo organization branch displayName fromDate".split(" ");
+for(var dir in data){
+    var obj = {};
+    var tar = data[dir];
+
+    if(!fs.existsSync(dir)){ fs.mkdirSync(dir); }
+    
+    for(var a in attrs){
+        var attr = attrs[a];
+        obj[attr] = tar[attr];
+        delete tar[attr];
+    }
+
+    fs.writeFileSync(dir+"/summary.json", JSON.stringify(tar));
+    objs[dir] = obj;
+}
+
+fs.writeFileSync("repo.js", JSON.stringify(objs));
