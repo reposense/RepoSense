ea2274aaba214ec0d45f444a4a382a20ce65b8b2
Yong Hao TENG <yonghao.teng@gmail.com>
2018-05-16 13:22:23 +0800

    Add bounds check in elements array

--- src/main/java/reposense/ConfigParser/CsvParser.java
+++ src/main/java/reposense/ConfigParser/CsvParser.java
@@ -10,23 +10,58 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 public class CsvParser extends Parser<List<RepoConfiguration>, InputParameter> {
 
+    private static final String PARSE_EXCEPTION_MESSAGE_MALFORMED_CSV_FILE = "The supplied CSV file is malformed or corrupted.";
+
     private static final int SKIP_FIRST_LINE = 1;
+
+    /** Positions of the elements of a line in the CSV file */
     private static final int ORGANIZATION_POSITION = 0;
     private static final int REPOSITORY_NAME_POSITION = 1;
     private static final int BRANCH_POSITION = 2;
-    private static final String PARSE_EXCEPTION_MESSAGE_MALFORMED_CSV_FILE = "The supplied CSV file is malformed or corrupted.";
+    private static final int GITHUB_ID_POSITION = 3;
+    private static final int DISPLAY_NAME_POSITION = 4;
+    private static final int ALIAS_POSITION = 5;
+
+    public static final String REPO_CONFIG_MAP_KEY_FORMAT = "%s|%s|%s";
+
+    private HashMap<String, RepoConfiguration> repoMap = new HashMap<String, RepoConfiguration>();
+
+    private void createRepoConfigInMapIfNotExists(final String organization, final String repositoryName, final String branch, final Date sinceDate, final Date untilDate) {
+
+        final String key = String.format(REPO_CONFIG_MAP_KEY_FORMAT, organization, repositoryName, branch);
+
+        if (!repoMap.containsKey(key)) {
+            RepoConfiguration config = new RepoConfiguration(organization, repositoryName, branch);
+
+            config.setToDate(sinceDate);
+            config.setFromDate(untilDate);
+
+            repoMap.put(key, config);
+        }
+    }
+
+    private RepoConfiguration getRepoConfigFromMap(final String organization, final String repositoryName, final String branch) {
+        final String key = String.format(REPO_CONFIG_MAP_KEY_FORMAT, organization, repositoryName, branch);
+
+        if (repoMap.containsKey(key)) {
+            return repoMap.get(key);
+        }
+
+        throw new RuntimeException("Illegal Usage: RepositoryConfiguration key does not exists");
+    }
 
     /**
      * Returns a list of RepoConfiguration, which are the inflated object a line of the csv file.
      *
-     * @param parameter Instance of an InputParameter object
-     * @return List of RepoConfiguration
-     * @throws IllegalArgumentException  If argument is null
-     * @throws ParseException If given inputs or file fail to parse
+     * @param parameter Instance of an InputParameter object.
+     * @return List of RepoConfiguration.
+     * @throws IllegalArgumentException  If parameter is null.
+     * @throws ParseException If user-supplied csv file fails to parse.
      */
     @Override
     public List<RepoConfiguration> parse(InputParameter parameter) throws ParseException {
@@ -35,27 +70,45 @@ public class CsvParser extends Parser<List<RepoConfiguration>, InputParameter> {
             throw new IllegalArgumentException("The supplied argument cannot be null");
         }
 
-        List<RepoConfiguration> configs = new ArrayList<>();
-
-        Date sinceDate = parameter.getSinceDate().orElse(null);
-        Date untilDate = parameter.getUntilDate().orElse(null);
-
-        Path path = parameter.getConfigFile().toPath();
+        final Date sinceDate = parameter.getSinceDate().orElse(null);
+        final Date untilDate = parameter.getUntilDate().orElse(null);
+        final Path path = parameter.getConfigFile().toPath();
 
         try {
             Files.lines(path).skip(SKIP_FIRST_LINE).forEach(line -> {
                         if (!line.isEmpty()) {
-                            String[] elements = line.split(Constants.CSV_SPLITTER);
+                            final String[] elements = line.split(Constants.CSV_SPLITTER);
 
-                            String org = elements[ORGANIZATION_POSITION];
-                            String repoName = elements[REPOSITORY_NAME_POSITION];
-                            String branch = elements[BRANCH_POSITION];
+                            final String organization = elements[ORGANIZATION_POSITION];
+                            final String repositoryName = elements[REPOSITORY_NAME_POSITION];
+                            final String branch = elements[BRANCH_POSITION];
 
-                            RepoConfiguration config = new RepoConfiguration(org, repoName, branch);
-                            aggregateAuthorInfo(elements, config);
-                            config.setToDate(sinceDate);
-                            config.setFromDate(untilDate);
-                            configs.add(config);
+                            createRepoConfigInMapIfNotExists(organization, repositoryName, branch, sinceDate, untilDate);
+
+                            RepoConfiguration config = getRepoConfigFromMap(organization, repositoryName, branch);
+
+                            Author author = new Author(elements[GITHUB_ID_POSITION]);
+                            config.getAuthorList().add(author);
+
+                            // Checks length of the elements array as , may not be included in empty columns, which results in a small length elements array
+                            if (elements.length > DISPLAY_NAME_POSITION && !elements[DISPLAY_NAME_POSITION].isEmpty()) {
+                                //Not empty then
+                                config.getAuthorDisplayNameMap().put(author, elements[DISPLAY_NAME_POSITION]);
+                            } else {
+                                //Use GitHub Id as the display name
+                                config.getAuthorDisplayNameMap().put(author, author.getGitId());
+                            }
+
+                            //Use GitHub Id as an alias
+                            config.getAuthorAliasMap().put(elements[GITHUB_ID_POSITION], author);
+
+                            if (elements.length > ALIAS_POSITION &&  !elements[ALIAS_POSITION].isEmpty()) {
+                                final String[] aliases = elements[ALIAS_POSITION].split(Constants.AUTHOR_ALIAS_SPLITTER);
+
+                                for (final String alias : aliases) {
+                                    config.getAuthorAliasMap().put(alias, author);
+                                }
+                            }
                         }
                     }
             );
@@ -63,35 +116,8 @@ public class CsvParser extends Parser<List<RepoConfiguration>, InputParameter> {
             throw new ParseException(PARSE_EXCEPTION_MESSAGE_MALFORMED_CSV_FILE);
         }
 
+        // Converts hashmap to list
+        List<RepoConfiguration> configs = new ArrayList<RepoConfiguration>(repoMap.values());
         return configs;
-    }
-
-    private static void aggregateAuthorInfo(String[] elements, RepoConfiguration config) {
-        for (int i = 3; i < elements.length; i += 3) {
-            Author currentAuthor = new Author(elements[i]);
-            config.getAuthorList().add(currentAuthor);
-            //put the gitID itself as alias
-            config.getAuthorAliasMap().put(elements[i], currentAuthor);
-            //handle student's display name
-            if (i + 1 == elements.length) {
-                // put the gitID itself as display name if display name is not available
-                config.getAuthorDisplayNameMap().put(currentAuthor, currentAuthor.getGitId());
-                break;
-            }
-            if (elements[i + 1].length() == 0) {
-                config.getAuthorDisplayNameMap().put(currentAuthor, currentAuthor.getGitId());
-            } else {
-                config.getAuthorDisplayNameMap().put(currentAuthor, elements[i + 1]);
-            }
-            //handle student's git aliases
-            if (i + 2 == elements.length) {
-                break;
-            }
-            if (elements[i + 2].length() != 0) {
-                for (String alias : elements[i + 2].split(Constants.AUTHOR_ALIAS_SPLITTER)) {
-                    config.getAuthorAliasMap().put(alias, currentAuthor);
-                }
-            }
-        }
     }
 }
