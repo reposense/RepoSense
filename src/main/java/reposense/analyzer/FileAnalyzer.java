4dec6640ef992114dbf32a5e0721a0dcf8db8f88
Eugene Peh <eugenepeh@users.noreply.github.com>
2018-05-16 14:06:02 +0800

    [#72] Remove threading (#104)
    Threading starves our development PC of processing power, and also
    causes the issue of randomized output.
    
    Let's remove threading temporarily for now.

--- src/main/java/reposense/analyzer/FileAnalyzer.java
+++ src/main/java/reposense/analyzer/FileAnalyzer.java
@@ -1,14 +1,18 @@
 package reposense.analyzer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import reposense.dataobject.FileInfo;
 import reposense.dataobject.RepoConfiguration;
+import reposense.git.GitBlamer;
+import reposense.util.Constants;
 
 
 public class FileAnalyzer {
@@ -41,13 +45,23 @@ public class FileAnalyzer {
 
     private static ArrayList<FileInfo> processFiles(RepoConfiguration config, List<String> relativePaths) {
         List<FileInfo> fileInfos = Collections.synchronizedList(new ArrayList<FileInfo>());
-        ExecutorService executor = Executors.newFixedThreadPool(5);
         for (String relativePath : relativePaths) {
-            Runnable worker = new FileAnalyzeThread(fileInfos, config, relativePath);
-            executor.execute(worker);
-        }
-        executor.shutdown();
-        while (!executor.isTerminated()) {
+            if (isReused(config.getRepoRoot(), relativePath)) {
+                continue;
+            }
+            FileInfo fileInfo = FileInfoGenerator.generateFileInfo(config.getRepoRoot(), relativePath);
+            GitBlamer.aggregateBlameInfo(fileInfo, config);
+            if (config.isNeedCheckStyle()) {
+                CheckStyleParser.aggregateStyleIssue(fileInfo, config.getRepoRoot());
+            }
+            if (config.isAnnotationOverwrite()) {
+                AnnotatorAnalyzer.aggregateAnnotationAuthorInfo(fileInfo, config);
+            }
+            if (!config.getAuthorList().isEmpty() && fileInfo.isAllAuthorsIgnored(config.getAuthorList())) {
+                continue;
+            }
+            fileInfo.constructAuthorContributionMap();
+            fileInfos.add(fileInfo);
         }
         return new ArrayList<>(fileInfos);
     }
@@ -62,4 +76,18 @@ public class FileAnalyzer {
         return false;
     }
 
+    private static boolean isReused(String repoRoot, String relativePath) {
+        File file = new File(repoRoot + '/' + relativePath);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String firstLine = br.readLine();
+            if (firstLine == null || firstLine.contains(Constants.REUSED_TAG)) {
+                return true;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
