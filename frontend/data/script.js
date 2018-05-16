c931d8c6298c09f9d854d11444ed231fb5bfbb2c
Metta Ong <ongspxm@gmail.com>
2018-05-16 10:46:12 +0800

    feat: conversion of files into JSON format

---
+++ frontend/data/script.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+var data = require("./raw/summary.json");
+var objs = {};
+
+var attrs = "repo organization branch displayName fromDate".split(" ");
+for(var dir in data){
+    console.log(dir);
+
+    var obj = {};
+    var tar = data[dir];
+
+    var ndir = "new/"+dir;
+
+    if(!fs.existsSync(ndir)){ fs.mkdirSync(ndir); }
+    
+    for(var a in attrs){
+        var attr = attrs[a];
+        obj[attr] = tar[attr];
+        delete tar[attr];
+    }
+
+    fs.writeFileSync(ndir+"/summary.json", JSON.stringify(tar));
+    objs[dir] = obj;
+}
+
+fs.writeFileSync("new/repo.js", JSON.stringify(objs));
