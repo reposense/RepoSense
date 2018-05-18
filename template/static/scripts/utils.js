c88871a0923be2ef7e50e5820d9ab7aac161bb38
Metta Ong <ongspxm@gmail.com>
2018-05-18 10:12:21 +0800

    cleanup for util scripts

--- template/static/scripts/utils.js
+++ template/static/scripts/utils.js
@@ -161,8 +161,6 @@ function isMatch(searchTerm, currentPhrase) {
 }
 
 function getMinDate() {
-    return "01/01/01";
-
     rawDate = summaryJson[Object.keys(summaryJson)[0]]["fromDate"];
     if (rawDate) {
         //the fromDate has been set
@@ -189,8 +187,6 @@ function getMinDate() {
 }
 
 function getMaxDate() {
-    return "01/01/99";
-
     rawDate = summaryJson[Object.keys(summaryJson)[0]]["toDate"];
     if (rawDate) {
         //the fromDate has been set
