ca7579137be53f5463b4ac996eaa7928d3fabf07
eugenepeh <e0012835@u.nus.edu>
2018-05-27 14:30:07 +0800

    Fix datetime inconsistency

--- src/functional/java/reposense/Entry.java
+++ src/functional/java/reposense/Entry.java
@@ -42,9 +42,9 @@ public class Entry {
     private String generateReport() throws URISyntaxException {
         Path configFilePath = Paths.get(getClass().getClassLoader().getResource("sample_full.csv").toURI());
         Calendar c = Calendar.getInstance();
-        c.set(2017, 6, 1);
+        c.set(2017, Calendar.JUNE, 1);
         Date fromDate = c.getTime();
-        c.set(2017, 10, 30);
+        c.set(2017, Calendar.OCTOBER, 30);
         Date toDate = c.getTime();
         List<RepoConfiguration> configs = CsvConfigurationBuilder.buildConfigs(configFilePath, fromDate, toDate);
         return RepoInfoFileGenerator.generateReposReport(configs, FT_TEMP_DIR);
