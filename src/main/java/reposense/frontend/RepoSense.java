4488ec82e8121741060c31a693dd39d8bd1cbe77
Yong Hao TENG <yonghao.teng@gmail.com>
2018-06-09 13:00:30 +0800

    Remove unused imports

--- src/main/java/reposense/frontend/RepoSense.java
+++ src/main/java/reposense/frontend/RepoSense.java
@@ -3,7 +3,6 @@ package reposense.frontend;
 import java.nio.file.Path;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import reposense.dataobject.RepoConfiguration;
