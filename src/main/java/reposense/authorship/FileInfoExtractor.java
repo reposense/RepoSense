2eaccf88f62c45b9dd24ca9742864def4602d962
Jun An <E0002937@u.nus.edu>
2018-07-12 12:52:18 +0800

    Update FileInfoExtractor BufferedReader to accept ISO_8859_1 charset

--- src/main/java/reposense/authorship/FileInfoExtractor.java
+++ src/main/java/reposense/authorship/FileInfoExtractor.java
@@ -2,6 +2,7 @@ package reposense.authorship;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -74,7 +75,7 @@ public class FileInfoExtractor {
     public static FileInfo generateFileInfo(String repoRoot, String relativePath) {
         FileInfo fileInfo = new FileInfo(relativePath);
         Path path = Paths.get(repoRoot, relativePath);
-        try (BufferedReader br = new BufferedReader(Files.newBufferedReader(path))) {
+        try (BufferedReader br = new BufferedReader(Files.newBufferedReader(path, StandardCharsets.ISO_8859_1))) {
             String line;
             int lineNum = 1;
             while ((line = br.readLine()) != null) {
