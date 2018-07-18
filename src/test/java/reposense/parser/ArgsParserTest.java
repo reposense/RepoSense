4c277fd471590b0ab06de2faabc3d6c5232e7ade
Yong Hao TENG <yonghao.teng@gmail.com>
2018-07-18 17:15:03 +0800

    Fix checkstyle

--- src/test/java/reposense/parser/ArgsParserTest.java
+++ src/test/java/reposense/parser/ArgsParserTest.java
@@ -11,7 +11,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
