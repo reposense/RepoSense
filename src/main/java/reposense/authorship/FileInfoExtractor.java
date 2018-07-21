cb261c0e9499511955f42dd635553460cbf2a3be
Jun An <E0002937@u.nus.edu>
2018-07-21 17:04:37 +0800

    Add a check for new empty file in git diff parsing

--- src/main/java/reposense/authorship/FileInfoExtractor.java
+++ src/main/java/reposense/authorship/FileInfoExtractor.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -36,12 +37,16 @@ public class FileInfoExtractor {
     private static final String LINE_INSERTED_SYMBOL = "+";
     private static final String STARTING_LINE_NUMBER_GROUP_NAME = "startingLineNumber";
     private static final String FILE_CHANGED_GROUP_NAME = "filePath";
+    private static final String MATCH_GROUP_FAIL_FORMAT = "Failed to match the %s group for:%s\n";
 
     private static final int LINE_CHANGED_HEADER_INDEX = 0;
 
     private static final Pattern STARTING_LINE_NUMBER_PATTERN = Pattern.compile(
             "-(\\d)+(,)?(\\d)* \\+(?<startingLineNumber>\\d+)(,)?(\\d)* @@");
     private static final Pattern FILE_CHANGED_PATTERN = Pattern.compile("\n(\\+){3} b/(?<filePath>.*)\n");
+
+    private static final Predicate<String> NEW_EMPTY_FILE_PREDICATE = Pattern.compile("^new file mode 100644\n"
+            + "index [a-zA-Z0-9]{7}\\.{2}[a-zA-Z0-9]{7}$").asPredicate();
 
     /**
      * Extracts a list of relevant files given in {@code config}.
@@ -84,8 +89,9 @@ public class FileInfoExtractor {
         String[] fileDiffResultList = fullDiffResult.split(DIFF_FILE_CHUNK_SEPARATOR);
 
         for (String fileDiffResult : fileDiffResultList) {
-            // file deleted or is a binary file, skip it
-            if (fileDiffResult.startsWith(FILE_DELETED_METADATA) || fileDiffResult.contains(BINARY_FILE_SYMBOL)) {
+            // file deleted, is binary file or is new and empty file, skip it
+            if (fileDiffResult.startsWith(FILE_DELETED_METADATA) || fileDiffResult.contains(BINARY_FILE_SYMBOL)
+                    || NEW_EMPTY_FILE_PREDICATE.test(fileDiffResult)) {
                 continue;
             }
 
@@ -202,6 +208,7 @@ public class FileInfoExtractor {
         Matcher filePathMatcher = FILE_CHANGED_PATTERN.matcher(fileDiffResult);
 
         if (!filePathMatcher.find()) {
+            logger.severe(String.format(MATCH_GROUP_FAIL_FORMAT, "file path", fileDiffResult));
             throw new AssertionError("Should not have error matching file path pattern inside file diff result!");
         }
 
@@ -216,6 +223,7 @@ public class FileInfoExtractor {
         Matcher chunkHeaderMatcher = STARTING_LINE_NUMBER_PATTERN.matcher(linesChanged);
 
         if (!chunkHeaderMatcher.find()) {
+            logger.severe(String.format(MATCH_GROUP_FAIL_FORMAT, "line changed", linesChanged));
             throw new AssertionError("Should not have error matching line number pattern inside chunk header!");
         }
 
