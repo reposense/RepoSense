876301ec151eb708bddff2d84288df9537bd6825
Yong Hao TENG <yonghao.teng@gmail.com>
2018-07-21 13:15:44 +0800

    Fix cs

--- src/main/java/reposense/model/RepoConfiguration.java
+++ src/main/java/reposense/model/RepoConfiguration.java
@@ -10,7 +10,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
