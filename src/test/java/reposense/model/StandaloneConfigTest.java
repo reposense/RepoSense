a630495f97df6d896cf495b4b4fd4806295fbc4a
eugenepeh <e0012835@u.nus.edu>
2018-08-05 01:47:14 +0800

    Rename test configs

--- src/test/java/reposense/model/StandaloneConfigTest.java
+++ src/test/java/reposense/model/StandaloneConfigTest.java
@@ -14,13 +14,13 @@ public class StandaloneConfigTest extends GitTestTemplate {
     private static final Path VALID_CONFIG = new File(StandaloneConfigTest.class.getClassLoader()
             .getResource("StandaloneConfigTest/valid_config.json").getFile()).toPath();
     private static final Path INVALID_GITID_CONFIG = new File(StandaloneConfigTest.class.getClassLoader()
-            .getResource("StandaloneConfigTest/invalidGitId_config.json").getFile()).toPath();
+            .getResource("StandaloneConfigTest/lithiumlkid_invalidGitId_config.json").getFile()).toPath();
     private static final Path INVALID_DISPLAYNAME_CONFIG = new File(StandaloneConfigTest.class.getClassLoader()
-            .getResource("StandaloneConfigTest/invalidDisplayName_config.json").getFile()).toPath();
+            .getResource("StandaloneConfigTest/jordancjq_invalidDisplayName_config.json").getFile()).toPath();
     private static final Path INVALID_ALIASES_CONFIG = new File(StandaloneConfigTest.class.getClassLoader()
-            .getResource("StandaloneConfigTest/invalidAuthorAliases_config.json").getFile()).toPath();
+            .getResource("StandaloneConfigTest/codeeong_invalidAuthorAliases_config.json").getFile()).toPath();
     private static final Path INVALID_IGNOREGLOB_CONFIG = new File(StandaloneConfigTest.class.getClassLoader()
-            .getResource("StandaloneConfigTest/invalidIgnoreGlob_config.json").getFile()).toPath();
+            .getResource("StandaloneConfigTest/lithiumlkid_invalidIgnoreGlob_config.json").getFile()).toPath();
 
     @Test
     public void standaloneConfig_validJson_success() throws IOException {
