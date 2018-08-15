623a6a7a386a3e566107a79539f6f63fcf34578a
Metta Ong <ongspxm@gmail.com>
2018-08-16 01:02:03 +0800

    [#289] Code view: tweak code packing algorithm (#292)
    Untouched code segments that are smaller than minimum lines are
    displayed together with the touched code. This distorts the truth as
    these code do not belong to the author and may lead to unnecessary
    disputes.
    
    Let's encapsulate untouched code in the expand box only if >5 lines but
    still differentiate them using color irregardless.

--- frontend/src/static/js/v_authorship.js
+++ frontend/src/static/js/v_authorship.js
@@ -1,25 +1,18 @@
-window.toggleNext = function toggleNext(ele) {
+function toggleNext(ele) {
   // function for toggling unopened code
-  const target = ele.nextSibling;
-  const child = ele.childNodes;
+  const targetClass = 'active';
 
-  let style = 'none';
-  let childBackground = '#bfbdbd';
-  let toolTipMessage = 'Show untouched code';
+  const parent = ele.parentNode;
+  const classes = parent.className.split(' ');
+  const idx = classes.indexOf(targetClass);
 
-  if (target.style.display === 'none') {
-    style = '';
-    childBackground = '#000';
-    toolTipMessage = 'Hide untouched code';
+  if (idx === -1) {
+    classes.push(targetClass);
+  } else {
+    classes.splice(idx, 1);
   }
 
-  target.style.display = style;
-
-  if (target.className === 'code') {
-    child[0].style.background = childBackground;
-    target.style.borderLeft = '4px solid rgba(197, 206, 197, 1)';
-    child[2].innerHTML = toolTipMessage;
-  }
+  parent.className = classes.join(' ');
 };
 
 const repoCache = [];
@@ -71,59 +64,12 @@ window.vAuthorship = {
           lastId += 1;
           lastState = authored;
         }
-        segments[lastId].lines.push(line.content);
+
+        const content = line.content || ' ';
+        segments[lastId].lines.push(content);
       });
 
       return segments;
-    },
-
-    mergeSegments(segments) {
-      let lastAuthored;
-      const mergedSegments = [];
-
-      segments.forEach((segment) => {
-        if (lastAuthored !== segment.authored || mergedSegments.length === 0) {
-          mergedSegments.push(segment);
-          lastAuthored = segment.authored;
-        } else {
-          const last = mergedSegments[mergedSegments.length - 1];
-          last.lines = last.lines.concat(segment.lines);
-        }
-      });
-
-      return mergedSegments;
-    },
-
-    removeSmallUnauthored(segments) {
-      const MIN_LINES = 5;
-      const res = [];
-
-      segments.forEach((segment) => {
-        if (segment.lines.length < MIN_LINES && !segment.authored) {
-          if (res.length === 0) {
-            const { lines } = segments[1];
-            segments[1].lines = segment.lines.concat(lines);
-          } else {
-            const last = res[res.length - 1];
-            last.lines = segment.lines.concat(last.lines);
-          }
-        } else {
-          res.push(segment);
-        }
-      });
-
-      return res;
-    },
-
-    removeEmptySegments(segments) {
-      const res = [];
-      segments.forEach((segment) => {
-        if (segment.lines.join('') !== '') {
-          res.push(segment);
-        }
-      });
-
-      return res;
     },
 
     processFiles(files) {
@@ -137,11 +83,7 @@ window.vAuthorship = {
           out.lineCount = lineCnt;
 
           const segments = this.splitSegments(file.lines);
-          const bigSegments = this.removeSmallUnauthored(segments);
-          const validSegments = this.removeEmptySegments(bigSegments);
-          const mergedSegments = this.mergeSegments(validSegments);
-
-          out.segments = mergedSegments;
+          out.segments = segments;
           res.push(out);
         }
       });
