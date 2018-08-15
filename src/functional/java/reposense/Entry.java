5b9cbcfe9c8f26167811e99b49f5748407818cf6
Yong Hao TENG <yonghao.teng@gmail.com>
2018-08-15 22:36:22 +0800

    Fix checkstyle

--- src/functional/java/reposense/Entry.java
+++ src/functional/java/reposense/Entry.java
@@ -7,7 +7,6 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
