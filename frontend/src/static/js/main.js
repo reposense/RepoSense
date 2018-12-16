1cf19f5a6cd2a558792abe1ad9f0128fcd9fc7d4
WANG CHAO <1229983126@qq.com>
2018-12-16 13:20:25 +0800

    Use anchor point instead to achieve autoscroll

--- frontend/src/static/js/main.js
+++ frontend/src/static/js/main.js
@@ -84,9 +84,6 @@ window.app = new window.Vue({
       this.isTabActive = true;
       this.isTabAuthorship = true;
       this.isCollapsed = false;
-      if (document.getElementById("tabs-wrapper") != null) {
-        document.getElementById("tabs-wrapper").scrollTop = 0;
-      }
     },
 
     /*global expandAll*/
