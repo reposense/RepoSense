94ed446de6ec2630fae7e7fe5e62f855e76c468b
Nadhira Salsabila <nadhira_salsabila@yahoo.co.id>
2018-12-19 11:05:21 +0000

    [#200] Console output: use info instead of infos (#417)

--- src/main/java/reposense/authorship/FileInfoExtractor.java
+++ src/main/java/reposense/authorship/FileInfoExtractor.java
@@ -50,7 +50,7 @@ public class FileInfoExtractor {
      * Extracts a list of relevant files given in {@code config}.
      */
     public static List<FileInfo> extractFileInfos(RepoConfiguration config) {
-        logger.info("Extracting relevant file info " + config.getLocation() + "...");
+        logger.info("Extracting relevant file info from " + config.getLocation() + "...");
 
         List<FileInfo> fileInfos = new ArrayList<>();
 
