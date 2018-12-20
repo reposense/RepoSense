66ef2f46c8024041e36e6e2fb8caa4cec10b12dc
eugenepeh <eugenepeh@users.noreply.github.com>
2018-12-20 20:02:16 +0800

    Add clone in GitCloneTest

--- src/test/java/reposense/git/GitCloneTest.java
+++ src/test/java/reposense/git/GitCloneTest.java
@@ -12,7 +12,9 @@ import reposense.template.GitTestTemplate;
 public class GitCloneTest extends GitTestTemplate {
 
     @Test
-    public void cloneTest_validRepo_success() {
+    public void cloneTest_validRepo_success() throws GitCloneException {
+        // As the clone has been performed in the {@code GitTestTemplate},
+        // this checks whether the clone has been executed successfully by performing a file system check.
         Path dir = Paths.get(config.getRepoRoot());
         Assert.assertTrue(Files.exists(dir));
     }
