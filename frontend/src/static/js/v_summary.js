6b7bb3fdfa1de0440de8dc4d1b4b4c3c43d0ab9c
Apoorva Ullas <apoorva_ullas@yahoo.com.sg>
2019-02-22 10:51:14 +0800

    Frontend updates

--- frontend/src/static/js/v_summary.js
+++ frontend/src/static/js/v_summary.js
@@ -324,7 +324,7 @@ window.vSummary = {
       this.sortFiltered();
     },
     processFileFormats() {
-      const selectedColors = ['#e6194b', '#ffe119', '#4363d8', '#3cb44b', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
+      const selectedColors = ['#ffe119', '#4363d8', '#3cb44b', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
         '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1',
         '#000075', '#808080'];
       const colors = {};
