45b0639b262a0b201e799fdaf1bced2dcf846ed0
WANG CHAO <1229983126@qq.com>
2019-03-22 23:03:30 +0800

    Redo unnecessary command

--- frontend/src/static/js/v_authorship.js
+++ frontend/src/static/js/v_authorship.js
@@ -215,11 +215,9 @@ window.vAuthorship = {
       if (!this.isSelectAllChecked) {
         this.selectedFileFormats = this.fileFormats.slice();
         this.activeFilesCount = this.files.length;
-        this.isSelectAllChecked = true;
       } else {
         this.selectedFileFormats = [];
         this.activeFilesCount = 0;
-        this.isSelectAllChecked = false;
       }
     },
 
