762da02d4f0297b7dd7ab911203b2076f1420eb8
Eugene Peh <eugenepeh@users.noreply.github.com>
2019-03-24 16:32:35 +0800

    Fix ParseException handling in CsvParser

--- src/main/java/reposense/parser/CsvParser.java
+++ src/main/java/reposense/parser/CsvParser.java
@@ -23,6 +23,9 @@ public abstract class CsvParser<T> {
     private static final String MESSAGE_UNABLE_TO_READ_CSV_FILE = "Unable to read the supplied CSV file.";
     private static final String MESSAGE_MALFORMED_LINE_FORMAT = "Warning! line %d in configuration file is malformed.\n"
             + "Contents: %s";
+    private static final String MESSAGE_LINE_PARSE_EXCEPTION_FORMAT
+            = "Warning! Error parsing line %d in configuration file.\n"
+            + "Error: %s";
 
     private Path csvFilePath;
 
@@ -43,27 +46,28 @@ public abstract class CsvParser<T> {
      */
     public List<T> parse() throws IOException {
         List<T> results = new ArrayList<>();
-        int lineNumber = 1;
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath.toFile()))) {
             // Skip first line, which is the header row
             br.readLine();
             String line;
 
-            while ((line = br.readLine()) != null) {
+            for (int lineNumber = 1; (line = br.readLine()) != null; lineNumber++)
+            {
                 String[] elements = line.split(ELEMENT_SEPARATOR);
 
                 if (line.isEmpty() || isLineMalformed(elements, lineNumber, line)) {
                     continue;
                 }
 
-                processLine(results, elements);
-                lineNumber++;
+                try {
+                    processLine(results, elements);
+                } catch (ParseException pe) {
+                    logger.warning(String.format(MESSAGE_LINE_PARSE_EXCEPTION_FORMAT, lineNumber, pe.getMessage()));
+                }
             }
         } catch (IOException ioe) {
             throw new IOException(MESSAGE_UNABLE_TO_READ_CSV_FILE, ioe);
-        } catch (ParseException pe) {
-            logger.log(Level.WARNING, pe.getMessage(), pe);
         } catch (IllegalArgumentException iae) {
             logger.log(Level.WARNING, iae.getMessage(), iae);
         }
