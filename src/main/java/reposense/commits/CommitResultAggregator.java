cf53c8fe5bc6e16aff39d371dedec1d8194f4442
Gilbert Emerson <emer7@users.noreply.github.com>
2019-03-24 21:58:27 +0800

    Fix style

--- src/main/java/reposense/commits/CommitResultAggregator.java
+++ src/main/java/reposense/commits/CommitResultAggregator.java
@@ -60,8 +60,8 @@ public class CommitResultAggregator {
         }
         //get mean
         float total = 0;
-        long totalDays = (lastDate.getTime()
-                - getStartOfDate(startDate).getTime()) / DAYS_IN_MS + 1;
+        long totalDays = (lastDate.getTime() - getStartOfDate(startDate).getTime())
+                / DAYS_IN_MS + 1;
 
         for (AuthorDailyContribution contribution : contributions) {
             total += contribution.getTotalContribution();
