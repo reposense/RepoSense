761687d03646ba6203485735450f1bc528ef1fb5
Chelsey Ong <chelseyong@gmail.com>
2019-04-21 17:00:20 +0700

    Remove extra check

--- frontend/src/static/js/v_summary.js
+++ frontend/src/static/js/v_summary.js
@@ -445,9 +445,6 @@ window.vSummary = {
         }
       } else if (this.filterGroupSelection === 'groupByNone') {
         this.sortWithinGroupSelection = '';
-        if (this.sortingOption.indexOf('Commits') !== -1 || this.sortingOption.indexOf('Variance') !== -1) {
-          this.sortGroupSelection = 'name';
-        }
       }
     },
     getOptionWithOrder() {
