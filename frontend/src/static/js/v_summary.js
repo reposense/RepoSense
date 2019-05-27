c29601b4e97ba0970509c1fd76345455c4d0326b
jylee-git <jylee.w94@gmail.com>
2019-05-27 14:18:46 +0800

    Fix colors assigned only to files touched by first author in users list

--- frontend/src/static/js/v_summary.js
+++ frontend/src/static/js/v_summary.js
@@ -355,12 +355,13 @@ window.vSummary = {
       let i = 0;
 
       this.repos.forEach((repo) => {
-        const user = repo.users[0];
-        Object.keys(user.fileFormatContribution).forEach((fileFormat) => {
-          if (!Object.prototype.hasOwnProperty.call(colors, fileFormat)) {
-            colors[fileFormat] = selectedColors[i];
-            i = (i + 1) % selectedColors.length;
-          }
+        repo.users.forEach((user) => {
+          Object.keys(user.fileFormatContribution).forEach((fileFormat) => {
+            if (!Object.prototype.hasOwnProperty.call(colors, fileFormat)) {
+              colors[fileFormat] = selectedColors[i];
+              i = (i + 1) % selectedColors.length;
+            }
+          });
         });
         this.contributionBarColors = colors;
       });
