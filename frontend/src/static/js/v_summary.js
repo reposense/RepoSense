3bfb38c075334d3859b55250c278fb01a9e38800
jamessspanggg <jamespangmunwai@gmail.com>
2019-05-31 16:59:37 +0800

    Add check for last week endDate

--- frontend/src/static/js/v_summary.js
+++ frontend/src/static/js/v_summary.js
@@ -392,24 +392,24 @@ window.vSummary = {
 
       for (let weekId = 0; weekId < diff / 7; weekId += 1) {
         const startOfWeekMs = sinceMs + (weekId * WEEK_IN_MS);
+        const endOfWeekMs = startOfWeekMs + durationMs - DAY_IN_MS;
 
         const week = {
           insertions: 0,
           deletions: 0,
           date: getDateStr(startOfWeekMs),
-          endDate: getDateStr(startOfWeekMs + durationMs - DAY_IN_MS),
+          endDate: getDateStr(endOfWeekMs <= untilMs ? endOfWeekMs : untilMs),
         };
 
         // commits are not contiguous, meaning there are gaps of days without
         // commits, so we are going to check each commit's date and make sure
         // it is within the duration of a week
         while (commits.length > 0
-        && (new Date(commits[0].date)).getTime() < startOfWeekMs + durationMs) {
+          && (new Date(commits[0].date)).getTime() < startOfWeekMs + durationMs) {
           const commit = commits.shift();
           week.insertions += commit.insertions;
           week.deletions += commit.deletions;
         }
-
         res.push(week);
       }
     },
