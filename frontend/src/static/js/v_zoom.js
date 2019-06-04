5d785bdf93acdb5ab3919998da26ae5f1f575c2d
jamessspanggg <jamespangmunwai@gmail.com>
2019-06-04 16:18:02 +0800

    Modify Commit Panel output when granularity is `week`

--- frontend/src/static/js/v_zoom.js
+++ frontend/src/static/js/v_zoom.js
@@ -1,7 +1,11 @@
 window.vZoom = {
   props: ['info'],
   template: window.$('v_zoom').innerHTML,
-  data: () => ({}),
+  data() {
+    return {
+      filterTimeFrame: 'commit',
+    };
+  },
   methods: {
     openSummary() {
       this.$emit('view-summary', {});
@@ -10,8 +14,19 @@ window.vZoom = {
     getSliceLink(slice) {
       return `${window.getBaseLink(this.info.user.repoId)}/commit/${slice.hash}`;
     },
+
+    getFilterTimeFrame() {
+      window.decodeHash();
+      const hash = window.hashParams;
+      if (hash.timeframe) {
+        this.filterTimeFrame = hash.timeframe;
+      }
+    },
   },
   components: {
     v_ramp: window.vRamp,
+  },
+  created() {
+    this.getFilterTimeFrame();
   },
 };
