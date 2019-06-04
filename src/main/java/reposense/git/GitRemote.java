269d4a320f55adb086d78a735deddfbd6b80a008
jamessspanggg <jamespangmunwai@gmail.com>
2019-06-04 10:36:05 +0800

    Add logger to output message when local repo does not have a remote url

--- src/main/java/reposense/git/GitRemote.java
+++ src/main/java/reposense/git/GitRemote.java
@@ -4,14 +4,20 @@ import static reposense.system.CommandRunner.runCommand;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 
 import reposense.model.RepoConfiguration;
+import reposense.system.LogsManager;
 
 /**
  * Contains git remote related functionalities.
  * Git remote is responsible to manage the set of tracked repositories.
  */
 public class GitRemote {
+
+    private static final Logger logger = LogsManager.getLogger(GitRemote.class);
+    private static final String MESSAGE_REMOTE_DOES_NOT_EXIST =
+            "%s does not have a remote url, will not update the location.";
 
     /**
      * Extracts the remote url from the cloned local repository
@@ -23,6 +29,7 @@ public class GitRemote {
         try {
             return runCommand(rootPath, command).replace("\n", "");
         } catch (RuntimeException re) {
+            logger.info(String.format(MESSAGE_REMOTE_DOES_NOT_EXIST, config.getLocation()));
             return null;
         }
     }
