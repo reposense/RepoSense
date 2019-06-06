1a4ac82869230ae6da2a3b0140187b2bd5986512
jylee-git <jylee.w94@gmail.com>
2019-06-06 11:59:51 +0800

    Update javadocs comment

--- src/main/java/reposense/parser/SinceDateArgumentType.java
+++ src/main/java/reposense/parser/SinceDateArgumentType.java
@@ -21,7 +21,7 @@ public class SinceDateArgumentType extends DateArgumentType {
             1).getTime();
 
     /**
-     * Returns an arbitrary year 2000 if user specifies * in {@code value}, or attempts to return
+     * Returns an arbitrary year 1/1/1 if user specifies * in {@code value}, or attempts to return
      * the desired date otherwise.
      */
     @Override
