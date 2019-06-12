85b95818d4c58b32fbf905cff5f18391fce94174
jylee-git <jylee.w94@gmail.com>
2019-06-12 13:06:51 +0800

    Update CsvParserTest.java

--- src/test/java/reposense/parser/CsvParserTest.java
+++ src/test/java/reposense/parser/CsvParserTest.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
