cd3b4a5367c4050df3871b009c91b145caddedff
jylee-git <jylee.w94@gmail.com>
2019-06-13 17:05:01 +0800

    Fix travis

--- src/main/java/reposense/report/ReportGenerator.java
+++ src/main/java/reposense/report/ReportGenerator.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
