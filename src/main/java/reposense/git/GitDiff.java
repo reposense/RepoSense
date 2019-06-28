3765701965cc048d26a16439dec3a7452c621a53
jylee-git <jylee.w94@gmail.com>
2019-06-28 11:07:52 +0800

    Remove unused imports

--- src/main/java/reposense/git/GitDiff.java
+++ src/main/java/reposense/git/GitDiff.java
@@ -4,7 +4,6 @@ import static reposense.system.CommandRunner.runCommand;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
