eafdcdc3d6a7fb61c4627509e22157a0de614d0e
jylee-git <jylee.w94@gmail.com>
2019-07-03 17:02:57 +0800

    Change the first commit date shorthand to use d1.

--- src/main/java/reposense/parser/SinceDateArgumentType.java
+++ src/main/java/reposense/parser/SinceDateArgumentType.java
@@ -12,17 +12,21 @@ import net.sourceforge.argparse4j.inf.ArgumentParserException;
  */
 public class SinceDateArgumentType extends DateArgumentType {
     /*
-     * When user specifies ^, arbitrary first commit date will be returned.
+     * When user specifies "d1", arbitrary first commit date will be returned.
      * Then, ReportGenerator will replace the arbitrary since date with the earliest commit date.
      */
     public static final Date ARBITRARY_FIRST_COMMIT_DATE = new Date(Long.MIN_VALUE);
+    public static final String FIRST_COMMIT_DATE_SHORTHAND = "d1";
 
     /**
-     * Returns an arbitrary year {@link SinceDateArgumentType#ARBITRARY_FIRST_COMMIT_DATE} if user specifies ^ in
-     * {@code value}, or attempts to return the desired date otherwise.
+     * Returns an arbitrary year {@link SinceDateArgumentType#ARBITRARY_FIRST_COMMIT_DATE} if user specifies
+     * {@link SinceDateArgumentType#FIRST_COMMIT_DATE_SHORTHAND} in {@code value}, or attempts to return the
+     * desired date otherwise.
      */
     @Override
     public Optional<Date> convert(ArgumentParser parser, Argument arg, String value) throws ArgumentParserException {
-        return ("^".equals(value)) ? Optional.of(ARBITRARY_FIRST_COMMIT_DATE) : super.convert(parser, arg, value);
+        return (FIRST_COMMIT_DATE_SHORTHAND.equals(value))
+                ? Optional.of(ARBITRARY_FIRST_COMMIT_DATE)
+                : super.convert(parser, arg, value);
     }
 }
