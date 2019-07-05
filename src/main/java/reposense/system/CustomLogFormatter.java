6ef8c9b89c7acbdec9bc410e8a0672697f278aba
bluein-green <j.minjia@gmail.com>
2019-07-05 17:27:10 +0800

    CustomLogFormatter.java: add error highlight for Level.WARNING/SEVERE log messages

--- src/main/java/reposense/system/CustomLogFormatter.java
+++ src/main/java/reposense/system/CustomLogFormatter.java
@@ -3,6 +3,7 @@ package reposense.system;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
 
@@ -12,11 +13,17 @@ import java.util.logging.SimpleFormatter;
 public class CustomLogFormatter extends SimpleFormatter {
 
     private static final DateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
+    private static final String ERROR_HIGHLIGHT = "\u001b[37;41m[ERROR]\u001b[0m";
 
     @Override
     public synchronized String format(LogRecord record) {
         StringBuilder builder = new StringBuilder();
         builder.append(dateFormat.format(new Date(record.getMillis()))).append(" - ");
+
+        if (record.getLevel().intValue() >= Level.WARNING.intValue()) {
+            builder.append(ERROR_HIGHLIGHT).append(" ");
+        }
+
         builder.append(formatMessage(record));
         builder.append("\n");
 
