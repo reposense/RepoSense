c8badc8bbc87faf50c7baf7fa2fbb9d8e57d857a
bluein-green <j.minjia@gmail.com>
2019-07-09 14:21:07 +0800

    CustomLogFormatter.java: use jansi renderer + add [WARNING] highlight

--- src/main/java/reposense/system/CustomLogFormatter.java
+++ src/main/java/reposense/system/CustomLogFormatter.java
@@ -1,5 +1,7 @@
 package reposense.system;
 
+import static org.fusesource.jansi.Ansi.ansi;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,21 +9,33 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
 
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.AnsiConsole;
+
 /**
  * Custom log formatter for different handlers to display only relevant information
  */
 public class CustomLogFormatter extends SimpleFormatter {
 
     private static final DateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
-    private static final String ERROR_HIGHLIGHT = "\u001b[37;41m[ERROR]\u001b[0m";
+    private static final String ERROR_HIGHLIGHT = ansi().bg(Ansi.Color.RED).fg(Ansi.Color.WHITE).a("[ERROR]")
+            .reset().toString();
+    private static final String WARNING_HIGHLIGHT = ansi().bg(Ansi.Color.YELLOW).fg(Ansi.Color.BLACK).a("[WARNING]")
+            .reset().toString();
+
+    public CustomLogFormatter() {
+        AnsiConsole.systemInstall();
+    }
 
     @Override
     public synchronized String format(LogRecord record) {
         StringBuilder builder = new StringBuilder();
         builder.append(dateFormat.format(new Date(record.getMillis()))).append(" - ");
 
-        if (record.getLevel().intValue() >= Level.WARNING.intValue()) {
+        if (record.getLevel().equals(Level.SEVERE)) {
             builder.append(ERROR_HIGHLIGHT).append(" ");
+        } else if (record.getLevel().equals(Level.WARNING)) {
+            builder.append(WARNING_HIGHLIGHT).append(" ");
         }
 
         builder.append(formatMessage(record));
