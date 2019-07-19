8405c4bd52d3476ffeb1af0103ab23e2430b6f38
bluein-green <j.minjia@gmail.com>
2019-07-19 16:30:50 +0800

    code view loading benchmark test added

---
+++ frontend/cypress/tests/codeView_load_benchmark.js
@@ -0,0 +1,33 @@
+describe('load code view benchmark', function() {
+  const MAXIMUM_LOADING_TIME = 8000;
+  const MAXIMUM_LOADING_TIME_SECONDS = MAXIMUM_LOADING_TIME / 1000;
+  let loadingTimeSeconds;
+
+  it(`time taken to load code view is within ${MAXIMUM_LOADING_TIME_SECONDS}s`,
+    function() {
+    // ensure that icons are loaded
+    Cypress.wait();
+
+    let startTime;
+
+    cy.get('.summary-chart__title').contains('Eugene')
+      .parent()
+      .within(($title) => {
+          cy.get('a .summary-chart__title--button.fa-code').click();
+          startTime = performance.now();
+          console.log("starting now");
+      })
+
+    cy.get('#tab-authorship .files')
+      .then(() => {
+        const endTime = performance.now();
+        const loadingTime = endTime - startTime;
+
+        loadingTimeSeconds = (loadingTime / 1000).toFixed(3);
+
+        assert.isTrue(loadingTime < MAXIMUM_LOADING_TIME,
+          "loading time is within limit");
+        cy.log(`time taken to load code view: ${loadingTimeSeconds}s`)
+      });
+  });
+});
