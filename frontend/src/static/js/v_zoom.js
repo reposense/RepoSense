21ffd32dae64774b28062da8a07e279b9d0c8588
jylee-git <jylee.w94@gmail.com>
2019-07-29 18:43:43 +0800

    Refactored code and fix v-on:click not called when icon is not wrapped with "a"

--- frontend/src/static/js/v_zoom.js
+++ frontend/src/static/js/v_zoom.js
@@ -1,3 +1,15 @@
+window.toggleCommitBodyMessage = function toggleCommitBodyMessage(ele) {
+  // function for expanding/collapsing a commit message body.
+  const activeClass = 'commit-message--body-active';
+  const inactiveClass = 'commit-message--body';
+
+  const commitMessageBodyElemet = ele.nextElementSibling;
+
+  commitMessageBodyElemet.className = commitMessageBodyElemet.className === activeClass
+    ? inactiveClass
+    : activeClass;
+};
+
 window.vZoom = {
   props: ['info'],
   template: window.$('v_zoom').innerHTML,
@@ -5,6 +17,7 @@ window.vZoom = {
     return {
       filterTimeFrame: window.hashParams.timeframe,
       showAllCommitMessageBody: true,
+      numExpandedCommitMessages: this.getNumCommitMessageBody(),
     };
   },
   methods: {
@@ -25,15 +38,38 @@ window.vZoom = {
       return `${window.getBaseLink(this.info.user.repoId)}/commit/${slice.hash}`;
     },
 
-    toggleAllCommitMessagesBody() {
-      this.showAllCommitMessageBody = !this.showAllCommitMessageBody;
-
-      const renameValue = this.showAllCommitMessageBody ? 'commit-message active' : 'commit-message';
-
-      const commitMessageClasses = document.getElementsByClassName('commit-message');
-      Array.from(commitMessageClasses).forEach((comitMessageClass) => {
-        comitMessageClass.className = renameValue;
+    getNumCommitMessageBody() {
+      let numNonEmptyCommitMessageBody = 0;
+      this.info.user.commits.forEach((commit) => {
+        commit.commitResults.forEach((commitResult) => {
+          if (commitResult.messageBody !== '') {
+            numNonEmptyCommitMessageBody += 1;
+          }
+        });
       });
+
+      return numNonEmptyCommitMessageBody;
+    },
+
+    toggleAllCommitMessagesBody(isActive) {
+      this.showAllCommitMessageBody = isActive;
+
+      const renameValue = this.showAllCommitMessageBody
+        ? 'commit-message--body-active'
+        : 'commit-message--body';
+
+      const commitMessageClasses = isActive
+        ? document.getElementsByClassName('commit-message--body')
+        : document.getElementsByClassName('commit-message--body-active');
+      Array.from(commitMessageClasses).forEach((commitMessageClass) => {
+        commitMessageClass.className = renameValue;
+      });
+
+      this.numExpandedCommitMessages = isActive ? this.getNumCommitMessageBody() : 0;
+    },
+
+    updateNumExpandedCommitMessages() {
+      this.numExpandedCommitMessages = document.getElementsByClassName('commit-message--body-active').length;
     },
   },
   created() {
