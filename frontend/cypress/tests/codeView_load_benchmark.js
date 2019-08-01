7198a1ebff5b36ebae6efd4575a823d936f8161d
bluein-green <j.minjia@gmail.com>
2019-08-01 14:40:48 +0800

    retrigger travis

--- frontend/cypress/tests/codeView_load_benchmark.js
+++ frontend/cypress/tests/codeView_load_benchmark.js
@@ -49,6 +49,7 @@ describe('load code view benchmark', function() {
       });
   };
 
+
   for (let i = 0; i < NUM_TRIALS; i++) {
     it(`time taken to load code view (trial ${i+1})`, function() {
       timeTrial(i);
