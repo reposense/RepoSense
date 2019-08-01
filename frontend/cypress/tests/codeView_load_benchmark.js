2e6245c27053a726e78d6709549d7d7bcb525d42
bluein-green <j.minjia@gmail.com>
2019-08-01 16:27:02 +0800

    benchmark: remove condition

--- frontend/cypress/tests/codeView_load_benchmark.js
+++ frontend/cypress/tests/codeView_load_benchmark.js
@@ -48,9 +48,7 @@ describe('load code view benchmark', function() {
 
   for (let i = 0; i < NUM_TRIALS; i++) {
     it(`time taken to load code view (trial ${i+1})`, function() {
-      if (!isATrialWithinMaxTime) {
         timeTrial(i);
-      }
     });
   }
 
