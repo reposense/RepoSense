8656136850735434e8c34bf571f654effeb88884
bluein-green <j.minjia@gmail.com>
2019-08-01 15:37:23 +0800

    benchmark: bring in new changes

--- frontend/cypress/tests/codeView_load_benchmark.js
+++ frontend/cypress/tests/codeView_load_benchmark.js
@@ -11,7 +11,7 @@ describe('load code view benchmark', function() {
 
   const MAXIMUM_LOADING_TIME = THRESHOLD_LOADING_TIME + ALLOWED_BUFFER_TIME;
 
-  let loadingTimes = [];
+  let isATrialWithinMaxTime = false;
 
   const timeTrial = function(i) {
       let startTime;
@@ -39,26 +39,23 @@ describe('load code view benchmark', function() {
 
           cy.log(`trial ${i+1} loading time: ${loadingTimeSeconds.toFixed(3)}s`);
 
-          loadingTimes.push(loadingTime);
+          if (loadingTime <= MAXIMUM_LOADING_TIME) {
+            isATrialWithinMaxTime = true;
+          }
         });
   };
 
 
   for (let i = 0; i < NUM_TRIALS; i++) {
     it(`time taken to load code view (trial ${i+1})`, function() {
+      if (isATrialWithinMaxTime) {
+        this.skip();
+      }
       timeTrial(i);
     });
   }
 
   it(`at least one trial is within ${THRESHOLD_LOADING_TIME_SECONDS}(+${ALLOWED_BUFFER_TIME_SECONDS})s`, function() {
-    const totalLoadingTime = loadingTimes.reduce((acc, curr) => acc + curr, 0);
-    const averageLoadingTime = totalLoadingTime / NUM_TRIALS;
-    const averageLoadingTimeSeconds = averageLoadingTime / 1000;
-
-    const isATrialWithinMaxTime = loadingTimes.map((time) => time <= MAXIMUM_LOADING_TIME)
-                                              .reduce((acc, curr) => acc || curr, false);
-
-    assert.isTrue(isATrialWithinMaxTime,
-      `[average loading time: ${averageLoadingTimeSeconds.toFixed(3)}s]`);
+    assert.isTrue(isATrialWithinMaxTime);
   });
 });
