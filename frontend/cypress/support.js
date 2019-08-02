df09eefc28ac36eb798f1f4dafde975c9e0eeeb7
bluein-green <j.minjia@gmail.com>
2019-08-02 16:43:15 +0800

    cypress support.js: increase cy.visit() timeout to 3min

--- frontend/cypress/support.js
+++ frontend/cypress/support.js
@@ -6,7 +6,7 @@ Cypress.Screenshot.defaults({
 });
 
 beforeEach(() => {
-  cy.visit('/', { timeout: 120000 });
+  cy.visit('/', { timeout: 180000 });
 });
 
 // Slows down test execution on non-CI environment.
