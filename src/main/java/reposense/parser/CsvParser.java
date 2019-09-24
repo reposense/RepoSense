47dbff8e193f736045ae6b49f7f16ebe40611b5b
anubh-v <anubhav@u.nus.edu>
2019-09-24 11:13:42 +0800

    Remove unused import

--- src/main/java/reposense/parser/CsvParser.java
+++ src/main/java/reposense/parser/CsvParser.java
@@ -3,7 +3,6 @@ package reposense.parser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
