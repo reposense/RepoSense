0951e2aa9b3149ab2b07c6400b181f287769e467
0blivious <lyw@415309@gmail.com>
2019-10-07 12:01:20 +0800

    Update cypress test

--- frontend/cypress/tests/codeView_toolBar_searchBox.js
+++ frontend/cypress/tests/codeView_toolBar_searchBox.js
@@ -1,7 +1,7 @@
 describe('search bar', () => {
   it('non-existent author shows no result', () => {
     cy.get('#tabs-wrapper .tab-close').click();
-    cy.get('#summary-wrapper input[type=search]')
+    cy.get('#summary-wrapper input[type=text]')
         .type('abcdef')
         .type('{enter}');
 
@@ -23,7 +23,7 @@ describe('search bar', () => {
 
   it('unique author shows one result', () => {
     cy.get('#tabs-wrapper .tab-close').click();
-    cy.get('#summary-wrapper input[type=search]')
+    cy.get('#summary-wrapper input[type=text]')
         .type('Yong Hao TENG')
         .type('{enter}');
 
