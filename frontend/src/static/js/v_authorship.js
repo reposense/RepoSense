6d3d269002144f6b164b022bf4ba620c2635faee
tejas bhuwania <tejasbhuwania2805@gmail.com>
2019-11-03 22:23:57 +0800

    Update variable name to be more meaningful

--- frontend/src/static/js/v_authorship.js
+++ frontend/src/static/js/v_authorship.js
@@ -39,7 +39,7 @@ window.vAuthorship = {
       totalLineCount: '',
       totalBlankLineCount: '',
       filesSortType: 'lineOfCode',
-      toReverseSortFiles: 'descending',
+      sortByOrder: 'descending',
       activeFilesCount: 0,
       filterSearch: '*',
       sortingFunction: window.comparator(filesSortDict.lineOfCode),
@@ -211,7 +211,7 @@ window.vAuthorship = {
     },
 
     sortFiles() {
-      this.sortingFunction = (a, b) => (this.toReverseSortFiles === 'descending' ? -1 : 1)
+      this.sortingFunction = (a, b) => (this.sortByOrder === 'descending' ? -1 : 1)
           * window.comparator(filesSortDict[this.filesSortType])(a, b);
     },
 
