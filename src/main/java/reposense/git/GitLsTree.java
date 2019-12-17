3d5ebd549b4c3f8d5b14a0031edd10c1aa384466
jylee-git <jylee.w94@gmail.com>
2019-12-17 17:39:18 +0800

    Update GitLsTree.java

--- src/main/java/reposense/git/GitLsTree.java
+++ src/main/java/reposense/git/GitLsTree.java
@@ -25,8 +25,8 @@ public class GitLsTree {
     private static final Pattern ILLEGAL_WINDOWS_FILE_PATTERN = Pattern.compile(
             "^((CON|PRN|AUX|NUL|COM[1-9]|LPT[1-9])(?=\\..*|$))" // contains reserved values (e.g.: com1)
                     + "|[<>:\"\\\\|?*\\x00-\\x1F]"  // contains any reserved characters in directory name
-                    + "|(^\\s)|(/\\s+)"             // file or folder names with leading whitespaces
-                    + "|([\\s.]$)|([\\s.]+/)",      // folder or file names ending with period or whitespaces
+                    + "|(^\\s)|((?<=/)\\s+)"             // file or folder names with leading whitespaces
+                    + "|([\\s.]$)|([\\s.]+(?=/))",      // folder or file names ending with period or whitespaces
             Pattern.CASE_INSENSITIVE);
 
     private static final Logger logger = LogsManager.getLogger(GitLsTree.class);
