faa662f2d07d0c3c79e39b0cfd2a004a795b82fe
jamessspanggg <jamespangmunwai@gmail.com>
2020-02-27 11:56:54 +0800

    Add viewing of existing comments (testing)

--- config/travis/deploy-surge.sh
+++ config/travis/deploy-surge.sh
@@ -45,6 +45,11 @@ do
     # Done so because every PR is an issue, and the issues api allows to post general comments,
     # while the PR api requires that comments are made to specific files and specific commits
     GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+
+    # Check if there's existing comment for deployment link
+    curl ${GITHUB_PR_COMMENTS} -v
+
+    # Post deployment link as comment
     curl -H "Authorization: token ${GITHUB_API_TOKEN}" -X POST \
     -d "{\"body\": \"Travis automatic deployment: ${DEPLOY_DOMAIN}\"}" \
     ${GITHUB_PR_COMMENTS}
