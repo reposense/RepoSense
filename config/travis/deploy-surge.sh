e5f57a99bc76738a90dddec9349b66ee9837b376
jamessspanggg <jamespangmunwai@gmail.com>
2020-02-29 23:46:47 +0800

    Fix codacy issue

--- config/travis/deploy-surge.sh
+++ config/travis/deploy-surge.sh
@@ -21,7 +21,7 @@ then
   #sort -t ...          ref: http://stackoverflow.com/a/4495368/689223
   #reverse with sed     ref: http://stackoverflow.com/a/744093/689223
   #git tags | ignore release candidates | sort versions | reverse | pick first line
-  LATEST_TAG=`git tag | grep -v rc | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | sed '1!G;h;$!d' | sed -n 1p`
+  LATEST_TAG=$(git tag | grep -v rc | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | sed '1!G;h;$!d' | sed -n 1p)
   echo $LATEST_TAG
   if [ "$TRAVIS_TAG" == "$LATEST_TAG" ]
   then
@@ -39,7 +39,7 @@ do
   # Domain names follow the RFC1123 spec [a-Z] [0-9] [-]
   # The length is limited to 253 characters
   # https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
-  DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/|\.]+/\-/g'`
+  DEPLOY_SUBDOMAIN=$(echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/|\.]+/\-/g')
 
   # skipping empty deploy subdomains
   if [ -n "${DEPLOY_SUBDOMAIN// }" ]
