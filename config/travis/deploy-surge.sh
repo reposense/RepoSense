e711b83bc6776696b0f2dbf6e3003cdafaf22588
jamessspanggg <jamespangmunwai@gmail.com>
2020-02-29 22:17:52 +0800

    Add debugging

--- config/travis/deploy-surge.sh
+++ config/travis/deploy-surge.sh
@@ -58,6 +58,7 @@ do
 #    -d "{\"body\": \"Travis automatic deployment: ${DEPLOY_DOMAIN}\"}" \
 #    ${GITHUB_PR_COMMENTS}
 
+    echo "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/statuses/$GIT_COMMIT?access_token=${GITHUB_API_TOKEN}"
     curl "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/statuses/$GIT_COMMIT?access_token=${GITHUB_API_TOKEN}" \
     -H "Content-Type: application/json" \
     -X POST \
