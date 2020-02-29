b26cbfadcf5aaa3d79193c2d0b0a5a8cbdbb5562
jamessspanggg <jamespangmunwai@gmail.com>
2020-02-29 17:21:05 +0800

    Testing to see if can get pr message

--- config/travis/deploy-surge.sh
+++ config/travis/deploy-surge.sh
@@ -50,10 +50,13 @@ do
     # while the PR api requires that comments are made to specific files and specific commits
     GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
 
-    # Check if there's existing comment for deployment link
-    curl ${GITHUB_PR_COMMENTS} -v
+    comments = json_decode(`curl ${GITHUB_PR_COMMENTS}`, true)
 
-    # Post deployment link as comment
+    # get first comment, which is the pr message. This step assumes that pr has message body.
+    pr_message = ${comments[0]['body']}
+    echo ${pr_message}
+
+    # modify pr message body with deployment link
     curl -H "Authorization: token ${GITHUB_API_TOKEN}" -X POST \
     -d "{\"body\": \"Travis automatic deployment: ${DEPLOY_DOMAIN}\"}" \
     ${GITHUB_PR_COMMENTS}
