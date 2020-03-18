f16ba1d169cf79b5620035d2e2a3280cb94e7e59
anubh-v <anubhav@u.nus.edu>
2020-03-19 01:35:07 +0800

    AnnotatorAnalyzer: Generalise method of asserting analysis correctness

--- src/test/java/reposense/authorship/AnnotatorAnalyzerTest.java
+++ src/test/java/reposense/authorship/AnnotatorAnalyzerTest.java
@@ -1,11 +1,14 @@
 package reposense.authorship;
 
+import static reposense.model.Author.UNKNOWN_AUTHOR;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,6 +27,17 @@ public class AnnotatorAnalyzerTest extends GitTestTemplate {
     private static final Author FAKE_AUTHOR = new Author(FAKE_AUTHOR_NAME);
     private static final Date SINCE_DATE = TestUtil.getSinceDate(2018, Calendar.FEBRUARY, 8);
     private static final Date UNTIL_DATE = TestUtil.getUntilDate(2018, Calendar.FEBRUARY, 10);
+    private static final Author[] EXPECTED_LINE_AUTHORS_OVERRIDE_AUTHORSHIP_TEST = {
+            FAKE_AUTHOR, FAKE_AUTHOR, FAKE_AUTHOR, FAKE_AUTHOR,
+            MAIN_AUTHOR, MAIN_AUTHOR, MAIN_AUTHOR, MAIN_AUTHOR, MAIN_AUTHOR,
+            FAKE_AUTHOR, FAKE_AUTHOR
+    };
+    private static final Author[] EXPECTED_LINE_AUTHORS_DISOWN_CODE_TEST = {
+            FAKE_AUTHOR, FAKE_AUTHOR, FAKE_AUTHOR, FAKE_AUTHOR,
+            UNKNOWN_AUTHOR, UNKNOWN_AUTHOR, UNKNOWN_AUTHOR, UNKNOWN_AUTHOR, UNKNOWN_AUTHOR,
+            FAKE_AUTHOR, FAKE_AUTHOR
+    };
+
 
     @Before
     public void before() throws InvalidLocationException {
@@ -36,14 +50,14 @@ public class AnnotatorAnalyzerTest extends GitTestTemplate {
     public void analyzeAnnotation_authorNamePresentInConfig_overrideAuthorship() {
         config.setAuthorList(Arrays.asList(FAKE_AUTHOR, MAIN_AUTHOR));
         FileResult fileResult = getFileResult("annotationTest.java");
-        assertAnnotationAnalysisCorrectness(fileResult, FAKE_AUTHOR, MAIN_AUTHOR);
+        assertAnnotationAnalysisCorrectness(fileResult, Arrays.asList(EXPECTED_LINE_AUTHORS_OVERRIDE_AUTHORSHIP_TEST));
     }
 
     @Test
     public void analyzeAnnotation_authorNameNotInConfig_disownCode() {
         config.setAuthorList(Collections.singletonList(FAKE_AUTHOR));
         FileResult fileResult = getFileResult("annotationTest.java");
-        assertAnnotationAnalysisCorrectness(fileResult, FAKE_AUTHOR, Author.UNKNOWN_AUTHOR);
+        assertAnnotationAnalysisCorrectness(fileResult, Arrays.asList(EXPECTED_LINE_AUTHORS_DISOWN_CODE_TEST));
     }
 
     public FileResult getFileResult(String relativePath) {
@@ -52,18 +66,17 @@ public class AnnotatorAnalyzerTest extends GitTestTemplate {
     }
 
     /**
-     * Asserts the correctness of authorship overriding with regards to @@author tags given
-     * in the test file.
+     * For each line in {@code FileResult}, assert that it is attributed to the expected author.
      */
-    private void assertAnnotationAnalysisCorrectness(FileResult fileResult, Author originalAuthor,
-            Author overridingAuthor) {
-        for (LineInfo line : fileResult.getLines()) {
-            Author lineAuthor = line.getAuthor();
-            if (line.getContent().startsWith("fake")) {
-                assertEquals(originalAuthor, lineAuthor);
-            } else {
-                assertEquals(overridingAuthor, lineAuthor);
-            }
+    private void assertAnnotationAnalysisCorrectness(FileResult fileResult, List<Author> expectedLineAuthors) {
+        List<LineInfo> lines = fileResult.getLines();
+        assertEquals(expectedLineAuthors.size(), lines.size());
+
+        Iterator<Author> lineAuthorsItr = expectedLineAuthors.iterator();
+        Iterator<LineInfo> linesItr = lines.iterator();
+
+        while(linesItr.hasNext() && lineAuthorsItr.hasNext()) {
+            assertEquals(lineAuthorsItr.next(), linesItr.next().getAuthor());
         }
     }
 
