c8115b6bf2d05257bba1ae954a8cacbc75bcf77d
anubh-v <anubhav@u.nus.edu>
2020-03-27 23:22:52 +0800

    Add RepoLocationTest

---
+++ src/test/java/reposense/model/RepoLocationTest.java
@@ -0,0 +1,59 @@
+package reposense.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.tools.ant.types.Assertions;
+import org.junit.Test;
+
+import reposense.parser.InvalidLocationException;
+import reposense.util.AssertUtil;
+
+public class RepoLocationTest {
+
+    @Test
+    public void repoLocation_parseGitUrl_success() throws Exception {
+        String gitUrl = "https://github.com/reposense/RepoSense.git";
+        RepoLocation repoLocation = new RepoLocation(gitUrl);
+        assertEquals(gitUrl, repoLocation.toString());
+        assertEquals("RepoSense", repoLocation.getRepoName());
+        assertEquals("reposense", repoLocation.getOrganization());
+        assertTrue(repoLocation.getParsedBranch().isEmpty());
+
+        String gitUrlWithBranch = "https://github.com/reposense/testrepo-Alpha.git#release";
+        repoLocation = new RepoLocation(gitUrlWithBranch);
+        assertEquals("https://github.com/reposense/testrepo-Alpha.git", repoLocation.toString());
+        assertEquals("testrepo-Alpha", repoLocation.getRepoName());
+        assertEquals("reposense", repoLocation.getOrganization());
+        assertEquals("release", repoLocation.getParsedBranch().get());
+    }
+
+    @Test
+    public void repoLocation_parseInvalidGitUrl_throwsInvalidLocationException() {
+        AssertUtil.assertThrows(InvalidLocationException.class,
+                () -> new RepoLocation("git.com/reposense/RepoSense.git"));
+        AssertUtil.assertThrows(InvalidLocationException.class,
+                () -> new RepoLocation("https://github.com/RepoSense.git"));
+        AssertUtil.assertThrows(InvalidLocationException.class,
+                () -> new RepoLocation("https://github.com/RepoSense.gi"));
+    }
+
+    @Test
+    public void repoLocation_parseBranchUrl_success() throws Exception {
+        String branchUrl = "https://github.com/reposense/RepoSense/tree/feature_branch_issue#1010";
+        RepoLocation repoLocation = new RepoLocation(branchUrl);
+        assertEquals("https://github.com/reposense/RepoSense.git", repoLocation.toString());
+        assertEquals("RepoSense", repoLocation.getRepoName());
+        assertEquals("reposense", repoLocation.getOrganization());
+        assertEquals("feature_branch_issue#1010", repoLocation.getParsedBranch().get());
+    }
+
+    @Test
+    public void repoLocation_parseInvalidBranchUrl_throwsInvalidLocationException() throws Exception {
+        AssertUtil.assertThrows(InvalidLocationException.class,
+                () -> new RepoLocation("ftp://github.com/reposense/RepoSense/tree/feature_branch_issue#1010"));
+        AssertUtil.assertThrows(InvalidLocationException.class,
+                () -> new RepoLocation("https://github.com/reposense/RepoSense/tree/"));
+    }
+
+}
