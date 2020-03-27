1a884130642364e7bc1be920cedb081962cc07c7
yash-chowdhary <yashchowdhary98@gmail.com>
2020-03-27 17:51:17 +0800

    Remove unused code from files

--- src/main/java/reposense/model/RepoLocation.java
+++ src/main/java/reposense/model/RepoLocation.java
@@ -6,8 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
