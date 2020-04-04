4a7e824a36404daa00d96b60c4c89e2e9c5f3c8e
anubh-v <anubhav@u.nus.edu>
2020-04-04 23:32:42 +0800

    RepoLocationTest: Add more negative tests

--- src/test/java/reposense/model/RepoLocationTest.java
+++ src/test/java/reposense/model/RepoLocationTest.java
@@ -11,16 +11,16 @@ import reposense.util.AssertUtil;
 public class RepoLocationTest {
 
     @Test
-    public void repoLocation_parseGitUrl_success() throws Exception {
-        String gitUrl = "https://github.com/reposense/RepoSense.git";
-        RepoLocation repoLocation = new RepoLocation(gitUrl);
-        assertEquals(gitUrl, repoLocation.toString());
+    public void repoLocation_parseRepoUrl_success() throws Exception {
+        String repoUrl = "https://github.com/reposense/RepoSense.git";
+        RepoLocation repoLocation = new RepoLocation(repoUrl);
+        assertEquals(repoUrl, repoLocation.toString());
         assertEquals("RepoSense", repoLocation.getRepoName());
         assertEquals("reposense", repoLocation.getOrganization());
         assertFalse(repoLocation.getParsedBranch().isPresent());
 
-        String gitUrlWithBranch = "https://github.com/reposense/testrepo-Alpha.git#release";
-        repoLocation = new RepoLocation(gitUrlWithBranch);
+        String repoUrlWithBranch = "https://github.com/reposense/testrepo-Alpha.git#release";
+        repoLocation = new RepoLocation(repoUrlWithBranch);
         assertEquals("https://github.com/reposense/testrepo-Alpha.git", repoLocation.toString());
         assertEquals("testrepo-Alpha", repoLocation.getRepoName());
         assertEquals("reposense", repoLocation.getOrganization());
@@ -28,13 +28,19 @@ public class RepoLocationTest {
     }
 
     @Test
-    public void repoLocation_parseInvalidGitUrl_throwsInvalidLocationException() {
+    public void repoLocation_parseInvalidRepoUrl_throwsInvalidLocationException() {
         // non GitHub url should rejected
-        assertInvalidLocation("git.com/reposense/RepoSense.git");
+        assertInvalidLocation("https://gitlab.com/reposense/RepoSense.git");
         // url without organisation name should be rejected
-        assertInvalidLocation("https://github.com/RepoSense.git");
-        // url with a typo should be rejected
-        assertInvalidLocation("https://github.com/RepoSense.gi");
+        assertInvalidLocation("https://github.com/reposense.git");
+        // url without repo name should be rejected
+        assertInvalidLocation("https://github.com/reposense/.git");
+        assertInvalidLocation("https://github.com/reposense");
+        // url without a .git suffix should be rejected
+        assertInvalidLocation("https://github.com/reposense/RepoSensegit");
+        assertInvalidLocation("https://github.com/reposense/RepoSense-git");
+        assertInvalidLocation("https://github.com/reposense/RepoSense.gi");
+        assertInvalidLocation("https://github.com/reposense/RepoSense");
     }
 
     @Test
@@ -48,14 +54,21 @@ public class RepoLocationTest {
     }
 
     @Test
-    public void repoLocation_parseInvalidBranchUrl_throwsInvalidLocationException() throws Exception {
+    public void repoLocation_parseInvalidBranchUrl_throwsInvalidLocationException() {
         // ftp url should be rejected
         assertInvalidLocation("ftp://github.com/reposense/RepoSense/tree/feature_branch_issue#1010");
         // url without branch name should be rejected
         assertInvalidLocation("https://github.com/reposense/RepoSense/tree/");
+        assertInvalidLocation("https://github.com/reposense/RepoSense/tree");
+        // url without the 'tree' keyword should be rejected
+        assertInvalidLocation("https://github.com/reposense/RepoSense/feature_branch_issue#1010");
+        assertInvalidLocation("https://github.com/reposense/RepoSense/TREE/feature_branch_issue#1010");
+        assertInvalidLocation("https://github.com/reposense/RepoSense/tre/feature_branch_issue#1010");
+        // non GitHub url should be rejected
+        assertInvalidLocation("https://gitlab.com/reposense/RepoSense/tree/feature_branch_issue#1010");
     }
 
-    private void assertInvalidLocation(String rawLocation) {
+    private static void assertInvalidLocation(String rawLocation) {
         AssertUtil.assertThrows(InvalidLocationException.class,
                 RepoLocation.MESSAGE_INVALID_LOCATION, () -> new RepoLocation(rawLocation));
     }
