8d694b8a3027659912ff05b3321e0f87ad02dc9a
jamessspanggg <jamespangmunwai@gmail.com>
2020-04-12 23:06:29 +0800

    Add front end tests for filterBreakdown

---
+++ frontend/cypress/tests/chartView_filterBreakdown.js
@@ -0,0 +1,63 @@
+describe('filter breakdown', () => {
+  it('check breakdown by file type should show file types', () => {
+    Cypress.wait();
+
+    cy.get('#summary label.filter-breakdown input:visible')
+        .should('be.visible')
+        .check()
+        .should('be.checked');
+
+    // should show file type checkboxes
+    cy.get('#summary div.fileTypes')
+        .should('be.visible');
+
+    // should show file type legends
+    cy.get('#summary-charts div.summary-charts__fileType--breakdown')
+        .should('be.visible');
+  });
+
+  it('uncheck all file types should show no file types', () => {
+    Cypress.wait();
+
+    cy.get('#summary label.filter-breakdown input:visible')
+        .should('be.visible')
+        .check()
+        .should('be.checked');
+
+    // uncheck all file types
+    cy.get('#summary div.fileTypes input:visible')
+        .first()
+        .uncheck()
+        .should('not.be.checked');
+
+    // should not show file type legends
+    cy.get('#summary-charts div.summary-charts__fileType--breakdown')
+        .should('not.be.visible');
+
+    // should not show any contribution bar
+    cy.get('#summary-charts div.summary-chart__contribution')
+        .should('be.empty');
+  });
+
+  it('uncheck file type should uncheck all option and not show legend', () => {
+    Cypress.wait();
+
+    cy.get('#summary label.filter-breakdown input:visible')
+        .should('be.visible')
+        .check()
+        .should('be.checked');
+
+    // uncheck all gradle file type
+    cy.get('#summary div.fileTypes input[id="gradle"]')
+        .uncheck()
+        .should('not.be.checked');
+
+    // should not show gradle file type legend
+    cy.get('#summary-charts > div > div.summary-charts__fileType--breakdown span')
+        .should('not.contain.text', 'gradle');
+
+    // all option should be unchecked
+    cy.get('#summary div.fileTypes input[id="gradle"]')
+        .should('not.be.checked');
+  });
+});
