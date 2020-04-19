reposense/RepoSense

commit 1a34084e5379a86bd685c1f35847c1038a1c37de

[#576] Refactor summary charts from summary (#1059)
* Refactor summary and charts out

* remove wrapper class

* Perform filter in summary

* folder structure

* update

* update

* remove extra blank line

* remove empty data

* reset updatefilter

* change name to vSummaryCharts

* use filteredrepo

* fix

* change naming

* fix

* codacy

* ci

Co-authored-by: 0blivious <lyw@415309@gmail.com>
Co-authored-by: WANG CHAO <1229983126@qq.com>

frontend/src/static/js/main.js
@@ -318,9 +318,9 @@ window.app = new window.Vue({
     },
   },
   components: {
-    v_zoom: window.vZoom,
-    v_summary: window.vSummary,
-    v_authorship: window.vAuthorship,
+    vZoom: window.vZoom,
+    vSummary: window.vSummary,
+    vAuthorship: window.vAuthorship,
     CircleSpinner: window.VueLoadingSpinner.Circle,
   },
   created() {

frontend/src/static/js/v_summary.js
@@ -1,102 +1,18 @@
-window.dismissTab = function dismissTab(node) {
-  const parent = node.parentNode;
-  parent.style.display = 'none';
-};
-
-window.comparator = (fn, sortingOption = '') => function compare(a, b) {
-  let a1;
-  let b1;
-  if (sortingOption) {
-    a1 = fn(a, sortingOption).toLowerCase
-        ? fn(a, sortingOption).toLowerCase()
-        : fn(a, sortingOption);
-    b1 = fn(b, sortingOption).toLowerCase
-        ? fn(b, sortingOption).toLowerCase()
-        : fn(b, sortingOption);
-  } else {
-    a1 = fn(a).toLowerCase ? fn(a).toLowerCase() : fn(a);
-    b1 = fn(b).toLowerCase ? fn(b).toLowerCase() : fn(b);
-  }
-  if (a1 === b1) {
-    return 0;
-  } if (a1 < b1) {
-    return -1;
-  }
-  return 1;
-};
-
-// ui funcs, only allow one ramp to be highlighted //
-let drags = [];
-
-function getBaseTarget(target) {
-  return (target.className === 'summary-chart__ramp')
-      ? target
-      : getBaseTarget(target.parentElement);
-}
-
-function deactivateAllOverlays() {
-  document.querySelectorAll('.summary-chart__ramp .overlay')
-      .forEach((x) => { x.className = 'overlay'; });
-}
-
-function dragViewDown(evt) {
-  deactivateAllOverlays();
-
-  const pos = evt.clientX;
-  const ramp = getBaseTarget(evt.target);
-  drags = [pos];
-
-  const base = ramp.offsetWidth;
-  const offset = ramp.parentElement.offsetLeft;
-
-  const overlay = ramp.getElementsByClassName('overlay')[0];
-  overlay.style.marginLeft = '0';
-  overlay.style.width = `${(pos - offset) * 100 / base}%`;
-  overlay.className += ' edge';
-}
-
-function dragViewUp(evt) {
-  deactivateAllOverlays();
-  const ramp = getBaseTarget(evt.target);
-
-  const base = ramp.offsetWidth;
-  drags.push(evt.clientX);
-  drags.sort((a, b) => a - b);
-
-  const offset = ramp.parentElement.offsetLeft;
-  drags = drags.map((x) => (x - offset) * 100 / base);
-
-  const overlay = ramp.getElementsByClassName('overlay')[0];
-  overlay.style.marginLeft = `${drags[0]}%`;
-  overlay.style.width = `${drags[1] - drags[0]}%`;
-  overlay.className += ' show';
-}
-
-window.viewClick = function viewClick(evt) {
-  const isKeyPressed = this.isMacintosh ? evt.metaKey : evt.ctrlKey;
-  if (drags.length === 2) {
-    drags = [];
-  }
-
-  if (isKeyPressed) {
-    return drags.length === 0
-        ? dragViewDown(evt)
-        : dragViewUp(evt);
-  }
-
-  return null;
-};
-
 // date functions //
 const DAY_IN_MS = (1000 * 60 * 60 * 24);
 window.DAY_IN_MS = DAY_IN_MS;
 const WEEK_IN_MS = DAY_IN_MS * 7;
 const dateFormatRegex = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/;
 
+window.deactivateAllOverlays = function deactivateAllOverlays() {
+  document.querySelectorAll('.summary-chart__ramp .overlay')
+      .forEach((x) => { x.className = 'overlay'; });
+};
 
-function getDateStr(date) {
+window.getDateStr = function getDateStr(date) {
   return (new Date(date)).toISOString().split('T')[0];
-}
+};
+
 function dateRounding(datestr, roundDown) {
   // rounding up to nearest monday
   const date = new Date(datestr);
@@ -108,7 +24,7 @@ function dateRounding(datestr, roundDown) {
     datems += ((8 - day) % 7) * DAY_IN_MS;
   }
 
-  return getDateStr(datems);
+  return window.getDateStr(datems);
 }
 
 window.vSummary = {
@@ -153,12 +69,15 @@ window.vSummary = {
     sortGroupSelection() {
       this.getFiltered();
     },
+
     sortWithinGroupSelection() {
       this.getFiltered();
     },
+
     filterTimeFrame() {
       this.getFiltered();
     },
+
     filterGroupSelection() {
       // merge group is not allowed when group by none
       if (this.filterGroupSelection === 'groupByNone') {
@@ -167,9 +86,11 @@ window.vSummary = {
 
       this.getFiltered();
     },
+
     isMergeGroup() {
       this.getFiltered();
     },
+
     tmpFilterSinceDate() {
       if (this.tmpFilterSinceDate && this.tmpFilterSinceDate >= this.minDate) {
         this.filterSinceDate = this.tmpFilterSinceDate;
@@ -178,6 +99,7 @@ window.vSummary = {
       }
       this.getFiltered();
     },
+
     tmpFilterUntilDate() {
       if (this.tmpFilterUntilDate && this.tmpFilterUntilDate <= this.maxDate) {
         this.filterUntilDate = this.tmpFilterUntilDate;
@@ -200,21 +122,7 @@ window.vSummary = {
         }
       },
     },
-    avgCommitSize() {
-      let totalCommits = 0;
-      let totalCount = 0;
-      this.filtered.forEach((repo) => {
-        repo.forEach((user) => {
-          user.commits.forEach((slice) => {
-            if (slice.insertions > 0) {
-              totalCount += 1;
-              totalCommits += slice.insertions;
-            }
-          });
-        });
-      });
-      return totalCommits / totalCount;
-    },
+
     avgContributionSize() {
       let totalLines = 0;
       let totalCount = 0;
@@ -229,93 +137,9 @@ window.vSummary = {
 
       return totalLines / totalCount;
     },
-    filteredRepos() {
-      return this.filtered.filter((repo) => repo.length > 0);
-    },
   },
   methods: {
     // view functions //
-    getFileTypeContributionBars(fileTypeContribution) {
-      let currentBarWidth = 0;
-      const fullBarWidth = 100;
-      const contributionPerFullBar = (this.avgContributionSize * 2);
-      const allFileTypesContributionBars = {};
-
-      Object.keys(fileTypeContribution)
-          .filter((fileType) => this.checkedFileTypes.includes(fileType))
-          .forEach((fileType) => {
-            const contribution = fileTypeContribution[fileType];
-            let barWidth = (contribution / contributionPerFullBar) * fullBarWidth;
-            const contributionBars = [];
-
-            // if contribution bar for file type is able to fit on the current line
-            if (currentBarWidth + barWidth < fullBarWidth) {
-              contributionBars.push(barWidth);
-              currentBarWidth += barWidth;
-            } else {
-              // take up all the space left on the current line
-              contributionBars.push(fullBarWidth - currentBarWidth);
-              barWidth -= fullBarWidth - currentBarWidth;
-              // additional bar width will start on a new line
-              const numOfFullBars = Math.floor(barWidth / fullBarWidth);
-              for (let i = 0; i < numOfFullBars; i += 1) {
-                contributionBars.push(fullBarWidth);
-              }
-              const remainingBarWidth = barWidth % fullBarWidth;
-              if (remainingBarWidth !== 0) {
-                contributionBars.push(remainingBarWidth);
-              }
-              currentBarWidth = remainingBarWidth;
-            }
-
-            allFileTypesContributionBars[fileType] = contributionBars;
-          });
-
-      return allFileTypesContributionBars;
-    },
-    getFileTypes(repo) {
-      const fileTypes = [];
-      repo.forEach((user) => {
-        Object.keys(user.fileTypeContribution).forEach((fileType) => {
-          if (this.checkedFileTypes.includes(fileType) && !fileTypes.includes(fileType)) {
-            fileTypes.push(fileType);
-          }
-        });
-      });
-      return fileTypes;
-    },
-    getContributionBars(totalContribution) {
-      const res = [];
-      const contributionLimit = (this.avgContributionSize * 2);
-
-      const cnt = parseInt(totalContribution / contributionLimit, 10);
-      for (let cntId = 0; cntId < cnt; cntId += 1) {
-        res.push(100);
-      }
-
-      const last = (totalContribution % contributionLimit) / contributionLimit;
-      if (last !== 0) {
-        res.push(last * 100);
-      }
-
-      return res;
-    },
-
-    getAuthorProfileLink(userName) {
-      return `https://github.com/${userName}`;
-    },
-
-    getRepoLink(repo) {
-      const { REPOS } = window;
-      const { location, branch } = REPOS[repo.repoId];
-
-      if (Object.prototype.hasOwnProperty.call(location, 'organization')) {
-        return `${window.BASE_URL}/${location.organization}/${location.repoName}/tree/${branch}`;
-      }
-
-      return repo.location;
-    },
-
     getReportIssueGitHubLink(stackTrace) {
       return `${window.BASE_URL}/reposense/RepoSense/issues/new?title=${this.getReportIssueTitle()
       }&body=${this.getReportIssueMessage(stackTrace)}`;
@@ -347,6 +171,7 @@ window.vSummary = {
       this.filterSearch = evt.target.value;
       this.getFiltered();
     },
+
     setSummaryHash() {
       const { addHash, encodeHash } = window;
 
@@ -370,6 +195,7 @@ window.vSummary = {
 
       encodeHash();
     },
+
     renderFilterHash() {
       window.decodeHash();
 
@@ -431,10 +257,11 @@ window.vSummary = {
       }
       this.$emit('get-dates', [this.minDate, this.maxDate]);
     },
+
     getFiltered() {
       this.setSummaryHash();
       this.getDates();
-      deactivateAllOverlays();
+      window.deactivateAllOverlays();
 
       // array of array, sorted by repo
       const full = [];
@@ -482,6 +309,7 @@ window.vSummary = {
         this.mergeGroup(this.filtered);
       }
     },
+
     mergeGroup(filtered) {
       filtered.forEach((group, groupIndex) => {
         const dateToIndexMap = {};
@@ -510,6 +338,7 @@ window.vSummary = {
         filtered[groupIndex].push(group[0]);
       });
     },
+
     mergeCommits(user, merged, dateToIndexMap) {
       // merge commits with the same date
       user.commits.forEach((commit) => {
@@ -537,6 +366,7 @@ window.vSummary = {
         }
       });
     },
+
     mergeFileTypeContribution(user, merged) {
       Object.entries(user.fileTypeContribution).forEach((fileType) => {
         const key = fileType[0];
@@ -548,6 +378,7 @@ window.vSummary = {
         merged[key] += value;
       });
     },
+
     processFileTypes() {
       const selectedColors = ['#ffe119', '#4363d8', '#3cb44b', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
           '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1',
@@ -572,6 +403,7 @@ window.vSummary = {
 
       this.checkedFileTypes = this.fileTypes.slice();
     },
+
     getFontColor(color) {
       // to convert color from hex code to rgb format
       const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
@@ -583,6 +415,7 @@ window.vSummary = {
 
       return luminosity < 120 ? '#ffffff' : '#000000';
     },
+
     splitCommitsWeek(user, sinceDate, untilDate) {
       const { commits } = user;
 
@@ -602,6 +435,7 @@ window.vSummary = {
       }
       user.commits = res;
     },
+
     pushCommitsWeek(sinceMs, untilMs, res, commits) {
       const diff = Math.round(Math.abs((untilMs - sinceMs) / DAY_IN_MS));
 
@@ -613,8 +447,8 @@ window.vSummary = {
         const week = {
           insertions: 0,
           deletions: 0,
-          date: getDateStr(startOfWeekMs),
-          endDate: getDateStr(endOfWeekMsWithinUntilMs),
+          date: window.getDateStr(startOfWeekMs),
+          endDate: window.getDateStr(endOfWeekMsWithinUntilMs),
           commitResults: [],
         };
 
@@ -624,6 +458,7 @@ window.vSummary = {
         }
       }
     },
+
     addLineContributionWeek(endOfWeekMs, week, commits) {
       // commits are not contiguous, meaning there are gaps of days without
       // commits, so we are going to check each commit's date and make sure
@@ -636,6 +471,7 @@ window.vSummary = {
         commit.commitResults.forEach((commitResult) => week.commitResults.push(commitResult));
       }
     },
+
     getUserCommits(user, sinceDate, untilDate) {
       user.commits = [];
       const userFirst = user.dailyCommits[0];
@@ -662,10 +498,12 @@ window.vSummary = {
 
       return null;
     },
+
     getOptionWithOrder() {
       [this.sortingOption, this.isSortingDsc] = this.sortGroupSelection.split(' ');
       [this.sortingWithinOption, this.isSortingWithinDsc] = this.sortWithinGroupSelection.split(' ');
     },
+
     sortFiltered(filtered, filterControl) {
       const { filterGroupSelection } = filterControl;
       this.getOptionWithOrder();
@@ -698,7 +536,7 @@ window.vSummary = {
       this.hasModifiedUntilDate = true;
       this.tmpFilterSinceDate = since;
       this.tmpFilterUntilDate = until;
-      deactivateAllOverlays();
+      window.deactivateAllOverlays();
     },
 
     updateTmpFilterSinceDate(event) {
@@ -739,56 +577,6 @@ window.vSummary = {
       }
     },
 
-    // triggering opening of tabs //
-    openTabAuthorship(user, repo, index) {
-      const { minDate, maxDate } = this;
-
-      this.$emit('view-authorship', {
-        minDate,
-        maxDate,
-        author: user.name,
-        repo: user.repoName,
-        name: user.displayName,
-        location: this.getRepoLink(repo[index]),
-        repoIndex: index,
-        totalCommits: user.totalCommits,
-      });
-    },
-    openTabZoomSubrange(user) {
-      // skip if accidentally clicked on ramp chart
-      if (drags.length === 2 && drags[1] - drags[0]) {
-        const tdiff = new Date(this.filterUntilDate) - new Date(this.filterSinceDate);
-        const idxs = drags.map((x) => x * tdiff / 100);
-        const tsince = getDateStr(new Date(this.filterSinceDate).getTime() + idxs[0]);
-        const tuntil = getDateStr(new Date(this.filterSinceDate).getTime() + idxs[1]);
-        this.openTabZoom(user, tsince, tuntil);
-      }
-    },
-
-    openTabZoom(user, since, until) {
-      const {
-        avgCommitSize, filterGroupSelection, filterTimeFrame, isMergeGroup, sortingOption,
-        sortingWithinOption, isSortingDsc, isSortingWithinDsc,
-      } = this;
-      const clonedUser = Object.assign({}, user); // so that changes in summary won't affect zoom
-      this.$emit('view-zoom', {
-        zRepo: user.repoName,
-        zAuthor: user.name,
-        zFilterGroup: filterGroupSelection,
-        zTimeFrame: filterTimeFrame,
-        zAvgCommitSize: avgCommitSize,
-        zUser: clonedUser,
-        zLocation: this.getRepoLink(user),
-        zSince: since,
-        zUntil: until,
-        zIsMerge: isMergeGroup,
-        zSorting: sortingOption,
-        zSortingWithin: sortingWithinOption,
-        zIsSortingDsc: isSortingDsc === 'dsc',
-        zIsSortingWithinDsc: isSortingWithinDsc === 'dsc',
-      });
-    },
-
     getFileTypeContribution(ele) {
       let validCommits = 0;
       Object.keys(ele.fileTypeContribution).forEach((fileType) => {
@@ -824,6 +612,7 @@ window.vSummary = {
       }
       return sortedRepos;
     },
+
     groupByNone(repos, sortingControl) {
       const sortedRepos = [];
       const { sortingOption, isSortingDsc } = sortingControl;
@@ -848,6 +637,7 @@ window.vSummary = {
 
       return sortedRepos;
     },
+
     groupByAuthors(repos, sortingControl) {
       const authorMap = {};
       const filtered = [];
@@ -884,22 +674,11 @@ window.vSummary = {
       return filtered;
     },
 
-    getPercentile(index) {
-      if (this.filterGroupSelection === 'groupByNone') {
-        return (Math.round((index + 1) * 1000 / this.filtered[0].length) / 10).toFixed(1);
-      }
-      return (Math.round((index + 1) * 1000 / this.filtered.length) / 10).toFixed(1);
-    },
-
     getGroupCommitsVariance(total, group) {
       if (this.filterBreakdown && this.sortingOption === 'totalCommits') {
         return total + this.getFileTypeContribution(group);
       }
       return total + group[this.sortingOption];
-    },
-
-    getGroupTotalContribution(group) {
-      return group.reduce((accContribution, user) => accContribution + user.totalCommits, 0);
     },
 
     sortingHelper(element, sortingOption) {
@@ -973,6 +752,6 @@ window.vSummary = {
     });
   },
   components: {
-    v_ramp: window.vRamp,
+    vSummaryCharts: window.vSummaryCharts,
   },
 };

frontend/src/static/js/v_summary_charts.js
@@ -1,0 +1,267 @@
+window.dismissTab = function dismissTab(node) {
+  const parent = node.parentNode;
+  parent.style.display = 'none';
+};
+
+window.comparator = (fn, sortingOption = '') => function compare(a, b) {
+  let a1;
+  let b1;
+  if (sortingOption) {
+    a1 = fn(a, sortingOption).toLowerCase
+        ? fn(a, sortingOption).toLowerCase()
+        : fn(a, sortingOption);
+    b1 = fn(b, sortingOption).toLowerCase
+        ? fn(b, sortingOption).toLowerCase()
+        : fn(b, sortingOption);
+  } else {
+    a1 = fn(a).toLowerCase ? fn(a).toLowerCase() : fn(a);
+    b1 = fn(b).toLowerCase ? fn(b).toLowerCase() : fn(b);
+  }
+  if (a1 === b1) {
+    return 0;
+  } if (a1 < b1) {
+    return -1;
+  }
+  return 1;
+};
+
+// ui funcs, only allow one ramp to be highlighted //
+let drags = [];
+
+function getBaseTarget(target) {
+  return (target.className === 'summary-chart__ramp')
+      ? target
+      : getBaseTarget(target.parentElement);
+}
+
+function deactivateAllOverlays() {
+  document.querySelectorAll('.summary-chart__ramp .overlay')
+      .forEach((x) => {
+        x.className = 'overlay';
+      });
+}
+
+function dragViewDown(evt) {
+  deactivateAllOverlays();
+
+  const pos = evt.clientX;
+  const ramp = getBaseTarget(evt.target);
+  drags = [pos];
+
+  const base = ramp.offsetWidth;
+  const offset = ramp.parentElement.offsetLeft;
+
+  const overlay = ramp.getElementsByClassName('overlay')[0];
+  overlay.style.marginLeft = '0';
+  overlay.style.width = `${(pos - offset) * 100 / base}%`;
+  overlay.className += ' edge';
+}
+
+function dragViewUp(evt) {
+  deactivateAllOverlays();
+  const ramp = getBaseTarget(evt.target);
+
+  const base = ramp.offsetWidth;
+  drags.push(evt.clientX);
+  drags.sort((a, b) => a - b);
+
+  const offset = ramp.parentElement.offsetLeft;
+  drags = drags.map((x) => (x - offset) * 100 / base);
+
+  const overlay = ramp.getElementsByClassName('overlay')[0];
+  overlay.style.marginLeft = `${drags[0]}%`;
+  overlay.style.width = `${drags[1] - drags[0]}%`;
+  overlay.className += ' show';
+}
+
+window.viewClick = function viewClick(evt) {
+  const isKeyPressed = this.isMacintosh ? evt.metaKey : evt.ctrlKey;
+  if (drags.length === 2) {
+    drags = [];
+  }
+
+  if (isKeyPressed) {
+    return drags.length === 0
+        ? dragViewDown(evt)
+        : dragViewUp(evt);
+  }
+
+  return null;
+};
+
+window.vSummaryCharts = {
+  props: ['checkedFileTypes', 'filtered', 'fileTypeColors', 'avgContributionSize', 'filterBreakdown',
+      'filterGroupSelection', 'filterTimeFrame', 'filterSinceDate', 'filterUntilDate', 'isMergeGroup',
+      'minDate', 'maxDate'],
+  template: window.$('v_summary_charts').innerHTML,
+  computed: {
+    avgCommitSize() {
+      let totalCommits = 0;
+      let totalCount = 0;
+      this.filteredRepos.forEach((repo) => {
+        repo.forEach((user) => {
+          user.commits.forEach((slice) => {
+            if (slice.insertions > 0) {
+              totalCount += 1;
+              totalCommits += slice.insertions;
+            }
+          });
+        });
+      });
+      return totalCommits / totalCount;
+    },
+
+    filteredRepos() {
+      return this.filtered.filter((repo) => repo.length > 0);
+    },
+  },
+  methods: {
+    getFileTypeContributionBars(fileTypeContribution) {
+      let currentBarWidth = 0;
+      const fullBarWidth = 100;
+      const contributionPerFullBar = (this.avgContributionSize * 2);
+      const allFileTypesContributionBars = {};
+
+      Object.keys(fileTypeContribution)
+          .filter((fileType) => this.checkedFileTypes.includes(fileType))
+          .forEach((fileType) => {
+            const contribution = fileTypeContribution[fileType];
+            let barWidth = (contribution / contributionPerFullBar) * fullBarWidth;
+            const contributionBars = [];
+
+            // if contribution bar for file type is able to fit on the current line
+            if (currentBarWidth + barWidth < fullBarWidth) {
+              contributionBars.push(barWidth);
+              currentBarWidth += barWidth;
+            } else {
+              // take up all the space left on the current line
+              contributionBars.push(fullBarWidth - currentBarWidth);
+              barWidth -= fullBarWidth - currentBarWidth;
+              // additional bar width will start on a new line
+              const numOfFullBars = Math.floor(barWidth / fullBarWidth);
+              for (let i = 0; i < numOfFullBars; i += 1) {
+                contributionBars.push(fullBarWidth);
+              }
+              const remainingBarWidth = barWidth % fullBarWidth;
+              if (remainingBarWidth !== 0) {
+                contributionBars.push(remainingBarWidth);
+              }
+              currentBarWidth = remainingBarWidth;
+            }
+
+            allFileTypesContributionBars[fileType] = contributionBars;
+          });
+
+      return allFileTypesContributionBars;
+    },
+
+    getFileTypes(repo) {
+      const fileTypes = [];
+      repo.forEach((user) => {
+        Object.keys(user.fileTypeContribution).forEach((fileType) => {
+          if (this.checkedFileTypes.includes(fileType) && !fileTypes.includes(fileType)) {
+            fileTypes.push(fileType);
+          }
+        });
+      });
+      return fileTypes;
+    },
+
+    getContributionBars(totalContribution) {
+      const res = [];
+      const contributionLimit = (this.avgContributionSize * 2);
+
+      const cnt = parseInt(totalContribution / contributionLimit, 10);
+      for (let cntId = 0; cntId < cnt; cntId += 1) {
+        res.push(100);
+      }
+
+      const last = (totalContribution % contributionLimit) / contributionLimit;
+      if (last !== 0) {
+        res.push(last * 100);
+      }
+
+      return res;
+    },
+
+    getAuthorProfileLink(userName) {
+      return `https://github.com/${userName}`;
+    },
+
+    getRepoLink(repo) {
+      const { REPOS } = window;
+      const { location, branch } = REPOS[repo.repoId];
+
+      if (Object.prototype.hasOwnProperty.call(location, 'organization')) {
+        return `${window.BASE_URL}/${location.organization}/${location.repoName}/tree/${branch}`;
+      }
+
+      return repo.location;
+    },
+
+    // triggering opening of tabs //
+    openTabAuthorship(user, repo, index) {
+      const { minDate, maxDate } = this;
+
+      this.$parent.$emit('view-authorship', {
+        minDate,
+        maxDate,
+        author: user.name,
+        repo: user.repoName,
+        name: user.displayName,
+        location: this.getRepoLink(repo[index]),
+        repoIndex: index,
+        totalCommits: user.totalCommits,
+      });
+    },
+
+    openTabZoomSubrange(user) {
+      // skip if accidentally clicked on ramp chart
+      if (drags.length === 2 && drags[1] - drags[0]) {
+        const tdiff = new Date(this.filterUntilDate) - new Date(this.filterSinceDate);
+        const idxs = drags.map((x) => x * tdiff / 100);
+        const tsince = window.getDateStr(new Date(this.filterSinceDate).getTime() + idxs[0]);
+        const tuntil = window.getDateStr(new Date(this.filterSinceDate).getTime() + idxs[1]);
+        this.openTabZoom(user, tsince, tuntil);
+      }
+    },
+
+    openTabZoom(user, since, until) {
+      const {
+        avgCommitSize, filterGroupSelection, filterTimeFrame, isMergeGroup, sortingOption,
+        sortingWithinOption, isSortingDsc, isSortingWithinDsc,
+      } = this;
+      const clonedUser = Object.assign({}, user); // so that changes in summary won't affect zoom
+      this.$parent.$emit('view-zoom', {
+        zRepo: user.repoName,
+        zAuthor: user.name,
+        zFilterGroup: filterGroupSelection,
+        zTimeFrame: filterTimeFrame,
+        zAvgCommitSize: avgCommitSize,
+        zUser: clonedUser,
+        zLocation: this.getRepoLink(user),
+        zSince: since,
+        zUntil: until,
+        zIsMerge: isMergeGroup,
+        zSorting: sortingOption,
+        zSortingWithin: sortingWithinOption,
+        zIsSortingDsc: isSortingDsc === 'dsc',
+        zIsSortingWithinDsc: isSortingWithinDsc === 'dsc',
+      });
+    },
+
+    getPercentile(index) {
+      if (this.filterGroupSelection === 'groupByNone') {
+        return (Math.round((index + 1) * 1000 / this.filtered[0].length) / 10).toFixed(1);
+      }
+      return (Math.round((index + 1) * 1000 / this.filtered.length) / 10).toFixed(1);
+    },
+
+    getGroupTotalContribution(group) {
+      return group.reduce((accContribution, user) => accContribution + user.totalCommits, 0);
+    },
+  },
+  components: {
+    vRamp: window.vRamp,
+  },
+};

frontend/src/static/js/v_zoom.js
@@ -123,6 +123,6 @@ window.vZoom = {
     this.updateExpandedCommitMessagesCount();
   },
   components: {
-    v_ramp: window.vRamp,
+    vRamp: window.vRamp,
   },
 };

commit 831c36581718d873342d813421b4da9b2e4f0297

[#1213] Regressions from #1059 (#1218)
With the recent merged PR #1059, the data related to sorting were not
passed from v_summary to v_summary_charts, even though
v_summary_charts uses those data within openTabZoom method.

This results in zoom tab to not be restorable at some situations, as it 
is unable to utilise the sorting options. However, the sorting options
and the call for sortFiltered is actually not needed
within the restoreZoomFiltered method. This is because the
matchZoomUser within restoreZoomFiltered will already filter out
other groups/repos. Thus, there is no need to pass those props into
v_summary_charts.

Let's remove the unnecessary parameters and methods.

frontend/src/static/js/main.js
@@ -210,10 +210,6 @@ window.app = new window.Vue({
       window.removeHash('zFGS');
       window.removeHash('zFTF');
       window.removeHash('zMG');
-      window.removeHash('zSO');
-      window.removeHash('zSWO');
-      window.removeHash('zSD');
-      window.removeHash('zSWD');
     },
 
     updateTabAuthorship(obj) {
@@ -260,10 +256,6 @@ window.app = new window.Vue({
         zFilterGroup: hash.zFGS,
         zTimeFrame: hash.zFTF,
         zIsMerge: hash.zMG === 'true',
-        zSorting: hash.zSO,
-        zSortingWithin: hash.zSWO,
-        zIsSortingDsc: hash.zSD === 'true',
-        zIsSortingWithinDsc: hash.zSWD === 'true',
       };
       const tabInfoLength = Object.values(zoomInfo).filter((x) => x !== null).length;
       if (Object.keys(zoomInfo).length === tabInfoLength) {

frontend/src/static/js/v_summary.js
@@ -803,10 +803,9 @@ window.vSummary = {
 
     restoreZoomFiltered(info) {
       const {
-        zSince, zUntil, zFilterGroup, zTimeFrame, zIsMerge, zSorting, zSortingWithin,
-        zIsSortingDsc, zIsSortingWithinDsc,
+        zSince, zUntil, zTimeFrame, zIsMerge,
       } = info;
-      let filtered = [];
+      const filtered = [];
 
       const groups = JSON.parse(JSON.stringify(this.repos));
 
@@ -827,15 +826,6 @@ window.vSummary = {
           filtered.push(res);
         }
       });
-
-      const filterControl = {
-        filterGroupSelection: zFilterGroup,
-        sortingOption: zSorting,
-        sortingWithinOption: zSortingWithin,
-        isSortingDsc: zIsSortingDsc,
-        isSortingWithinDsc: zIsSortingWithinDsc,
-      };
-      filtered = this.sortFiltered(filtered, filterControl);
 
       if (zIsMerge) {
         this.mergeGroup(filtered);

frontend/src/static/js/v_summary_charts.js
@@ -229,8 +229,7 @@ window.vSummaryCharts = {
 
     openTabZoom(user, since, until) {
       const {
-        avgCommitSize, filterGroupSelection, filterTimeFrame, isMergeGroup, sortingOption,
-        sortingWithinOption, isSortingDsc, isSortingWithinDsc,
+        avgCommitSize, filterGroupSelection, filterTimeFrame, isMergeGroup,
       } = this;
       const clonedUser = Object.assign({}, user); // so that changes in summary won't affect zoom
       this.$parent.$emit('view-zoom', {
@@ -244,10 +243,6 @@ window.vSummaryCharts = {
         zSince: since,
         zUntil: until,
         zIsMerge: isMergeGroup,
-        zSorting: sortingOption,
-        zSortingWithin: sortingWithinOption,
-        zIsSortingDsc: isSortingDsc === 'dsc',
-        zIsSortingWithinDsc: isSortingWithinDsc === 'dsc',
       });
     },
 

frontend/src/static/js/v_zoom.js
@@ -78,8 +78,8 @@ window.vZoom = {
     setInfoHash() {
       const { addHash, encodeHash } = window;
       const {
-        zAvgCommitSize, zSince, zUntil, zFilterGroup, zTimeFrame, zIsMerge, zSorting,
-        zSortingWithin, zIsSortingDsc, zIsSortingWithinDsc, zAuthor, zRepo,
+        zAvgCommitSize, zSince, zUntil, zFilterGroup,
+        zTimeFrame, zIsMerge, zAuthor, zRepo,
       } = this.info;
 
       addHash('zA', zAuthor);
@@ -90,10 +90,6 @@ window.vZoom = {
       addHash('zMG', zIsMerge);
       addHash('zFTF', zTimeFrame);
       addHash('zFGS', zFilterGroup);
-      addHash('zSO', zSorting);
-      addHash('zSWO', zSortingWithin);
-      addHash('zSD', zIsSortingDsc);
-      addHash('zSWD', zIsSortingWithinDsc);
       encodeHash();
     },
 