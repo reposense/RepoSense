838ef11f5e41638a07f26338e2e47ac0b789bf4b
James Pang <jamespangmunwai@gmail.com>
2020-05-12 22:58:54 +0800

    [#1220] Add front end tests for summary chart functionalities (#1228)
    Our coverage for front end tests is rather low. UI regressions will be
    harder to discover due to this.
    
    Let's add front end tests for summary chart's functionalities such as 
    merge group, zoom, filter breakdown features and the restoring of 
    summary controls after page reloads.

---
+++ frontend/cypress/tests/chartView/chartView_reload.js
@@ -0,0 +1,75 @@
+describe('reload page', () => {
+  it('reload page should restore all controls', () => {
+    Cypress.wait();
+
+    // search
+    cy.get('div.mui-textfield.search_box > input:visible')
+        .should('be.visible')
+        .type('eugene');
+
+    // group by
+    cy.get('div.mui-select.grouping > select:visible')
+        .select('groupByAuthors');
+
+    // sort groups by
+    cy.get('div.mui-select.sort-group > select:visible')
+        .select('↓ contribution');
+
+    // sort within groups by
+    cy.get('div.mui-select.sort-within-group > select:visible')
+        .select('↓ contribution');
+
+    // granularity
+    cy.get('div.mui-select.granularity > select:visible')
+        .select('week');
+
+    // since date
+    cy.get('input[name="since"]:visible')
+        .type('2018-06-10');
+
+    // until date
+    cy.get('input[name="until"]:visible')
+        .type('2019-06-10');
+
+    // break down by file type
+    cy.get('#summary label.filter-breakdown > input:visible')
+        .should('be.visible')
+        .check()
+        .should('be.checked');
+
+    // merge group
+    cy.get('#summary label.merge-group > input:visible')
+        .should('be.visible')
+        .check()
+        .should('be.checked');
+
+    cy.reload();
+
+    cy.get('div.mui-textfield.search_box > input:visible')
+        .should('have.value', 'eugene');
+
+    cy.get('div.mui-select.grouping > select:visible')
+        .should('have.value', 'groupByAuthors');
+
+    cy.get('div.mui-select.sort-group > select:visible')
+        .should('have.value', 'totalCommits dsc');
+
+    cy.get('div.mui-select.sort-within-group > select:visible')
+        .should('have.value', 'totalCommits dsc');
+
+    cy.get('div.mui-select.granularity > select:visible')
+        .should('have.value', 'week');
+
+    cy.get('input[name="since"]:visible')
+        .should('have.value', '2018-06-10');
+
+    cy.get('input[name="until"]:visible')
+        .should('have.value', '2019-06-10');
+
+    cy.get('#summary label.filter-breakdown > input:visible')
+        .should('be.checked');
+
+    cy.get('#summary label.merge-group > input:visible')
+        .should('be.checked');
+  });
+});
