dd8a13f92a598c9af9c84a9b602b91c390b47a3d
anubh-v <anubhav@u.nus.edu>
2020-05-18 10:08:04 +0800

    Remove unused import

--- src/main/java/reposense/parser/AuthorConfigCsvParser.java
+++ src/main/java/reposense/parser/AuthorConfigCsvParser.java
@@ -3,7 +3,6 @@ package reposense.parser;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.csv.CSVRecord;
