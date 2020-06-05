9924913615f12d898d4732d568504d0c46ce1cc8
Ni Tianzhen <nitianzhen1210@gmail.com>
2020-06-05 21:23:57 +0800

    Guard against large numbers

--- src/main/java/reposense/parser/PeriodArgumentType.java
+++ src/main/java/reposense/parser/PeriodArgumentType.java
@@ -17,6 +17,8 @@ public class PeriodArgumentType implements ArgumentType<Optional<Integer>> {
             + "where n is a number greater than 0.";
     private static final String PARSE_EXCEPTION_MESSAGE_SMALLER_THAN_ZERO =
             "Invalid format. Period must be greater than 0.";
+    private static final String PARSE_EXCEPTION_MESSAGE_NUMBER_TOO_LARGE =
+            "Invalid format. Input number may be too large.";
     private static final Pattern PERIOD_PATTERN = Pattern.compile("[0-9]+[dw]");
 
     @Override
@@ -27,12 +29,17 @@ public class PeriodArgumentType implements ArgumentType<Optional<Integer>> {
         }
 
         int multiplier = value.substring(value.length() - 1).equals("d") ? 1 : 7;
-        int convertedValue = Integer.parseInt(value.substring(0, value.length() - 1)) * multiplier;
-        if (convertedValue <= 0) {
-            throw new ArgumentParserException(
-                    String.format(PARSE_EXCEPTION_MESSAGE_SMALLER_THAN_ZERO, value), parser);
-        }
+        try {
+            int convertedValue = Integer.parseInt(value.substring(0, value.length() - 1)) * multiplier;
+            if (convertedValue <= 0) {
+                throw new ArgumentParserException(
+                        String.format(PARSE_EXCEPTION_MESSAGE_SMALLER_THAN_ZERO, value), parser);
+            }
 
-        return Optional.of(convertedValue);
+            return Optional.of(convertedValue);
+        } catch (NumberFormatException e) {
+            throw new ArgumentParserException(
+                    String.format(PARSE_EXCEPTION_MESSAGE_NUMBER_TOO_LARGE, value), parser);
+        }
     }
 }
