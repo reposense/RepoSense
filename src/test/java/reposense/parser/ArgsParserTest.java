d02d73514e957569c88f16af1548c8909dc0151a
Ni Tianzhen <nitianzhen1210@hotmail.com>
2020-08-02 10:03:29 +0800

    [#1279] Provide a way to run analysis for a moving window (#1280)
    Some users might want to monitor repositories for a moving window rather
    than from a specific date.
    
    We can give a CLI option that runs the analysis for a period of specific 
    length e.g., `--period 7d` runs the analysis for the last 7 days, and
    `--period 1w` runs the analysis for the last 1 week.
    
    Period can combine with either since date or until date, but not both.
    If both not supplied, it will be the period before the current date.

--- src/test/java/reposense/parser/ArgsParserTest.java
+++ src/test/java/reposense/parser/ArgsParserTest.java
@@ -361,6 +361,30 @@ public class ArgsParserTest {
     }
 
     @Test
+    public void period_inDaysWithSinceDate_success() throws Exception {
+        String input = DEFAULT_INPUT_BUILDER
+                .addSinceDate("01/07/2017")
+                .addPeriod("2d")
+                .build();
+        CliArguments cliArguments = ArgsParser.parse(translateCommandline(input));
+        Assert.assertTrue(cliArguments instanceof ConfigCliArguments);
+        Date expectedUntilDate = TestUtil.getUntilDate(2017, Calendar.JULY, 2);
+        Assert.assertEquals(expectedUntilDate, cliArguments.getUntilDate());
+    }
+
+    @Test
+    public void period_inWeeksWithUntilDate_success() throws Exception {
+        String input = DEFAULT_INPUT_BUILDER
+                .addUntilDate("14/07/2017")
+                .addPeriod("2w")
+                .build();
+        CliArguments cliArguments = ArgsParser.parse(translateCommandline(input));
+        Assert.assertTrue(cliArguments instanceof ConfigCliArguments);
+        Date expectedSinceDate = TestUtil.getSinceDate(2017, Calendar.JULY, 1);
+        Assert.assertEquals(expectedSinceDate, cliArguments.getSinceDate());
+    }
+
+    @Test
     public void formats_inAlphanumeric_success() throws Exception {
         String input = DEFAULT_INPUT_BUILDER.addFormats("java js css 7z").build();
         CliArguments cliArguments = ArgsParser.parse(translateCommandline(input));
@@ -530,6 +554,27 @@ public class ArgsParserTest {
         String input = DEFAULT_INPUT_BUILDER.addSinceDate("01/12/2017")
                 .addUntilDate("30/11/2017")
                 .build();
+        ArgsParser.parse(translateCommandline(input));
+    }
+
+    @Test(expected = ParseException.class)
+    public void period_withBothSinceDateAndUntilDate_throwsParseException() throws Exception {
+        String input = DEFAULT_INPUT_BUILDER.addPeriod("18d")
+                .addSinceDate("30/11/2017")
+                .addUntilDate("01/12/2017")
+                .build();
+        ArgsParser.parse(translateCommandline(input));
+    }
+
+    @Test(expected = ParseException.class)
+    public void period_notNumeric_throwsParseExcpetion() throws Exception {
+        String input = DEFAULT_INPUT_BUILDER.addPeriod("abcd").build();
+        ArgsParser.parse(translateCommandline(input));
+    }
+
+    @Test(expected = ParseException.class)
+    public void period_isZero_throwsParseExcpetion() throws Exception {
+        String input = DEFAULT_INPUT_BUILDER.addPeriod("0w").build();
         ArgsParser.parse(translateCommandline(input));
     }
 
