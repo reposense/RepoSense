e790c7c965355a12b76304f948b3c427b7254f98
tejas bhuwania <tejasbhuwania2805@gmail.com>
2020-09-28 14:27:04 +0800

    fix indentation

--- frontend/cypress/tests/codeView/codeView_checkFileTypes.js
+++ frontend/cypress/tests/codeView/codeView_checkFileTypes.js
@@ -1,67 +1,67 @@
 describe('check file types', () => {
-    it('check if all files types are visible by default', () => {
-        Cypress.wait();
+  it('check if all files types are visible by default', () => {
+    Cypress.wait();
 
-        //open the code panel
-        cy.get('.icon-button.fa-code')
-            .should('be.visible')
-            .first()
-            .click();
+    //open the code panel
+    cy.get('.icon-button.fa-code')
+      .should('be.visible')
+      .first()
+      .click();
 
-        cy.get('#tab-authorship .files', { timeout: 90000 })
-            .should('be.visible');
+    cy.get('#tab-authorship .files', {timeout: 90000})
+      .should('be.visible');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
-            .should('be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
+      .should('be.checked');
 
-    });
+  });
 
-    it('uncheck all files types should show no files', () => {
-        Cypress.wait();
+  it('uncheck all files types should show no files', () => {
+    Cypress.wait();
 
-        //open the code panel
-        cy.get('.icon-button.fa-code')
-            .should('be.visible')
-            .first()
-            .click();
+    //open the code panel
+    cy.get('.icon-button.fa-code')
+      .should('be.visible')
+      .first()
+      .click();
 
-        cy.get('#tab-authorship .files', { timeout: 90000 })
-            .should('be.visible');
+    cy.get('#tab-authorship .files', {timeout: 90000})
+      .should('be.visible');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
-            .uncheck()
-            .should('not.be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
+      .uncheck()
+      .should('not.be.checked');
 
-        cy.get('#tab-authorship .files')
-            .should('not.be.visible');
+    cy.get('#tab-authorship .files')
+      .should('not.be.visible');
 
-    });
+  });
 
-    it('uncheck file type should uncheck all option and not show legend', () => {
-        Cypress.wait();
+  it('uncheck file type should uncheck all option and not show legend', () => {
+    Cypress.wait();
 
-        //open the code panel
-        cy.get('.icon-button.fa-code')
-            .should('be.visible')
-            .first()
-            .click();
+    //open the code panel
+    cy.get('.icon-button.fa-code')
+      .should('be.visible')
+      .first()
+      .click();
 
-        cy.get('#tab-authorship .files', { timeout: 90000 })
-            .should('be.visible');
+    cy.get('#tab-authorship .files', {timeout: 90000})
+      .should('be.visible');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="java"]')
-            .uncheck()
-            .should('not.be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="java"]')
+      .uncheck()
+      .should('not.be.checked');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
-            .should('not.be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
+      .should('not.be.checked');
 
-        cy.get('.file > .title > .fileTypeLabel')
-            .should('not.contain.text', 'java');
+    cy.get('.file > .title > .fileTypeLabel')
+      .should('not.contain.text', 'java');
 
-        cy.get('.file > .title > .fileTypeLabel')
-            .should('contain.text', 'gradle');
+    cy.get('.file > .title > .fileTypeLabel')
+      .should('contain.text', 'gradle');
 
-    });
+  });
 
 });
