798ca0990a35aa35455c674db7acc8758e522a45
tejas bhuwania <tejasbhuwania2805@gmail.com>
2020-09-28 18:48:06 +0800

    see indentation working

--- frontend/cypress/tests/codeView/codeView_hideFileDetails.js
+++ frontend/cypress/tests/codeView/codeView_hideFileDetails.js
@@ -4,101 +4,101 @@ describe('hide all file details', () => {
 
     // open the code panel
     cy.get('.icon-button.fa-code')
-      .should('be.visible')
-      .first()
-      .click();
+        .should('be.visible')
+        .first()
+        .click();
 
     cy.get('#tab-authorship .files', {timeout: 90000})
-      .should('be.visible');
+        .should('be.visible');
 
     // the contents of all the files should be visible
     cy.get('#tab-authorship .file-content ')
-      .should('be.visible');
+        .should('be.visible');
 
     // hide the details of all the files
     cy.get('#tab-authorship .toolbar--multiline')
-      .should('be.visible')
-      .click();
+        .should('be.visible')
+        .click();
 
     cy.get('#tab-authorship .files')
-      .should('be.visible');
+        .should('be.visible');
 
     // the contents of all the files should be hidden
     cy.get('#tab-authorship .file-content ')
-      .should('not.be.visible');
+        .should('not.be.visible');
   });
 
   it('check details of one file are shown, rest are hidden', () => {
     Cypress.wait();
 
     cy.get('.icon-button.fa-code')
-      .should('be.visible')
-      .first()
-      .click();
+        .should('be.visible')
+        .first()
+        .click();
 
     cy.get('#tab-authorship .files', {timeout: 90000})
-      .should('be.visible');
+        .should('be.visible');
 
     cy.get('#tab-authorship .toolbar--multiline')
-      .should('be.visible')
-      .click();
+        .should('be.visible')
+        .click();
 
     // should show 'show all file details' only
     cy.get('#tab-authorship .toolbar--multiline a')
-      .should('not.contain.text', 'hide all file details')
-      .should('contain.text', 'show all file details');
+        .should('not.contain.text', 'hide all file details')
+        .should('contain.text', 'show all file details');
 
     // open contents of the first file
     cy.get('#tab-authorship .title .path')
-      .should('be.visible')
-      .first()
-      .click();
+        .should('be.visible')
+        .first()
+        .click();
 
     // should show both 'show/hide all file details'
     cy.get('#tab-authorship .toolbar--multiline a')
-      .should('contain.text', 'hide all file details')
-      .should('contain.text', 'show all file details');
+        .should('contain.text', 'hide all file details')
+        .should('contain.text', 'show all file details');
 
     // contents of the first file should be visible
     cy.get('#tab-authorship .file-content ')
-      .first()
-      .should('be.visible');
+        .first()
+        .should('be.visible');
 
     // contents of the last file should be hidden
     cy.get('#tab-authorship .file-content ')
-      .last()
-      .should('not.be.visible');
+        .last()
+        .should('not.be.visible');
   });
 
   it('check show all file details shows the content of all the files', () => {
     Cypress.wait();
 
     cy.get('.icon-button.fa-code')
-      .should('be.visible')
-      .first()
-      .click();
+        .should('be.visible')
+        .first()
+        .click();
 
     cy.get('#tab-authorship .files', {timeout: 90000})
-      .should('be.visible');
+        .should('be.visible');
 
     cy.get('#tab-authorship .file-content ')
-      .should('be.visible');
+        .should('be.visible');
 
     // hide the content of all the files
     cy.get('#tab-authorship .toolbar--multiline')
-      .should('be.visible')
-      .click();
+        .should('be.visible')
+        .click();
 
     cy.get('#tab-authorship .file-content ')
-      .should('not.be.visible');
+        .should('not.be.visible');
 
     // show the content of all the files
     cy.get('#tab-authorship .toolbar--multiline')
-      .should('be.visible')
-      .click();
+        .should('be.visible')
+        .click();
 
     cy.get('#tab-authorship .file-content ')
-      .should('be.visible');
+        .should('be.visible');
 
   });
 });
