b2550a02164e3056bee12b118d6ad7e90e44ccbe
tejas bhuwania <tejasbhuwania2805@gmail.com>
2020-10-01 16:07:13 +0800

    fix indentation

--- frontend/cypress/tests/codeView/codeView_reload.js
+++ frontend/cypress/tests/codeView/codeView_reload.js
@@ -1,56 +1,56 @@
 describe('reload page', () => {
-    it('reload page should restore all controls', () => {
-        Cypress.wait();
+  it('reload page should restore all controls', () => {
+    Cypress.wait();
 
-        //open the code panel
-        cy.get('.icon-button.fa-code')
-            .should('be.visible')
-            .first()
-            .click();
+    //open the code panel
+    cy.get('.icon-button.fa-code')
+        .should('be.visible')
+        .first()
+        .click();
 
-        //sort by
-        cy.get('#tab-authorship > .title > .contribution > .sorting > .sort-by > select')
-            .select('LoC');
+    //sort by
+    cy.get('#tab-authorship > .title > .contribution > .sorting > .sort-by > select')
+        .select('LoC');
 
-        //sort order
-        cy.get('#tab-authorship > .title > .contribution > .sorting > .sort-order > select')
-            .select('Descending');
+    //sort order
+    cy.get('#tab-authorship > .title > .contribution > .sorting > .sort-order > select')
+        .select('Descending');
 
-        //select radio-button
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes > .radio-button--checkbox')
-            .should('be.visible')
-            .check()
-            .should('be.checked');
+    //select radio-button
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes > .radio-button--checkbox')
+        .should('be.visible')
+        .check()
+        .should('be.checked');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
-            .should('be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
+        .should('be.checked');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="gradle"]')
-            .uncheck()
-            .should('not.be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="gradle"]')
+        .uncheck()
+        .should('not.be.checked');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="pug"]')
-            .should('be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="pug"]')
+        .should('be.checked');
 
-        cy.reload();
+    cy.reload();
 
-        cy.get('#tab-authorship > .title > .contribution > .sorting > .sort-by > select')
-            .should('have.value', 'lineOfCode');
+    cy.get('#tab-authorship > .title > .contribution > .sorting > .sort-by > select')
+        .should('have.value', 'lineOfCode');
 
-        cy.get('#tab-authorship > .title > .contribution > .sorting > .sort-order > select')
-            .should('have.value','true');
+    cy.get('#tab-authorship > .title > .contribution > .sorting > .sort-order > select')
+        .should('have.value', 'true');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes > .radio-button--checkbox')
-            .should('be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes > .radio-button--checkbox')
+        .should('be.checked');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
-            .should('not.be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
+        .should('not.be.checked');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="gradle"]')
-            .should('not.be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="gradle"]')
+        .should('not.be.checked');
 
-        cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="pug"]')
-            .should('be.checked');
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="pug"]')
+        .should('be.checked');
 
-    });
+  });
 });
