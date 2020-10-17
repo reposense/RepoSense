d788bc44a0f39c6ec2eb0cd506f2370b8edbcdd0
Boon Jun <boon-jun1996@hotmail.com>
2020-10-17 16:56:18 +0800

    Correct indentation issues

--- frontend/cypress/tests/zoomView/zoomView_selectFileTypes.js
+++ frontend/cypress/tests/zoomView/zoomView_selectFileTypes.js
@@ -3,58 +3,58 @@ describe('check file types ', () => {
     Cypress.wait();
 
     cy.get('.icon-button.fa-list-ul')
-      .should('be.visible')
-      .first()
-      .click();
+        .should('be.visible')
+        .first()
+        .click();
 
-    cy.get('#tab-zoom .zoom__day', {timeout: 90000})
-      .should('exist');
+    cy.get('#tab-zoom .zoom__day', { timeout: 90000 })
+        .should('exist');
 
     cy.get('#tab-zoom .fileTypes input[value="all"]')
-      .should('be.checked');
+        .should('be.checked');
   });
 
   it('uncheck all file types should show no files', () => {
     Cypress.wait();
 
     cy.get('.icon-button.fa-list-ul')
-      .should('be.visible')
-      .first()
-      .click();
+        .should('be.visible')
+        .first()
+        .click();
 
     cy.get('#tab-zoom .zoom__day')
-      .should('exist');
+        .should('exist');
 
     cy.get('#tab-zoom .fileTypes input[value="all"]')
-      .uncheck();
+        .uncheck();
 
     cy.get('#tab-zoom .zoom__day')
-      .should('not.exist');
+        .should('not.exist');
   });
 
   it('uncheck file type should uncheck all option and not show legend', () => {
     Cypress.wait();
 
     cy.get('.icon-button.fa-list-ul')
-      .should('be.visible')
-      .first()
-      .click();
+        .should('be.visible')
+        .first()
+        .click();
 
     cy.get('#tab-zoom .fileTypes input[value="java"]')
-      .uncheck()
-      .should('not.be.checked');
+        .uncheck()
+        .should('not.be.checked');
 
     cy.get('#tab-zoom .fileTypes input[value="js"]')
-      .uncheck()
-      .should('not.be.checked');
+        .uncheck()
+        .should('not.be.checked');
 
     cy.get('#tab-zoom .fileTypes input[value="ball"]')
-      .should('not.be.checked');
+        .should('not.be.checked');
 
     cy.get('.zoom__day > .commit-message > .message-title > .fileTypeLabel')
-      .should('not.contain.text', 'java');
+        .should('not.contain.text', 'java');
 
     cy.get('.zoom__day > .commit-message > .message-title > .fileTypeLabel')
-      .should('not.contain.text', 'gradle');
+        .should('not.contain.text', 'gradle');
   });
 });
