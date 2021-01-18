f48e87fd91db82a8dca215731e16f0379c99ebf6
Ger Hean <gerhean@live.com>
2021-01-18 18:33:04 +0800

    remove redundant setup file

--- vue.config.js
+++ vue.config.js
@@ -18,4 +18,13 @@ module.exports = {
         }),
     ],
   },
+  chainWebpack: (config) => {
+    // Pug Loader
+    config.module
+        .rule('pug')
+        .test(/\.pug$/)
+        .use('pug-plain-loader')
+        .loader('pug-plain-loader')
+        .end();
+  },
 };
