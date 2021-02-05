21ef8ffdc0075a21011687ab2fc04570043809fa
rolandyuwy <rolandyuwenyang@hotmail.com>
2021-02-05 23:41:15 +0800

    Complete switch authorship tab tests

--- frontend/cypress/tests/codeView/codeView_switchAuthorship.js
+++ frontend/cypress/tests/codeView/codeView_switchAuthorship.js
@@ -1,5 +1,5 @@
 describe('switch authorship', () => {
-  it('switch authorship view should have all file types checked', () => {
+  it('switch authorship view should restore all default controls', () => {
     Cypress.wait();
 
     // open the code panel
@@ -8,16 +8,31 @@ describe('switch authorship', () => {
         .first()
         .click();
 
-    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
-        .should('be.checked');
-
     // switch authorship view
     cy.get('.icon-button.fa-code')
         .should('be.visible')
         .last()
         .click();
 
+    cy.get('#tab-authorship > .title > .contribution > .sorting > .sort-by > select')
+        .should('have.value', 'lineOfCode');
+
+    cy.get('#tab-authorship > .title > .contribution > .sorting > .sort-order > select')
+        .should('have.value', 'true');
+
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes > .radio-button--checkbox')
+        .should('be.checked');
+
     cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="all"]')
+        .should('be.checked');
+
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="gradle"]')
+        .should('be.checked');
+
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="java"]')
+        .should('be.checked');
+
+    cy.get('#tab-authorship > .title > .contribution > .fileTypes input[id="yml"]')
         .should('be.checked');
   });
 });
