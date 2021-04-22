reposense/RepoSense

commit 5f0dff261de89c7c056e54b5ef7443e773bfa73e

v_zoom: rewrite refresh logic (#1419)
The zoom tab uses `this.$root.$emit` to obtain information from 
`v_summary` during a page refresh.

However, we are upgrading to Vue3 to projectify frontend and Vue3 
does not support this.

Let's update the way zoom tab loads data on page refresh to be 
compatible with Vue3.

frontend/src/static/js/v_zoom.js
@@ -118,8 +118,10 @@ window.vZoom = {
 
   methods: {
     initiate() {
-      if (!this.info.zUser) { // restoring zoom tab from reloaded page
-        this.restoreZoomTab();
+      if (this.info.zUser) {
+        // This code should always run since zUser must be defined
+        this.updateFileTypes();
+        this.selectedFileTypes = this.fileTypes.slice();
       }
 
       this.updateFileTypes();
@@ -143,11 +145,6 @@ window.vZoom = {
       if (el) {
         el.focus();
       }
-    },
-
-    restoreZoomTab() {
-      // restore selected user's commits and file type colors from v_summary
-      this.$root.$emit('restoreCommits', this.info);
     },
 
     updateFileTypes() {

frontend/src/static/js/main.js
@@ -37,6 +37,9 @@ window.app = new window.Vue({
   },
   watch: {
     '$store.state.tabZoomInfo': function () {
+      if (this.$store.state.tabZoomInfo.isRefreshing) {
+        return;
+      }
       this.activateTab('zoom');
     },
     '$store.state.tabAuthorshipInfo': function () {
@@ -81,10 +84,10 @@ window.app = new window.Vue({
         await Promise.all(names.map((name) => (
           window.api.loadCommits(name)
         )));
-        this.userUpdated = true;
         this.loadingOverlayOpacity = 0.5;
         this.getUsers();
         this.renderTabHash();
+        this.userUpdated = true;
       } catch (error) {
         window.alert(error);
       } finally {
@@ -143,6 +146,7 @@ window.app = new window.Vue({
     renderZoomTabHash() {
       const hash = window.hashParams;
       const zoomInfo = {
+        isRefreshing: true,
         zAuthor: hash.zA,
         zRepo: hash.zR,
         zAvgCommitSize: hash.zACS,

frontend/src/static/js/v_summary.js
@@ -699,7 +699,10 @@ window.vSummary = {
       if (zIsMerge) {
         this.mergeGroupByIndex(filtered, 0);
       }
-      return filtered[0][0];
+      [[info.zUser]] = filtered;
+      info.zFileTypeColors = this.fileTypeColors;
+      info.isRefreshing = false;
+      this.$store.commit('updateTabZoomInfo', info);
     },
     matchZoomUser(info, user) {
       const {
@@ -731,13 +734,10 @@ window.vSummary = {
     this.processFileTypes();
     this.renderFilterHash();
     this.getFiltered();
-  },
-  beforeMount() {
-    this.$root.$on('restoreCommits', (info) => {
-      const zoomFilteredUser = this.restoreZoomFiltered(info);
-      info.zUser = zoomFilteredUser;
-      info.zFileTypeColors = this.fileTypeColors;
-    });
+    if (this.$store.state.tabZoomInfo.isRefreshing) {
+      const zoomInfo = Object.assign({}, this.$store.state.tabZoomInfo);
+      this.restoreZoomFiltered(zoomInfo);
+    }
   },
   mounted() {
     // Delay execution of filterGroupSelection watcher

commit f1115c2908b0740a9765e356c8f13342c9536255

Remove duplicate initiate in v_zoom
Some code in v_zoom.js was accidentally duplicated. This a 
regression caused by #1419.

Let's remove the duplicated code.

frontend/src/static/js/v_zoom.js
@@ -119,12 +119,7 @@ window.vZoom = {
 
   methods: {
     initiate() {
-      if (this.info.zUser) {
-        // This code should always run since zUser must be defined
-        this.updateFileTypes();
-        this.selectedFileTypes = this.fileTypes.slice();
-      }
-
+      // This code crashes if info.zUser is not defined
       this.updateFileTypes();
       this.selectedFileTypes = this.fileTypes.slice();
     },