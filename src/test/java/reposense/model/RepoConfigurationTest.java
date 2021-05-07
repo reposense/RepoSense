07bbd79e2ddef68060d47d90591d1616066e155f
HCY123902 <54735123+HCY123902@users.noreply.github.com>
2021-05-07 20:36:42 +0800

    [#1190] Add message for invalid repository link on the summary view (#1509)
    The report generated does not show alert message for link given by 
    the user that cannot be recognized as a repository path.
    
    Showing the message that the link provided is invalid can help the 
    user to identify the reason why some repository is not shown in the 
    report.
    
    Let's add message for invalid link on the summary view.

--- src/test/java/reposense/model/RepoConfigurationTest.java
+++ src/test/java/reposense/model/RepoConfigurationTest.java
@@ -54,6 +54,7 @@ public class RepoConfigurationTest {
     private static final String TEST_REPO_DELTA = "https://github.com/reposense/testrepo-Delta.git";
     private static final String TEST_REPO_MINIMAL_STANDALONE_CONFIG =
             "https://github.com/reposense/testrepo-minimalstandaloneconfig.git";
+    private static final String TEST_REPO_INVALID_LOCATION = "ftp://github.com/reposense/testrepo-Delta.git";
 
     private static final Author FIRST_AUTHOR = new Author("lithiumlkid");
     private static final Author SECOND_AUTHOR = new Author("codeeong");
@@ -559,5 +560,18 @@ public class RepoConfigurationTest {
         updateAuthorList.invoke(null, actualConfig);
 
         TestUtil.compareRepoConfig(expectedConfig, actualConfig);
+    }
+
+    @Test
+    public void repoConfig_withInvalidLocation_success() throws Exception {
+        String formats = String.join(" ", CLI_FORMATS);
+        String input = new InputBuilder().addRepos(TEST_REPO_BETA, TEST_REPO_DELTA, TEST_REPO_INVALID_LOCATION)
+                .addFormats(formats)
+                .addIgnoreStandaloneConfig()
+                .build();
+        CliArguments cliArguments = ArgsParser.parse(translateCommandline(input));
+        List<RepoConfiguration> actualConfigs = RepoSense.getRepoConfigurations((LocationsCliArguments) cliArguments);
+
+        Assert.assertEquals(2, actualConfigs.size());
     }
 }
