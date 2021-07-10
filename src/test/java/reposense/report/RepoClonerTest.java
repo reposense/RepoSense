94032bb1ffc8f08dfb2b73fcaed41baa2020e51c
chan-j-d <chan.j.d.98@gmail.com>
2021-07-10 16:10:41 +0800

    Add new test case for relative pathing and spaces

--- src/test/java/reposense/report/RepoClonerTest.java
+++ src/test/java/reposense/report/RepoClonerTest.java
@@ -3,13 +3,22 @@ package reposense.report;
 import org.junit.Assert;
 import org.junit.Test;
 
+import reposense.git.GitClone;
 import reposense.model.RepoConfiguration;
 import reposense.model.RepoLocation;
+import reposense.util.FileUtil;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class RepoClonerTest {
 
     private static final String TEST_REPO_EMPTY_GIT_LOCATION = "https://github.com/reposense/testrepo-Empty.git";
+    private static final String TEST_REPO_GIT_LOCATION = "https://github.com/reposense/testrepo-Alpha.git";
     private static final String OUTPUT_PATH = "repocloner_test";
+    private static final Path REPOCLONE_LOCAL_TEST_PATH =
+            Paths.get(FileUtil.REPOS_ADDRESS,"repoclone test/dummy-repo");
 
     @Test
     public void repoCloner_emptyRepo_failsGracefully() throws Exception {
@@ -21,5 +30,20 @@ public class RepoClonerTest {
         RepoLocation clonedRepoLocation = repoCloner.getClonedRepoLocation();
 
         Assert.assertNull(clonedRepoLocation);
+    }
+
+    @Test
+    public void repoCloner_validRepoLocationWithRelativePathingAndSpaces_success() throws Exception {
+        //Clones a test repository into the test directory for testing of relative pathing
+        RepoConfiguration tempRemoteConfiguration = new RepoConfiguration(new RepoLocation(TEST_REPO_GIT_LOCATION));
+        FileUtil.deleteDirectory(REPOCLONE_LOCAL_TEST_PATH.toString());
+        FileUtil.createDirectory(REPOCLONE_LOCAL_TEST_PATH);
+        GitClone.cloneBare(tempRemoteConfiguration, Paths.get("."), REPOCLONE_LOCAL_TEST_PATH.toString());
+
+        RepoConfiguration repoWithRelativePathingAndSpacesAndEndingBackslash =
+                new RepoConfiguration(new RepoLocation(REPOCLONE_LOCAL_TEST_PATH.toString()));
+        RepoCloner repoCloner = new RepoCloner();
+        repoCloner.cloneBare(repoWithRelativePathingAndSpacesAndEndingBackslash);
+        Assert.assertTrue(Files.exists(REPOCLONE_LOCAL_TEST_PATH));
     }
 }
