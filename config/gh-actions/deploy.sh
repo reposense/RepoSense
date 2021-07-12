e804cc9fb40a5b3966d6ae85d21016e35d294d68
Ger Hean <gerhean@live.com>
2021-07-13 00:12:58 +0800

    Undo changes to workflow

--- config/gh-actions/deploy.sh
+++ config/gh-actions/deploy.sh
@@ -71,7 +71,6 @@ REPO_SLUG_ARRAY=(${GITHUB_REPOSITORY//\// })
 REPO_OWNER=${REPO_SLUG_ARRAY[0]}
 REPO_NAME=${REPO_SLUG_ARRAY[1]}
 DASHBOARD_DEPLOY_PATH=./reposense-report
-PROJECTIFY_DASHBOARD_DEPLOY_PATH=./new-reposense-report
 MARKBIND_DEPLOY_PATH=./docs/_site
 ACTIONS_DEPLOY="false"
 ACTIONS_WORKFLOW_RUN_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
@@ -82,12 +81,10 @@ then
   ACTIONS_PULL_REQUEST_HEAD=$(cat ./pr/SHA)
   ACTIONS_PULL_REQUEST_NUMBER=$(cat ./pr/NUMBER)
   ACTIONS_DASHBOARD_ID=$(cat ./pr/DASHBOARD_ID)
-  ACTIONS_PROJECTIFY_DASHBOARD_ID=$(cat ./pr/PROJECTIFY_DASHBOARD_ID)
   ACTIONS_DOCS_ID=$(cat ./pr/DOCS_ID)
 fi
 
 ACTIONS_DASHBOARD_ENV="dashboard-${ACTIONS_PULL_REQUEST_NUMBER}"
-ACTIONS_PROJECTIFY_DASHBOARD_ENV="pdashboard-${ACTIONS_PULL_REQUEST_NUMBER}"
 ACTIONS_DOCS_ENV="docs-${ACTIONS_PULL_REQUEST_NUMBER}"
 
 DEPLOY_SUBDOMAIN_UNFORMATTED_LIST=()
@@ -111,23 +108,19 @@ do
   then
     # Update GitHub status to failed
     update_deployment "${ACTIONS_DASHBOARD_ID}" "failure" "Dashboard deploy failed" "${ACTIONS_DASHBOARD_ENV}" "${ACTIONS_WORKFLOW_RUN_URL}"
-    update_deployment "${ACTIONS_PROJECTIFY_DASHBOARD_ID}" "failure" "pDashboard deploy failed" "${ACTIONS_PROJECTIFY_DASHBOARD_ENV}" "${ACTIONS_WORKFLOW_RUN_URL}"
     update_deployment "${ACTIONS_DOCS_ID}" "failure" "Docs deploy failed" "${ACTIONS_DOCS_ENV}" "${ACTIONS_WORKFLOW_RUN_URL}"
   elif [ "$ACTIONS_STATUS" == "in_progress" ]
   then
     # Set GitHub status to in_progress to indicate that deployment is in progress
     update_deployment "${ACTIONS_DASHBOARD_ID}" "in_progress" "Dashboard deployment in progress..." "${ACTIONS_DASHBOARD_ENV}"
-    update_deployment "${ACTIONS_PROJECTIFY_DASHBOARD_ID}" "in_progress" "Dashboard deployment in progress..." "${ACTIONS_PROJECTIFY_DASHBOARD_ENV}"
     update_deployment "${ACTIONS_DOCS_ID}" "in_progress" "Docs deployment in progress..." "${ACTIONS_DOCS_ENV}"
   elif [ "$ACTIONS_STATUS" == "pending" ]
   then
     # Set GitHub status to pending so that reviewers know that it is part of the checklist
     ACTIONS_DASHBOARD_ID=$(create_deployment "${ACTIONS_DASHBOARD_ENV}" "RepoSense dashboard preview")
-    ACTIONS_PROJECTIFY_DASHBOARD_ID=$(create_deployment "${ACTIONS_PROJECTIFY_DASHBOARD_ENV}" "RepoSense pdashboard preview")
     ACTIONS_DOCS_ID=$(create_deployment "${ACTIONS_DOCS_ENV}" "RepoSense documentation preview")
 
     echo "$ACTIONS_DASHBOARD_ID" > ./pr/DASHBOARD_ID
-    echo "$ACTIONS_PROJECTIFY_DASHBOARD_ID" > ./pr/PROJECTIFY_DASHBOARD_ID
     echo "$ACTIONS_DOCS_ID" > ./pr/DOCS_ID
   elif [ "$ACTIONS_STATUS" == "success" ] && [ "$ACTIONS_DEPLOY" == "true" ]
   then
@@ -139,13 +132,8 @@ do
     echo "Deploy domain: ${MARKBIND_DEPLOY_DOMAIN}"
     surge --project ${MARKBIND_DEPLOY_PATH} --domain $MARKBIND_DEPLOY_DOMAIN;
 
-    PROJECTIFY_DASHBOARD_DEPLOY_DOMAIN=https://pdashboard-${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
-    echo "Deploy domain: ${PROJECTIFY_DASHBOARD_DEPLOY_DOMAIN}"
-    surge --project ${PROJECTIFY_DASHBOARD_DEPLOY_PATH} --domain $PROJECTIFY_DASHBOARD_DEPLOY_DOMAIN;
-
     # Create github statuses that redirects users to the deployed dashboard and markbind docs
     update_deployment "${ACTIONS_DASHBOARD_ID}" "success" "Deploy domain: ${DASHBOARD_DEPLOY_DOMAIN}" "${ACTIONS_DASHBOARD_ENV}" "${DASHBOARD_DEPLOY_DOMAIN}"
-    update_deployment "${ACTIONS_PROJECTIFY_DASHBOARD_ID}" "success" "Deploy domain: ${PROJECTIFY_DASHBOARD_DEPLOY_DOMAIN}" "${ACTIONS_PROJECTIFY_DASHBOARD_ENV}" "${PROJECTIFY_DASHBOARD_DEPLOY_DOMAIN}"
     update_deployment "${ACTIONS_DOCS_ID}" "success" "Deploy domain: ${MARKBIND_DEPLOY_DOMAIN}" "${ACTIONS_DOCS_ENV}" "${MARKBIND_DEPLOY_DOMAIN}"
   fi
 done
