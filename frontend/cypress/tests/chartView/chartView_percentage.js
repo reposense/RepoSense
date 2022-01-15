068602a107cc089d4896a48da1b4f554c2b7b4e9
Zhou-Jiahao-1998 <jasonzhoujiahao@gmail.com>
2022-01-15 14:21:37 +0800

    Fix test style

--- frontend/cypress/tests/chartView/chartView_percentage.js
+++ frontend/cypress/tests/chartView/chartView_percentage.js
@@ -1,38 +1,38 @@
 describe('sort by contribution', () => {
-    it('percentage should only be displayed when sort by contribution', () => {
-        Cypress.wait();
+  it('percentage should only be displayed when sort by contribution', () => {
+    Cypress.wait();
 
-        cy.get('#summary .summary-chart__title--percentile')
-            .should('not.exist')
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('not.exist')
 
-        cy.get('div.mui-select.sort-group > select:visible')
-            .select('↑ contribution');
+    cy.get('div.mui-select.sort-group > select:visible')
+        .select('↑ contribution');
 
-        cy.get('#summary .summary-chart__title--percentile')
-            .should("exist")
+    cy.get('#summary .summary-chart__title--percentile')
+        .should("exist")
 
-        cy.get('div.mui-select.sort-group > select:visible')
-            .select('↓ group title');
+    cy.get('div.mui-select.sort-group > select:visible')
+        .select('↓ group title');
 
-        cy.get('#summary .summary-chart__title--percentile')
-            .should('not.exist')
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('not.exist')
 
-        cy.get('div.mui-select.sort-group > select:visible')
-            .select('↓ contribution');
+    cy.get('div.mui-select.sort-group > select:visible')
+        .select('↓ contribution');
 
-        cy.get('#summary .summary-chart__title--percentile')
-            .should("exist")
+    cy.get('#summary .summary-chart__title--percentile')
+        .should("exist")
 
-        cy.get('div.mui-select.sort-group > select:visible')
-            .select('↑ variance');
+    cy.get('div.mui-select.sort-group > select:visible')
+        .select('↑ variance');
 
-        cy.get('#summary .summary-chart__title--percentile')
-            .should('not.exist')
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('not.exist')
 
-        cy.get('div.mui-select.sort-group > select:visible')
-            .select('↓ variance');
+    cy.get('div.mui-select.sort-group > select:visible')
+        .select('↓ variance');
 
-        cy.get('#summary .summary-chart__title--percentile')
-            .should('not.exist')
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('not.exist')
     });
 });
