ba30decd2c3c372d7e4f6795556af1d92295fc71
Zhou-Jiahao-1998 <jasonzhoujiahao@gmail.com>
2022-01-16 22:21:08 +0800

    Add more test cases

--- frontend/cypress/tests/chartView/chartView_percentage.js
+++ frontend/cypress/tests/chartView/chartView_percentage.js
@@ -35,4 +35,66 @@ describe('sort by contribution', () => {
     cy.get('#summary .summary-chart__title--percentile')
         .should('not.exist');
   });
+
+  it('percentage should be shown by changing other fields', () => {
+    Cypress.wait();
+
+    cy.get('div.mui-select.grouping > select:visible')
+        .select('None');
+
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('not.exist');
+
+    cy.get('div.mui-select.grouping > select:visible')
+        .select('Author');
+
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('not.exist');
+
+    cy.get('div.mui-select.sort-within-group > select:visible')
+        .select('↓ contribution');
+
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('not.exist');
+
+    cy.get('div.mui-select.sort-within-group > select:visible')
+        .select('↑ variance');
+
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('not.exist');
+  });
+
+  it('percentage should be hidden by changing other fields', () => {
+    Cypress.wait();
+
+    cy.get('div.mui-select.sort-group > select:visible')
+        .select('↓ contribution');
+
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('exist');
+
+    cy.get('div.mui-select.grouping > select:visible')
+        .select('None');
+
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('exist');
+
+    cy.get('div.mui-select.grouping > select:visible')
+        .select('Author');
+
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('exist');
+
+    cy.get('div.mui-select.sort-within-group > select:visible')
+        .select('↑ contribution');
+
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('exist');
+
+    cy.get('div.mui-select.sort-within-group > select:visible')
+        .select('↓ title');
+
+    cy.get('#summary .summary-chart__title--percentile')
+        .should('exist');
+  });
 });
