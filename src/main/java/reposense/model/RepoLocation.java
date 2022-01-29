0dcdafee7a96a48ae19194371b084f89e2a93b18
chan-j-d <chan.j.d.98@gmail.com>
2022-01-29 19:49:29 +0800

    Fix rebase conflict

--- src/main/java/reposense/model/RepoLocation.java
+++ src/main/java/reposense/model/RepoLocation.java
@@ -19,7 +19,7 @@ public class RepoLocation {
     private static final String GIT_LINK_SUFFIX = ".git";
     private static final String MESSAGE_INVALID_LOCATION = "%s is an invalid location.";
     private static final Pattern GIT_REPOSITORY_LOCATION_PATTERN =
-            Pattern.compile("^https?://github.com/(?<org>.+?)/(?<repoName>.+?)\\.git$");
+            Pattern.compile("^.*/(?<org>.+?)/(?<repoName>.+?)\\.git$");
 
     private final String location;
     private final String repoName;
@@ -32,15 +32,16 @@ public class RepoLocation {
         if (SystemUtil.isWindows()) {
             location = StringsUtil.removeTrailingBackslash(location);
         }
-        verifyLocation(location);
         this.location = location;
         Matcher matcher = GIT_REPOSITORY_LOCATION_PATTERN.matcher(location);
 
-        if (matcher.matches()) {
+        if (isLocalRepo(location)) {
+            repoName = Paths.get(location).getFileName().toString().replace(GIT_LINK_SUFFIX, "");
+        } else if (matcher.matches()) {
             organization = matcher.group("org");
             repoName = matcher.group("repoName");
         } else {
-            repoName = Paths.get(location).getFileName().toString().replace(GIT_LINK_SUFFIX, "");
+            repoName = "UNABLE_TO_DETERMINE";
         }
     }
 
@@ -56,28 +57,23 @@ public class RepoLocation {
         return organization;
     }
 
-    /**
-     * Verifies {@code location} can be presented as a {@code URL} or {@code Path}.
-     * @throws InvalidLocationException if otherwise.
-     */
-    private void verifyLocation(String location) throws InvalidLocationException {
-        boolean isValidPathLocation = false;
-        boolean isValidGitUrl = false;
-
-        try {
-            Path pathLocation = Paths.get(location);
-            isValidPathLocation = Files.exists(pathLocation);
-        } catch (InvalidPathException ipe) {
-            // Ignore exception
+    private boolean isLocalRepo(String repoArgument) {
+        boolean containsColon = repoArgument.contains(":");
+        if (!containsColon) {
+            return true;
         }
 
-        Matcher matcher = GIT_REPOSITORY_LOCATION_PATTERN.matcher(location);
-        isValidGitUrl = matcher.matches();
-
-        if (!isValidPathLocation && !isValidGitUrl) {
-            ErrorSummary.getInstance().addErrorMessage(location, String.format(MESSAGE_INVALID_LOCATION, location));
-            throw new InvalidLocationException(String.format(MESSAGE_INVALID_LOCATION, location));
+        boolean hasSlashBeforeFirstColon = repoArgument.split(":", 2)[0].contains("/");
+        if (hasSlashBeforeFirstColon) {
+            return true;
         }
+
+        boolean isURLFileType = repoArgument.split(":", 2)[0].equals("file");
+        if (isURLFileType) {
+            return true;
+        }
+
+        return false;
     }
 
 
