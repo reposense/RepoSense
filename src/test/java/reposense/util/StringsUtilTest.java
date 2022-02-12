a55f2d39f5601983897ad49f33e1a34056db1d96
chan-j-d <chan.j.d.98@gmail.com>
2022-02-12 20:28:43 +0800

    Adjust bash test case

--- src/test/java/reposense/util/StringsUtilTest.java
+++ src/test/java/reposense/util/StringsUtilTest.java
@@ -76,7 +76,7 @@ public class StringsUtilTest {
     @Test
     public void addQuotationMarksForPath_specialBashCharacters_success() {
         Assume.assumeTrue(!SystemUtil.isWindows());
-        String specialBashSymbols = "!\"#$&'()*,;<=>?\\[]^`{| \t}";
+        String specialBashSymbols = "!\"#$&'()*,;<=>?\\[]^`{| }";
         String result = CommandRunner.runCommand(Paths.get("./"),
                 "echo " + StringsUtil.addQuotesForFilePath(specialBashSymbols));
         Assert.assertEquals(specialBashSymbols, result);
