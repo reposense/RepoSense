fb82dbb2d7e9e30c870aface5f68d30be454dba0
Gokul Rajiv <gokul.rajiv99@gmail.com>
2022-02-27 19:55:20 +0800

    Fix style

--- frontend/cypress/tests/general/general_reportTitle.js
+++ frontend/cypress/tests/general/general_reportTitle.js
@@ -1,5 +1,5 @@
-describe("general", () => {
-  it("correctly replaces report title", () => {
+describe('general', () => {
+  it('correctly replaces report title', () => {
     cy.title().should('eq', 'RepoSense Report');
   });
 });
