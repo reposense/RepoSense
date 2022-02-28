2693cb1bfadda60332513e0c37d1ce3f5ce06fe5
Gokul Rajiv <gokul.rajiv99@gmail.com>
2022-03-01 01:58:13 +0800

    Use different report name

--- frontend/cypress/tests/general/general.js
+++ frontend/cypress/tests/general/general.js
@@ -1,5 +1,5 @@
 describe('general', () => {
   it('correctly replaces report title', () => {
-    cy.title().should('eq', 'RepoSense Report');
+    cy.title().should('eq', 'RepoSense Test Report');
   });
 });
