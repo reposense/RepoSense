6d60108f43cec211aba83c89a72f44d73ede0808
Zhou-Jiahao-1998 <jasonzhoujiahao@gmail.com>
2022-03-06 10:06:03 +0800

    Move v-show to tooltip

--- frontend/src/views/v-authorship.vue
+++ frontend/src/views/v-authorship.vue
@@ -96,14 +96,12 @@
       .file
         .title
           span.path(v-on:click="toggleFileActiveProperty(file)")
-            span(v-show="file.active")
-              .tooltip
-                font-awesome-icon(icon="caret-down", fixed-width)
-                span.tooltip-text Click to hide file details
-            span(v-show="!file.active")
-              .tooltip
-                font-awesome-icon(icon="caret-right", fixed-width)
-                span.tooltip-text Click to show file details
+            .tooltip(v-show="file.active")
+              font-awesome-icon(icon="caret-down", fixed-width)
+              span.tooltip-text Click to hide file details
+            .tooltip(v-show="!file.active")
+              font-awesome-icon(icon="caret-right", fixed-width)
+              span.tooltip-text Click to show file details
             span {{ i + 1 }}. &nbsp;&nbsp; {{ file.path }} &nbsp;
           span.icons
             a(
