e536be4ec6ca3fc918f9d12e527918a207cf77d3
Zhou-Jiahao-1998 <jasonzhoujiahao@gmail.com>
2022-03-07 14:17:18 +0800

    Import normalize.css from node_modules

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -320,7 +320,6 @@ export default app;
 
 <style lang="scss">
 @import 'https://fonts.googleapis.com/css?family=Titillium+Web';
-@import 'https://cdn.jsdelivr.net/npm/normalize.css@8.0/normalize.min.css';
 @import 'https://cdn.jsdelivr.net/npm/muicss@0.9/dist/css/mui.min.css';
 @import 'https://cdn.jsdelivr.net/npm/highlight.js@9.14/styles/color-brewer.min.css';
 @import 'https://cdn.jsdelivr.net/npm/vue-loading-overlay@3/dist/vue-loading.css';
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import { dom } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import hljs from 'highlight.js';
+import 'normalize.css/normalize.css';
 
 // Need to import for side effects
 import './utils/api';
