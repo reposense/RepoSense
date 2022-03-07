d33be9c9b522dc5b45457ca1a19fe51fc732f129
Zhou-Jiahao-1998 <jasonzhoujiahao@gmail.com>
2022-03-07 15:02:39 +0800

    Import from node_module

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import { dom } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import hljs from 'highlight.js';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 // Need to import for side effects
 import './utils/api';
