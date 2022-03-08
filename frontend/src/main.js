49ee6eed5ba87438d7b029d62dc2f0d4ef2e7e25
Zhou-Jiahao-1998 <jasonzhoujiahao@gmail.com>
2022-03-08 09:46:01 +0800

    Left with highlight.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import { dom } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import hljs from 'highlight.js';
+import 'muicss/dist/css/mui.min.css';
+import 'normalize.css/normalize.css';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 // Need to import for side effects
 import './utils/api';
