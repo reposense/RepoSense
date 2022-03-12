5053e6984d4717c32e6c326ed438a3e27fdac3ec
chan-j-d <chan.j.d.98@gmail.com>
2022-03-12 23:14:25 +0800

    Implement solution that works during compile time

--- src/main/java/reposense/model/SupportedDomainUrlMap.java
+++ src/main/java/reposense/model/SupportedDomainUrlMap.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
+ * Represents the map of supported remote repo domain names.
  */
 public class SupportedDomainUrlMap {
 
@@ -12,21 +12,27 @@ public class SupportedDomainUrlMap {
     private static final String COMMIT_PATH_KEY = "COMMIT_PATH";
     private static final String BLAME_PATH_KEY = "BLAME_PATH";
 
-    private static final Map<String, String> GITHUB_MAP = new HashMap<String, String>() {{
-        put(REPO_URL_KEY, "https://github.com/ORGANIZATION/REPO_NAME/");
-        put(COMMIT_PATH_KEY, "commit/COMMIT_HASH/");
-        put(BLAME_PATH_KEY, "blame/BRANCH/FILE_PATH/");
-    }};
-    private static final Map<String, String> GITLAB_MAP = new HashMap<String, String>() {{
-        put(REPO_URL_KEY, "https://gitlab.com/ORGANIZATION/REPO_NAME/");
-        put(COMMIT_PATH_KEY, "-/commit/COMMIT_HASH/");
-        put(BLAME_PATH_KEY, "-/blame/BRANCH/FILE_PATH/");
-    }};
-    private static final Map<String, String> BITBUCKET_MAP = new HashMap<String, String>() {{
-        put(REPO_URL_KEY, "https://bitbucket.org/ORGANIZATION/REPO_NAME/");
-        put(COMMIT_PATH_KEY, "commits/COMMIT_HASH/");
-        put(BLAME_PATH_KEY, "annotate/BRANCH/FILE_PATH/");
-    }};
+    private static final Map<String, String> GITHUB_MAP = new HashMap<String, String>() {
+        {
+            put(REPO_URL_KEY, "https://github.com/ORGANIZATION/REPO_NAME/");
+            put(COMMIT_PATH_KEY, "commit/COMMIT_HASH/");
+            put(BLAME_PATH_KEY, "blame/BRANCH/FILE_PATH/");
+        }
+    };
+    private static final Map<String, String> GITLAB_MAP = new HashMap<String, String>() {
+        {
+            put(REPO_URL_KEY, "https://gitlab.com/ORGANIZATION/REPO_NAME/");
+            put(COMMIT_PATH_KEY, "-/commit/COMMIT_HASH/");
+            put(BLAME_PATH_KEY, "-/blame/BRANCH/FILE_PATH/");
+        }
+    };
+    private static final Map<String, String> BITBUCKET_MAP = new HashMap<String, String>() {
+        {
+            put(REPO_URL_KEY, "https://bitbucket.org/ORGANIZATION/REPO_NAME/");
+            put(COMMIT_PATH_KEY, "commits/COMMIT_HASH/");
+            put(BLAME_PATH_KEY, "annotate/BRANCH/FILE_PATH/");
+        }
+    };
 
     private static final SupportedDomainUrlMap DEFAULT_DOMAIN_URL_MAP = new SupportedDomainUrlMap();
 
@@ -43,10 +49,16 @@ public class SupportedDomainUrlMap {
         return domainUrlMap;
     }
 
+    /**
+     * Returns true if {@code domainName} is currently supported.
+     */
     public boolean isSupportedDomain(String domainName) {
         return domainUrlMap.containsKey(domainName);
     }
 
+    /**
+     * Returns true if {@code domainName} is currently supported.
+     */
     public static boolean isSupportedDomainName(String domainName) {
         return DEFAULT_DOMAIN_URL_MAP.isSupportedDomain(domainName);
     }
