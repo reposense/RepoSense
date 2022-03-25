c775ede6b274aff4dd3d9a9ef22dd98f392aa902
Tay Yi Hsuen <69678785+yhtMinceraft1010X@users.noreply.github.com>
2022-03-25 11:48:01 +0800

    [#1651] Enhance Checkstyle (#1652)
    Checkstyle 8.1 does not have checks for certain Javadoc standards like
    whitespace after asterisks and an empty line between description and
    block tag group. At the same time, it is tedious to type out all 3
    checkstyle tasks for gradlew.
    
    Let's migrate to Checkstyle 9.3 to add the missing checks and create a
    consolidated checkstyleAll task to manage all 3 checkstyle tasks.

--- src/main/java/reposense/git/GitClone.java
+++ src/main/java/reposense/git/GitClone.java
@@ -137,6 +137,7 @@ public class GitClone {
 
     /**
      * Clones a bare repo specified in {@code config} into the folder {@code outputFolderName}.
+     *
      * @throws IOException if it fails to delete a directory.
      */
     public static void cloneBare(RepoConfiguration config, Path rootPath,
@@ -155,6 +156,7 @@ public class GitClone {
     /**
      * Performs a full clone from {@code clonedBareRepoLocation} into the folder {@code outputFolderName} and
      * directly branches out to {@code targetBranch}.
+     *
      * @throws IOException if it fails to delete a directory.
      * @throws GitCloneException when an error occurs during command execution.
      */
