10a8c1884fb6d7b998dcfb006d7fcd220f3c869b
chan-j-d <chan.j.d.98@gmail.com>
2022-04-05 19:58:31 +0800

    Fix checkstyle issue

--- src/systemtest/java/reposense/LocalRepoSystemTest.java
+++ src/systemtest/java/reposense/LocalRepoSystemTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import reposense.git.GitClone;
 import reposense.model.RepoConfiguration;
 import reposense.model.RepoLocation;
 import reposense.util.FileUtil;
