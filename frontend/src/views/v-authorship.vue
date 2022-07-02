790f7bd410a0562cce16542057f1c252374cbb57
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-07-02 16:37:33 +0700

    Align button to glob input for smaller viewport
    Filter button is not properly aligned to the glob filter input
    when the viewport is smaller.
    
    Flex-wrap .mui-form--inline to vertically align the
    glob filter input and the filter button.

--- frontend/src/views/v-authorship.vue
+++ frontend/src/views/v-authorship.vue
@@ -710,19 +710,24 @@ export default {
         vertical-align: middle;
       }
 
-      #search {
-        @include medium-font;
-        margin-top: 1.25rem;
-        padding: .5rem 1.0rem .25rem 1.0rem;
-        width: 70%;
-      }
+      .mui-form--inline {
+        display: flex;
+        flex-wrap: wrap;
 
-      #submit-button {
-        @include medium-font;
-        background-color: mui-color('blue');
-        color: mui-color('white');
-        margin: 1.0rem 0 0 .25rem;
-        padding: .5rem 1.0rem .25rem 1.0rem;
+        #search {
+          @include medium-font;
+          margin-top: 1.25rem;
+          padding: .5rem 1.0rem .25rem 1.0rem;
+          width: 70%;
+        }
+
+        #submit-button {
+          @include medium-font;
+          background-color: mui-color('blue');
+          color: mui-color('white');
+          margin: 1.0rem 0 0 .25rem;
+          padding: .5rem 1.0rem .25rem 1.0rem;
+        }
       }
 
       .searchbox {
