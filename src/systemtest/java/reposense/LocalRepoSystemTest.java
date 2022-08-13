5675c63321fdaaec9ac76c55ef7eba085ce67d7f
lawwm <41231584+lawwm@users.noreply.github.com>
2022-08-13 21:00:50 +0800

    [#1818] Add input builder to LocalRepoSystemTest (#1821)
    Currently, all test cases within LocalRepoSystemTest uses
    String.format().
    
    Let's change the CLI input of test cases within LocalRepoSystemTest to
    use the InputBuilder class.

--- src/systemtest/java/reposense/LocalRepoSystemTest.java
+++ src/systemtest/java/reposense/LocalRepoSystemTest.java
@@ -1,5 +1,6 @@
 package reposense;
 
+import static org.apache.tools.ant.types.Commandline.translateCommandline;
 import static reposense.util.TestUtil.loadResource;
 
 import java.nio.file.Path;
@@ -16,8 +17,10 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 import reposense.model.RepoConfiguration;
 import reposense.model.RepoLocation;
 import reposense.model.SupportedDomainUrlMap;
+import reposense.parser.SinceDateArgumentType;
 import reposense.report.ErrorSummary;
 import reposense.util.FileUtil;
+import reposense.util.InputBuilder;
 import reposense.util.SystemTestUtil;
 import reposense.util.TestRepoCloner;
 
@@ -33,7 +36,6 @@ public class LocalRepoSystemTest {
     private static final String OUTPUT_DIRECTORY = "local-test";
     private static final String TIME_ZONE = "UTC+08";
     private static final Path REPORT_DIRECTORY_PATH = Paths.get(OUTPUT_DIRECTORY, "reposense-report");
-
 
     @BeforeAll
     public static void setupLocalRepos() throws Exception {
@@ -63,26 +65,35 @@ public class LocalRepoSystemTest {
 
     @Test
     public void testSameFinalDirectory() {
-        String cliInput = String.format("-r %s %s -s d1 -u %s -o %s -t %s",
-                LOCAL_DIRECTORY_ONE, LOCAL_DIRECTORY_TWO, LAST_COMMIT_DATE, OUTPUT_DIRECTORY, TIME_ZONE);
-        runTest(cliInput, "LocalRepoSystemTest/testSameFinalDirectory");
+        InputBuilder inputBuilder = new InputBuilder()
+                .addRepos(LOCAL_DIRECTORY_ONE, LOCAL_DIRECTORY_TWO)
+                .addSinceDate(SinceDateArgumentType.FIRST_COMMIT_DATE_SHORTHAND)
+                .addUntilDate(LAST_COMMIT_DATE)
+                .addOutput(Paths.get(OUTPUT_DIRECTORY))
+                .addTimezone(TIME_ZONE);
+        runTest(inputBuilder, "LocalRepoSystemTest/testSameFinalDirectory");
     }
 
     @Test
     public void testRelativePathing() {
         String relativePathForTesting = "parent1/../parent1/./test-repo";
-        String cliInput = String.format("-r %s -s d1 -u %s -o %s -t %s",
-                relativePathForTesting, LAST_COMMIT_DATE, OUTPUT_DIRECTORY, TIME_ZONE);
-        runTest(cliInput, "LocalRepoSystemTest/testRelativePathing");
+
+        InputBuilder inputBuilder = new InputBuilder()
+                .addRepos(relativePathForTesting)
+                .addSinceDate(SinceDateArgumentType.FIRST_COMMIT_DATE_SHORTHAND)
+                .addUntilDate(LAST_COMMIT_DATE)
+                .addOutput(Paths.get(OUTPUT_DIRECTORY))
+                .addTimezone(TIME_ZONE);
+
+        runTest(inputBuilder, "LocalRepoSystemTest/testRelativePathing");
     }
 
     /**
-     * Runs RepoSense with {@code commandArgs} and tests it against the expected
+     * Runs RepoSense with {@code inputBuilder} and tests it against the expected
      * files in {@code expectedFilesPathString}.
      */
-    private void runTest(String commandArgs, String expectedFilesPathString) {
-        String[] args = commandArgs.split(" ");
-        RepoSense.main(args);
+    private void runTest(InputBuilder inputBuilder, String expectedFilesPathString) {
+        RepoSense.main(translateCommandline(inputBuilder.build()));
         Path expectedFilesPath = loadResource(getClass(), expectedFilesPathString);
         SystemTestUtil.verifyReportJsonFiles(expectedFilesPath, REPORT_DIRECTORY_PATH);
     }
