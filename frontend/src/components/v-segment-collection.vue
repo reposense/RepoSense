fd8473c4a169a2d9d1dc701a594e67962850974d
Gokul Rajiv <34594184+gok99@users.noreply.github.com>
2022-08-15 06:25:42 +0400

    [#1748] Lazy loading for code segments in authorship view (#1750)
    Code segments are loaded all at once in the authorship view which could
    take a long time or possibly break the report for especially large
    diffs.
    
    Let's implement lazy loading by only rendering code segments that are
    visible to the user, to speed up report loading. Let's also replace the
    overall character limit with a single file character limit, since a
    large number of small code segments slowing down report loading is no
    longer a concern.

---
+++ frontend/src/components/v-segment-collection.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+.segment-collection(v-observe-visibility="{ callback: visibilityChanged }")
+  template(v-if="isRendered", v-for="segment in segments")
+    v-segment(v-bind:segment="segment", v-bind:path="path")
+</template>
+
+<script>
+import vSegment from './v-segment.vue';
+
+export default {
+  name: 'v-segment-collection',
+  components: {
+    vSegment,
+  },
+  props: ['segments', 'path'],
+  data() {
+    return {
+      isRendered: false,
+    };
+  },
+  methods: {
+    visibilityChanged(isVisible) {
+      if (isVisible) {
+        this.isRendered = true;
+      }
+    },
+  },
+};
+</script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp, DirectiveBinding } from 'vue';
 import { dom } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { ObserveVisibility } from 'vue-observe-visibility';
 import hljs from 'highlight.js';
 import 'muicss/dist/css/mui.min.css';
 import 'normalize.css/normalize.css';
@@ -28,6 +29,14 @@ app.directive('hljs', {
   },
 });
 
+app.directive('observe-visibility', {
+  beforeMount: (el, binding, vnode) => {
+    (vnode as any).context = binding.instance;
+    ObserveVisibility.bind(el, binding, vnode);
+  },
+  updated: ObserveVisibility.update,
+  unmounted: ObserveVisibility.unbind,
+});
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(store);
 
