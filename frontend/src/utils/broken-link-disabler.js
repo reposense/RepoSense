cd42c648beb70b31c604d62ffe65871f3704c5ad
LuoZhijie-tom <e0492460@u.nus.edu>
2022-08-28 15:03:57 +0800

    Use logic in api.js to check if the link is broken

--- frontend/src/utils/broken-link-disabler.js
+++ frontend/src/utils/broken-link-disabler.js
@@ -1,14 +1,7 @@
-window.$ = (id) => document.getElementById(id);
 const mixin = {
   methods: {
     isBrokenLink(link) {
-      let isLinkBroken = true;
-      Object.keys(window.DOMAIN_URL_MAP).forEach((k) => {
-        if (k !== 'NOT_RECOGNIZED' && link.startsWith(window.DOMAIN_URL_MAP[k].BASE_URL)) {
-          isLinkBroken = false;
-        }
-      });
-      return isLinkBroken;
+      return link !== undefined;
     },
   },
   data: {
