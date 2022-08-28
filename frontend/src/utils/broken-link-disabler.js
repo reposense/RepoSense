96a70712244eb3b0ade8051cebe99edd85819fbe
LuoZhijie-tom <e0492460@u.nus.edu>
2022-08-28 13:17:47 +0800

    Use supportedDomainUrlMap to perform the logic of checking valid link

--- frontend/src/utils/broken-link-disabler.js
+++ frontend/src/utils/broken-link-disabler.js
@@ -1,8 +1,14 @@
+window.$ = (id) => document.getElementById(id);
 const mixin = {
   methods: {
     isBrokenLink(link) {
-      const linkFormat = /^(https:\/\/)(github.com|bitbucket.org|gitlab.com).*/;
-      return !linkFormat.test(link);
+      let isLinkBroken = true;
+      Object.keys(window.DOMAIN_URL_MAP).forEach((k) => {
+        if (k !== 'NOT_RECOGNIZED' && link.startsWith(window.DOMAIN_URL_MAP[k].BASE_URL)) {
+          isLinkBroken = false;
+        }
+      });
+      return isLinkBroken;
     },
   },
   data: {
