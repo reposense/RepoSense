10c4e18ce17811d6251e3abd5dc95e69abd7e4c4
vvidday <davids89034@gmail.com>
2023-02-02 12:57:04 +0800

    Fix typo in c-authorship

--- frontend/src/views/c-authorship.vue
+++ frontend/src/views/c-authorship.vue
@@ -7,7 +7,7 @@
       v-if="activeFilesCount < selectedFiles.length",
       v-on:click="toggleAllFileActiveProperty(true)"
     ) show all file details
-    a(v-if="activeFilesCount > 0", v-on:click="toggleAllFileActiveProperty(False)") hide all file details
+    a(v-if="activeFilesCount > 0", v-on:click="toggleAllFileActiveProperty(false)") hide all file details
   .panel-heading
     a.group-name(
       v-bind:href="info.location", target="_blank",
