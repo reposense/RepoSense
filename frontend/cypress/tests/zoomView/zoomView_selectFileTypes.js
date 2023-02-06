83834a6e7f448c169bb7c5b2743b8c5ca1bd3f0f
YIHSUEN\Yi Hsuen <tayyihsuen@gmail.com>
2023-02-06 23:26:12 +0800

    Fix eslint indentation levels
    The indentation level options have incorrect values, resulting in code
    that violates the 2 spaces indentation rule being considered correct
    by ESLint.
    
    Let's update the indentation level options and correct the indentations
    in the code.

--- frontend/cypress/tests/zoomView/zoomView_selectFileTypes.js
+++ frontend/cypress/tests/zoomView/zoomView_selectFileTypes.js
@@ -1,110 +1,110 @@
 describe('check file types ', () => {
   it('check if all file types are visible by default', () => {
     cy.get('.icon-button.fa-list-ul')
-        .should('be.visible')
-        .first()
-        .click();
+      .should('be.visible')
+      .first()
+      .click();
 
     cy.get('#tab-zoom .zoom__day', { timeout: 90000 })
-        .should('exist');
+      .should('exist');
 
     cy.get('#tab-zoom .fileTypes input[value="all"]')
-        .should('be.checked');
+      .should('be.checked');
   });
 
   it('uncheck all file types should show no files', () => {
     cy.get('.icon-button.fa-list-ul')
-        .should('be.visible')
-        .first()
-        .click();
+      .should('be.visible')
+      .first()
+      .click();
 
     cy.get('#tab-zoom .zoom__day')
-        .should('exist');
+      .should('exist');
 
     cy.get('#tab-zoom .fileTypes input[value="all"]')
-        .uncheck();
+      .uncheck();
 
     cy.get('#tab-zoom .zoom__day')
-        .should('not.exist');
+      .should('not.exist');
   });
 
   it('uncheck file type should uncheck all option', () => {
     // Assumptions: the first author of the first repo
     // committed .java, .js and .gradle files.
     cy.get('.icon-button.fa-list-ul')
-        .should('be.visible')
-        .first()
-        .click();
+      .should('be.visible')
+      .first()
+      .click();
 
     cy.get('#tab-zoom .fileTypes input[value="java"]')
-        .uncheck()
-        .should('not.be.checked');
+      .uncheck()
+      .should('not.be.checked');
 
     cy.get('#tab-zoom .fileTypes input[value="js"]')
-        .uncheck()
-        .should('not.be.checked');
+      .uncheck()
+      .should('not.be.checked');
 
     cy.get('#tab-zoom .fileTypes input[value="all"]')
-        .should('not.be.checked');
+      .should('not.be.checked');
   });
 
   it('a commit should not be seen when all of its file types are unchecked', () => {
     // Assumptions: the third commit (19e3294) of the first author of the first repo
     // contains changes in only .java files.
     cy.get('.icon-button.fa-list-ul')
-        .should('be.visible')
-        .first()
-        .click();
+      .should('be.visible')
+      .first()
+      .click();
 
     cy.get('.zoom__day > .commit-message')
-        .eq(2)
-        .within(() => {
-          cy.get('.hash')
-              .should('have.text', '19e3294');
-          cy.get('.fileTypeLabel')
-              .should('have.text', 'java');
-        });
+      .eq(2)
+      .within(() => {
+        cy.get('.hash')
+          .should('have.text', '19e3294');
+        cy.get('.fileTypeLabel')
+          .should('have.text', 'java');
+      });
 
     cy.get('#tab-zoom .fileTypes input[value="java"]')
-        .uncheck()
-        .should('not.be.checked');
+      .uncheck()
+      .should('not.be.checked');
 
     cy.get('.zoom__day > .commit-message')
-        .should('not.contain.text', '19e3294');
+      .should('not.contain.text', '19e3294');
   });
 
   it.only('unchecked file type label can still remain in a commit of multiple file types', () => {
     // Assumptions: the sixth commit (5ab0322) of the first author of the first repo
     // contains changes in both .java and .md files.
     cy.get('.icon-button.fa-list-ul')
-        .should('be.visible')
-        .first()
-        .click();
+      .should('be.visible')
+      .first()
+      .click();
 
     cy.get('.zoom__day > .commit-message')
-        .eq(5)
-        .within(() => {
-          cy.get('.hash')
-              .should('have.text', '5ab0322');
-          cy.get('.fileTypeLabel')
-              .should('contain.text', 'java')
-              .should('contain.text', 'md');
-        });
+      .eq(5)
+      .within(() => {
+        cy.get('.hash')
+          .should('have.text', '5ab0322');
+        cy.get('.fileTypeLabel')
+          .should('contain.text', 'java')
+          .should('contain.text', 'md');
+      });
 
     cy.get('#tab-zoom .fileTypes input[value="java"]')
-        .uncheck()
-        .should('not.be.checked');
+      .uncheck()
+      .should('not.be.checked');
 
     cy.get('.zoom__day > .commit-message')
-        // note that the same commit is now of second order since other commits
-        // were filtered out
-        .eq(2)
-        .within(() => {
-          cy.get('.hash')
-              .should('have.text', '5ab0322');
-          cy.get('.fileTypeLabel')
-              .should('contain.text', 'java')
-              .should('contain.text', 'md');
-        });
+    // note that the same commit is now of second order since other commits
+    // were filtered out
+      .eq(2)
+      .within(() => {
+        cy.get('.hash')
+          .should('have.text', '5ab0322');
+        cy.get('.fileTypeLabel')
+          .should('contain.text', 'java')
+          .should('contain.text', 'md');
+      });
   });
 });
