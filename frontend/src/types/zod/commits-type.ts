9a09728ef6af516d04b65278bfe1da221a5bd061
vvidday <davids89034@gmail.com>
2023-02-11 01:15:10 +0800

    Fix formatting of zod types and add typescript exports

--- frontend/src/types/zod/commits-type.ts
+++ frontend/src/types/zod/commits-type.ts
@@ -1,9 +1,9 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 const fileTypesAndContributionSchema = z.object({
   insertions: z.number(),
   deletions: z.number(),
-})
+});
 
 const commitResult = z.object({
   hash: z.string(),
@@ -11,22 +11,26 @@ const commitResult = z.object({
   messageBody: z.string(),
   tags: z.array(z.string()).optional(),
   fileTypesAndContributionMap: z.record(fileTypesAndContributionSchema),
-})
+});
 
 const authorDailyContributionsSchema = z.object({
   date: z.string(),
   commitResults: z.array(commitResult),
-})
+});
 
 const authorFileTypeContributionsSchema = z.record(z.number());
 
 // Contains the zod validation schema for the commits.json file
 
 export const commitsSchema = z.object({
-  authorDailyContributionsMap: z.record(authorDailyContributionsSchema),
+  authorDailyContributionsMap: z.record(z.array(authorDailyContributionsSchema)),
   authorFileTypeContributionMap: z.record(authorFileTypeContributionsSchema),
   authorContributionVariance: z.record(z.number()),
   authorDisplayNameMap: z.record(z.string()),
-})
+});
 
-export type CommitResultRaw = z.infer<typeof commitResult>
+// Export typescript types
+export type Commits = z.infer<typeof commitsSchema>;
+export type CommitResultRaw = z.infer<typeof commitResult>;
+export type AuthorDailyContributions = z.infer<typeof authorDailyContributionsSchema>;
+export type AuthorFileTypeContributions = z.infer<typeof authorFileTypeContributionsSchema>;
--- frontend/src/types/zod/summary-type.ts
+++ frontend/src/types/zod/summary-type.ts
@@ -1,24 +1,34 @@
-import { z } from 'zod'
-import { urlSchema } from './url'
+import { z } from 'zod';
 
 const locationSchema = z.object({
   location: z.string(),
   repoName: z.string(),
   organization: z.string(),
   domainName: z.string(),
-})
+});
 
 const repoSchema = z.object({
   location: locationSchema,
   branch: z.string(),
   displayName: z.string(),
   outputFolderName: z.string(),
-})
+});
 
 const errorSchema = z.object({
   repoName: z.string(),
   errorMessage: z.string(),
-})
+});
+
+const urlSchema = z.object({
+  BASE_URL: z.string(),
+  BLAME_PATH: z.string(),
+  BRANCH: z.string(),
+  COMMIT_PATH: z.string(),
+  HISTORY_PATH: z.string(),
+  REPO_URL: z.string(),
+});
+
+const supportedDomainUrlMapSchema = z.record(urlSchema);
 
 // Contains the zod validation schema for the summary.json file
 
@@ -34,14 +44,10 @@ export const summarySchema = z.object({
   untilDate: z.string(),
   isSinceDateProvided: z.boolean(),
   isUntilDateProvided: z.boolean(),
-  supportedDomainUrlMap: z.object({
-    NOT_RECOGNIZED: urlSchema,
-    github: urlSchema,
-  })
-})
+  supportedDomainUrlMap: supportedDomainUrlMapSchema,
+});
 
 // Export typescript types
-export type Repo = z.infer<typeof repoSchema>;
-
-
-
+export type DomainUrlMap = z.infer<typeof supportedDomainUrlMapSchema>;
+export type RepoRaw = z.infer<typeof repoSchema>;
+export type ErrorMessage = z.infer<typeof errorSchema>;
