7f7367329ce047f6bd9d569d037f766010ed26de
Marcus Tang <txk.marcus@gmail.com>
2023-02-14 11:57:54 +0800

    Fix batch pipe bug

--- config/checks/check-eof-newline.bat
+++ config/checks/check-eof-newline.bat
@@ -1,23 +1,24 @@
 @echo
-setlocal enabledelayedexpansion
 
+set temp=check-eof-newline~%RANDOM%.tmp
+
+git grep --cached -I -l -e "*" -- "../../" > %temp%
+
+setlocal enabledelayedexpansion
 set ret=0
 
-git grep --cached -I -l -e "*" -- "../../" > temp.txt
 
-for /f "delims=" %%F in (temp.txt) do @(
-       rem /* Count the number of lines that contain zero or more characters at the end;
-       rem    this is true for every line except for the last when it is not terminated
-       rem    by a line-break, because the `$` is anchored to such: */
+rem Count the number of lines that contain zero or more characters at the end;
+rem this is true for every line except for the last when it is not terminated
+rem by a line-break, because the `$` is anchored to such:
 
+for /f "delims=" %%F in (%temp%) do @(
        set file=%%F
        set "file=!file:/=\!"
-
        for /F %%D in ('findstr ".*$" "!file!" ^| find /C /V ""') do (
-           rem // Count the total number of lines in the file:
+           rem Count the total number of lines in the file and compare
            for /F %%C in ('^< "!file!" find /C /V ""') do (
                (
-                   rem // Compare the line counts and conditionally append a line-break:
                    if %%D lss %%C (
                     echo ERROR:!file!:%%C: no newline at EOF.
                     set ret=1
--- config/checks/check-line-endings.bat
+++ config/checks/check-line-endings.bat
@@ -1,13 +1,16 @@
 @echo off
-setlocal enabledelayedexpansion
 
+set temp=check-line-endings~%RANDOM%.tmp
+
+git grep --cached -I -n --no-color -P "\r$" -- ":/" > %temp%
+
+setlocal enabledelayedexpansion
 set ret=0
 
-git grep --cached -I -n --no-color -P "\r$" -- "../../" | (
-    for /f "tokens=1,2 delims=:" %%A in ('more') do @(
-        echo ERROR:%%A:%%B: prohibited \r\n line ending, use \n instead.
-        set ret=1
-    )
+for /f "tokens=1,2 delims=:" %%A in (%temp%) do @(
+    echo ERROR:%%A:%%B: prohibited \r\n line ending, use \n instead.
+    set ret=1
 )
 
+del %temp%
 exit /b !ret!
