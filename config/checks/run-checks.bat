e9c10dd2cab249818746654aee0db1effa248cfe
Marcus Tang <txk.marcus@gmail.com>
2023-02-25 02:41:56 +0800

    Add double quotes for filepath

--- config/checks/run-checks.bat
+++ config/checks/run-checks.bat
@@ -4,9 +4,9 @@ rem Runs all check-*.bat scripts, and returns a non-zero exit code if any of the
 setlocal enabledelayedexpansion
 set ret=0
 
-for /R %%X in (check-*.bat) do (
- call %%X
+for %%X in (check-*.bat) do (
+ call "%%X"
  if ERRORLEVEL 1 set ret=1
 )
-
+echo bye
 exit /b !ret!
