97c4329dff9d9662f22d02cbe85cdc605f63a1a5
Charisma Kausar <charisma.kausar@gmail.com>
2023-03-03 22:48:14 +0800

    Add tests for merge commits in zoom view

--- frontend/cypress/tests/zoomView/zoomView_mergeCommits.js
+++ frontend/cypress/tests/zoomView/zoomView_mergeCommits.js
@@ -1,9 +1,30 @@
 describe('merge commits', () => {
     it('show merge commits when all file types selected', () => {
-      // open the commit panel
+      // open the commits panel
       cy.get('.icon-button.fa-list-ul')
         .should('be.visible')
         .first()
         .click();
+
+      // check if the icon for merge commits is visible
+      cy.get('.code-merge-icon')
+        .should('exist');
+    });
+
+    it('hide merge commits when some file types unselected', () => {
+      // open the commits panel
+      cy.get('.icon-button.fa-list-ul')
+        .should('be.visible')
+        .first()
+        .click();
+
+      // uncheck the java file type
+      cy.get('#tab-zoom .fileTypes input[value="java"]')
+        .uncheck()
+        .should('not.be.checked');
+      
+      // check if the icon for merge commits is not visible
+      cy.get('.code-merge-icon')
+        .should('not.exist');
     });
 });
