85b36a00af949840933335be98bd69f5c4aa44fa
Charisma Kausar <charisma.kausar@gmail.com>
2023-03-04 01:24:39 +0800

    Add tests for merge commits in chart view

---
+++ frontend/cypress/tests/chartView/chartView_mergeCommits.js
@@ -0,0 +1,26 @@
+describe('include merge commits in chart view', () => {  
+  it('show merge commits in summary chart', () => {
+    // ramp chart should have merge commit slices
+    cy.get('[title="[2023-03-03] Merge branch \'new-branch\' into cypress: +0 -0 lines "]')
+      .should('have.class', 'ramp__slice')
+      .should('exist')
+  });
+
+  it('hide merge commits in summary chart when some file types unselected', () => {
+    // check breakdown by file type
+    cy.get('#summary label.filter-breakdown input:visible')
+      .should('be.visible')
+      .check()
+      .should('be.checked');
+
+    // uncheck gradle file type
+    cy.get('#summary div.fileTypes input[id="gradle"]')
+      .uncheck()
+      .should('not.be.checked');
+
+    // ramp chart should not have merge commit slices
+    cy.get('[title="[2023-03-03] Merge branch \'new-branch\' into cypress: +0 -0 lines "]')
+      .should('not.exist')
+  });
+});
+  
