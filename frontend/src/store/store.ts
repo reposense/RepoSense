e3f84ac8c7cd40863918a75b63fa800edb2e904f
vvidday <davids89034@gmail.com>
2023-03-11 23:52:48 +0800

    Convert store.js to TypeScript

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,55 +1,62 @@
 import { createStore } from 'vuex';
+import { AuthorshipFile, CommitResult, DailyCommit } from '../types/types';
+import {
+  AuthorshipInfo,
+  StoreState,
+  SummaryDates,
+  ZoomInfo,
+} from '../types/vuex.d';
 
 export default createStore({
   state: {
-    tabAuthorshipInfo: {},
-    tabZoomInfo: {},
-    summaryDates: {},
+    tabAuthorshipInfo: {} as AuthorshipInfo,
+    tabZoomInfo: {} as ZoomInfo,
+    summaryDates: {} as SummaryDates,
     mergedGroups: [],
     fileTypeColors: {},
     loadingOverlayCount: 0,
     loadingOverlayMessage: '',
     isTabActive: true,
-  },
+  } as StoreState,
   mutations: {
-    updateTabZoomInfo(state, info) {
+    updateTabZoomInfo(state, info: ZoomInfo) {
       state.tabZoomInfo = info;
     },
-    updateTabAuthorshipInfo(state, info) {
+    updateTabAuthorshipInfo(state, info: AuthorshipInfo) {
       state.tabAuthorshipInfo = info;
     },
-    updateSummaryDates(state, info) {
+    updateSummaryDates(state, info: SummaryDates) {
       state.summaryDates = info;
     },
-    updateFileTypeColors(state, info) {
+    updateFileTypeColors(state, info: { [key: string]: string }) {
       state.fileTypeColors = info;
     },
-    updateMergedGroup(state, info) {
+    updateMergedGroup(state, info: string[]) {
       state.mergedGroups = info;
     },
-    incrementLoadingOverlayCount(state, increment) {
+    incrementLoadingOverlayCount(state, increment: number) {
       state.loadingOverlayCount += increment;
       if (state.loadingOverlayCount === 0) {
         state.loadingOverlayMessage = 'Loading. Please wait...';
       }
     },
-    updateLoadingOverlayMessage(state, message) {
+    updateLoadingOverlayMessage(state, message: string) {
       state.loadingOverlayMessage = message;
     },
-    updateTabState(state, isTabOpen) {
+    updateTabState(state, isTabOpen: boolean) {
       state.isTabActive = isTabOpen;
-      window.addHash('tabOpen', isTabOpen);
+      window.addHash('tabOpen', isTabOpen.toString());
       if (!isTabOpen) {
         window.removeHash('tabType');
       }
       window.encodeHash();
     },
-    toggleZoomCommitMessageBody(_, slice) {
+    toggleZoomCommitMessageBody(_, slice: CommitResult) {
       if (slice.isOpen !== undefined) {
         slice.isOpen = !slice.isOpen;
       }
     },
-    setAllZoomCommitMessageBody(_, { isOpen, commits }) {
+    setAllZoomCommitMessageBody(_, { isOpen, commits }: { isOpen: boolean; commits: DailyCommit[] }) {
       commits.forEach((commit) => {
         commit.commitResults.forEach((slice) => {
           if (slice.isOpen !== undefined) {
@@ -58,14 +65,14 @@ export default createStore({
         });
       });
     },
-    updateTabAuthorshipFiles(state, files) {
+    updateTabAuthorshipFiles(state, files: AuthorshipFile[]) {
       state.tabAuthorshipInfo.files.splice(0, state.tabAuthorshipInfo.files.length, ...files);
     },
-    toggleAuthorshipFileActiveProperty(_, file) {
+    toggleAuthorshipFileActiveProperty(_, file: AuthorshipFile) {
       file.active = !file.active;
       file.wasCodeLoaded = file.wasCodeLoaded || file.active;
     },
-    setAllAuthorshipFileActiveProperty(_, { isActive, files }) {
+    setAllAuthorshipFileActiveProperty(_, { isActive, files }: { isActive: boolean; files: AuthorshipFile[] }) {
       files.forEach((file) => {
         file.active = isActive;
         file.wasCodeLoaded = file.wasCodeLoaded || file.active;
@@ -75,7 +82,7 @@ export default createStore({
   actions: {
     // Actions are called with dispatch
 
-    async incrementLoadingOverlayCountForceReload({ commit }, increment) {
+    async incrementLoadingOverlayCountForceReload({ commit }, increment: number) {
       commit('incrementLoadingOverlayCount', increment);
       await new Promise(window.requestAnimationFrame);
       await new Promise(window.requestAnimationFrame);
