36711548a81773b91cbc429a5cdcdf6c64d0ab42
Charisma Kausar <charisma.kausar@gmail.com>
2023-03-12 21:23:19 +0800

    Change type of knownAuthor in segment.ts

--- frontend/src/utils/segment.ts
+++ frontend/src/utils/segment.ts
@@ -1,11 +1,11 @@
 export default class Segment {
-  knownAuthor?: string;
+  knownAuthor: string | null;
 
   lineNumbers: Array<number>;
 
   lines: Array<string>;
 
-  constructor(knownAuthor: string, lineNumbers: Array<number>, lines: Array<string>) {
+  constructor(knownAuthor: string | null, lineNumbers: Array<number>, lines: Array<string>) {
     this.knownAuthor = knownAuthor;
     this.lineNumbers = lineNumbers;
     this.lines = lines;
