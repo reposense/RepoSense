a5d904e434a10b3515539a0ecba1ba40bc8326a5
Charisma Kausar <charisma.kausar@gmail.com>
2023-03-13 04:05:41 +0800

    Add more ramp chart tests

--- frontend/cypress/tests/zoomView/zoomView_rampChart.js
+++ frontend/cypress/tests/zoomView/zoomView_rampChart.js
@@ -21,7 +21,7 @@ describe('show ramp chart for period', () => {
       });
   });
 
-  it('show ramp chart for selected commits when data range changed', () => {
+  it('show ramp chart for selected commits when date range changed', () => {
     // change since date
     cy.get('input[name="since"]:visible')
       .type('2018-06-10');
@@ -73,4 +73,139 @@ describe('show ramp chart for period', () => {
           .should('have.length', $length);
       });
   });
+
+  it('ramps should be between start date and end date', () => {
+    // change since date
+    cy.get('input[name="since"]:visible')
+      .type('2018-06-10');
+
+    // change until date
+    cy.get('input[name="until"]:visible')
+      .type('2019-06-10');
+
+    // open the commit panel
+    cy.get('.icon-button.fa-list-ul')
+      .should('be.visible')
+      .first()
+      .click();
+
+    // ramp chart should be visible
+    cy.get('#tab-zoom > .ramp')
+      .should('be.visible');
+
+    // first ramp should be for commit after start date
+    cy.get('#tab-zoom .ramp .ramp__slice')
+      .last()
+      .invoke('attr', 'title')
+      .should('eq', '[2018-06-12] Setup AppVeyor CI (#142): +19 -0 lines ')
+
+    // last ramp should be for commit before end date
+    cy.get('#tab-zoom .ramp .ramp__slice')
+      .first()
+      .invoke('attr', 'title')
+      .should('eq', '[2019-03-25] [#622] CsvParser#parse: fix error handling of `processLine` (#623): +30 -10 lines ')
+
+  });
+
+  it('ramp should have expected properties', () => {
+    // change until date
+    cy.get('input[name="until"]:visible')
+      .type('2019-06-10');
+
+    // open the commit panel
+    cy.get('.icon-button.fa-list-ul')
+      .should('be.visible')
+      .first()
+      .click();
+
+    // ramp chart should be visible
+    cy.get('#tab-zoom > .ramp')
+      .should('be.visible');
+
+    // last ramp should have expected z-index
+    cy.get('#tab-zoom .ramp .ramp__slice')
+      .first()
+      .should('have.css', 'z-index', '39');
+
+    // last ramp should have expected width
+    cy.get('#tab-zoom .ramp .ramp__slice')
+      .first()
+      .should('have.css', 'border-left-width', '7px');
+
+    // last ramp should be at expected location
+    cy.get('#tab-zoom .ramp .ramp__slice')
+      .first()
+      .should('have.css', 'right', '84.0078px');
+
+  });
+
+  it('ramps should have expected relative properties', () => {
+    // change until date
+    cy.get('input[name="until"]:visible')
+      .type('2019-06-10');
+
+    // open the commit panel
+    cy.get('.icon-button.fa-list-ul')
+      .should('be.visible')
+      .first()
+      .click();
+
+    // ramp chart should be visible
+    cy.get('#tab-zoom > .ramp')
+      .should('be.visible');
+
+    // last 2 ramps should have expected relative z-indices
+    cy.get('#tab-zoom .ramp .ramp__slice')
+      .first()
+      .then($el => $el.css('z-index'))  
+      .then(parseInt) // get 1st z-index
+      .then(index1 => {
+        cy.get('#tab-zoom .ramp .ramp__slice')
+          .eq(1)
+          .then($el => $el.css('z-index')) 
+          .then(parseInt) // get 2nd z-index
+          .then(index2 => {
+            expect(index1).to.be.gt(index2)
+          })
+      })
+
+    // last 2 ramps should have expected relative distances
+    cy.get('#tab-zoom .ramp .ramp__slice')
+      .first()
+      .then($el => $el.css('right'))  
+      .then(parseFloat) // get 1st distance
+      .then(distance1 => {
+        cy.get('#tab-zoom .ramp .ramp__slice')
+          .eq(1)
+          .then($el => $el.css('right')) 
+          .then(parseFloat) // get 2nd distance
+          .then(distance2 => {
+            expect(distance1).to.be.lt(distance2)
+          })
+      })
+  });
+
+  it('ramp should link to commit webpage', () => {
+    // change until date
+    cy.get('input[name="until"]:visible')
+      .type('2019-06-10');
+
+    // open the commit panel
+    cy.get('.icon-button.fa-list-ul')
+      .should('be.visible')
+      .first()
+      .click();
+
+    // ramp chart should be visible
+    cy.get('#tab-zoom > .ramp')
+      .should('be.visible');
+
+    // last ramp should have expected link
+    cy.get('#tab-zoom .ramp .ramp__slice')
+      .first()
+      .invoke('attr', 'href')
+      .should('eq', 'https://github.com/reposense/RepoSense/commit/19e32944d4c67ed0b6bdc5697016536cc9e17ed9');
+
+  });
+
 });
