c2ac5c8520260f28a68cdd4b92a4f6840b225e16
Marcus <txk.marcus@gmail.com>
2023-03-20 16:55:12 +0800

    Update router to use lazy loading

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/c-home.vue';
-import Widget from '../views/c-widget-home.vue';
 
 const routes = [
   {
@@ -9,11 +8,10 @@ const routes = [
   },
   {
     path: '/widget',
-    component: Widget,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/c-about.vue'),
+    component: () => import('../views/c-widget-home.vue'),
   },
 ];
 
