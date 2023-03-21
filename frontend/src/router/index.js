32f84a0a9c838c32a03b9383069f3d68c9ebe59b
Marcus Tang <txk.marcus@gmail.com>
2023-03-21 12:05:07 +0800

    Refactor out common code for c-summary into single file

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../views/c-widget-home.vue'),
+    component: () => import('../views/c-widget.vue'),
   },
 ];
 
