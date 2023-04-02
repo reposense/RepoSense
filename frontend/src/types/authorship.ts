ce7d7f791ec79fe4bb04b05e7bada4dc57f188d0
vvidday <davids89034@gmail.com>
2023-04-02 22:01:08 +0800

    Add enum types for authorship sort and filter options

---
+++ frontend/src/types/authorship.ts
@@ -0,0 +1,11 @@
+export enum FilesSortType {
+  LineOfCode = 'lineOfCode',
+  Path = 'path',
+  FileName = 'fileName',
+  FileType = 'fileType',
+}
+
+export enum FilterType {
+  Checkboxes = 'checkboxes',
+  Search = 'search',
+}
