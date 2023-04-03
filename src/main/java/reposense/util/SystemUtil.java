b41def3f5e7b58f0c3cf7598de82406d221e2363
sikai00 <sikai4111@gmail.com>
2023-04-03 16:44:25 +0800

    Remove unused method

--- src/main/java/reposense/util/SystemUtil.java
+++ src/main/java/reposense/util/SystemUtil.java
@@ -1,8 +1,5 @@
 package reposense.util;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 /**
  * Contains system related functionalities.
  */
@@ -13,18 +10,6 @@ public class SystemUtil {
      */
     public static boolean isWindows() {
         return (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0);
-    }
-
-    /**
-     * Returns true if the given String is a valid URL.
-     */
-    public static boolean isValidUrl(String possibleUrl) {
-        try {
-            new URL(possibleUrl);
-        } catch (MalformedURLException mue) {
-            return false;
-        }
-        return true;
     }
 
     /**
