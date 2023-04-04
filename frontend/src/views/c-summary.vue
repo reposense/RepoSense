fc393da1f877a9a673037066d7c192a803eb8dba
Marcus Tang <txk.marcus@gmail.com>
2023-04-04 22:56:11 +0800

    Fix user.checkedFileTypeContribution possibly being undefined

--- frontend/src/views/c-summary.vue
+++ frontend/src/views/c-summary.vue
@@ -219,7 +219,8 @@ export default defineComponent({
           if (user.checkedFileTypeContribution === undefined) {
             this.updateCheckedFileTypeContribution(user);
           }
-          if (user.checkedFileTypeContribution > 0) {
+
+          if (user.checkedFileTypeContribution && user.checkedFileTypeContribution > 0) {
             totalCount += 1;
             totalLines += user.checkedFileTypeContribution;
           }
@@ -546,7 +547,7 @@ export default defineComponent({
 
         this.mergeFileTypeContribution(user, mergedFileTypeContribution);
 
-        totalMergedCheckedFileTypeCommits += user.checkedFileTypeContribution;
+        totalMergedCheckedFileTypeCommits += user.checkedFileTypeContribution || 0;
         mergedVariance += user.variance;
       });
       mergedCommits.sort(window.comparator((ele) => ele.date));
