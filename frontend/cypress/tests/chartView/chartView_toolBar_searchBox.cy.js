ba4f45ca09c4fecf47a7f004ae1470f13c1993ba
Marcus Tang <txk.marcus@gmail.com>
2023-04-08 18:00:04 +0800

    Fix chartView_toolBar_searchBox

--- frontend/cypress/tests/chartView/chartView_toolBar_searchBox.cy.js
+++ frontend/cypress/tests/chartView/chartView_toolBar_searchBox.cy.js
@@ -5,11 +5,6 @@ describe('search bar', () => {
       .type('abcdef')
       .type('{enter}');
 
-    // Enter does not work. Related issue: https://github.com/cypress-io/cypress/issues/3405
-    // Let's manually submit form
-    cy.get('#summary-wrapper form.summary-picker')
-      .submit();
-
     cy.get('#summary-wrapper #summary-charts').then(($ele) => {
       const content = $ele.html();
 
@@ -22,11 +17,6 @@ describe('search bar', () => {
     cy.get('#summary-wrapper input[type=text]')
       .type('Yong Hao TENG')
       .type('{enter}');
-
-    // Enter does not work. Related issue: https://github.com/cypress-io/cypress/issues/3405
-    // Let's manually submit form
-    cy.get('#summary-wrapper form.summary-picker')
-      .submit();
 
     cy.get('#summary-wrapper #summary-charts').then(($ele) => {
       const children = $ele.children().length;
