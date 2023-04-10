7610f3f88adb586e8cb5ef69893ded42bcf3c4fc
Marcus Tang <50147457+MarcusTXK@users.noreply.github.com>
2023-04-10 20:00:01 +0800

    [#1909] Refactor CliArguments and subclasses for polymorphism (#1942)
    Currently, ArgsParser::parse does the following (1) sanitizes and 
    formats the inputs (2) decides whether the reposense run should be a 
    simple view, a CLI run or a config file run. After returning an instance
    of CliArguments, RepoSense main class decides based on the class type
    how to construct the corresponding repoConfigurations and 
    groupConfigurations. Once the repoConfigurations and groupConfigurations
    are setup, it no longer matters whether it is a CLI run or config file
    run. 
    
    Refactoring the code will allow us to reduce the complexity of the 
    ArgsParser::parse method and allow for a slightly more extensible 
    approach to implementing possible new run configurations. For example, a
    class can be added to decide the appropriate run configuration for the
    CliArguments which then generates the appropriate Config files. 
    
    Using the builder pattern for the construction of CliArguments will also
    help to avoid a bloated constructor and optionally initialize fields. 
    
    Let's encapsulate specific run configuration behavior (CLI or config) to
    a specific class which makes it easier to maintain as well as adopt the
    builder pattern for CliArguments.

---
+++ src/test/java/reposense/model/RunConfigurationDeciderTest.java
@@ -0,0 +1,83 @@
+package reposense.model;
+
+import static org.apache.tools.ant.types.Commandline.translateCommandline;
+import static reposense.model.RunConfigurationDecider.getRunConfiguration;
+import static reposense.util.TestUtil.loadResource;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import reposense.parser.ArgsParser;
+import reposense.parser.ArgsParserTest;
+import reposense.util.FileUtil;
+import reposense.util.InputBuilder;
+
+
+
+
+public class RunConfigurationDeciderTest {
+    private static final Path PROJECT_DIRECTORY = Paths.get(System.getProperty("user.dir"));
+    private static final Path CONFIG_FOLDER_ABSOLUTE = loadResource(ArgsParserTest.class, "cli_location_test");
+
+    private static final String NONEXISTENT_DIRECTORY = "some_non_existent_dir/";
+
+    private static final InputBuilder DEFAULT_INPUT_BUILDER = new InputBuilder();
+
+    private static final String TEST_REPO_REPOSENSE = "https://github.com/reposense/RepoSense.git";
+    private static final String TEST_REPO_BETA = "https://github.com/reposense/testrepo-Beta.git";
+
+
+    @BeforeEach
+    public void before() {
+        DEFAULT_INPUT_BUILDER.reset().addConfig(CONFIG_FOLDER_ABSOLUTE);
+    }
+
+    @AfterEach
+    public void after() {
+        try {
+            FileUtil.deleteDirectory(PROJECT_DIRECTORY.resolve(NONEXISTENT_DIRECTORY).toString());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void parse_addConfig_returnsConfigRunConfiguration() throws Exception {
+        String input = new InputBuilder()
+                .addConfig(CONFIG_FOLDER_ABSOLUTE)
+                .addSinceDate("01/07/2017")
+                .addUntilDate("30/11/2017")
+                .addView()
+                .build();
+        CliArguments cliArguments = ArgsParser.parse(translateCommandline(input));
+        RunConfiguration runConfiguration = getRunConfiguration(cliArguments);
+        Assertions.assertTrue(runConfiguration instanceof ConfigRunConfiguration);
+    }
+
+    @Test
+    public void parse_addRepos_returnsCliRunConfiguration() throws Exception {
+        String input = new InputBuilder().addRepos(TEST_REPO_REPOSENSE, TEST_REPO_BETA)
+                .build();
+        CliArguments cliArguments = ArgsParser.parse(translateCommandline(input));
+        RunConfiguration runConfiguration = getRunConfiguration(cliArguments);
+        Assertions.assertTrue(runConfiguration instanceof CliRunConfiguration);
+    }
+
+    @Test
+    public void parse_reposAndConfigNotSpecified_returnsConfigRunConfiguration() throws Exception {
+        String input = new InputBuilder()
+                .addSinceDate("01/07/2017")
+                .addUntilDate("30/11/2017")
+                .addView()
+                .build();
+        CliArguments cliArguments = ArgsParser.parse(translateCommandline(input));
+        RunConfiguration runConfiguration = getRunConfiguration(cliArguments);
+        Assertions.assertTrue(runConfiguration instanceof ConfigRunConfiguration);
+    }
+}
