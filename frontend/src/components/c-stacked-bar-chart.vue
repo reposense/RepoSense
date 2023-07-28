a9f102d39e912461d8e9caadac52e4395431c2f3
Seah Shau Chung Nicholas <97350362+nseah21@users.noreply.github.com>
2023-07-28 09:14:23 +0800

    [#1899] Add graphic for commit diffstat (#2010)
    A diffstat graphic is useful in providing a visual representation
    of the changes within and between commits. 
    
    Let's add a diffstat graphic emulating the diffstat used by cgit, 
    to help users have a high-level overview of the impact of a
    commit.

---
+++ frontend/src/components/c-stacked-bar-chart.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+.stacked-bar-container
+  .stacked-bar__contrib--bar(
+    v-for="bar in bars",
+    v-bind:title="bar.tooltipText",
+    v-bind:style="{ width: `${bar.width}%`,\
+      'background-color': bar.color }"
+  )
+</template>
+
+<script lang="ts">
+import { PropType, defineComponent } from 'vue';
+import { Bar } from '../types/types';
+
+export default defineComponent({
+  props: {
+    bars: {
+      type: Array as PropType<Bar[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import '../styles/_colors.scss';
+
+.stacked-bar-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.stacked-bar__contrib--bar {
+  background-color: mui-color('blue');
+  height: 4px;
+  margin-top: 2px;
+}
+
+</style>
