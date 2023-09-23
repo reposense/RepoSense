57de7d2d6140aefb61c0c6a84661f1de9dd53e44
jq1836 <95712150+jq1836@users.noreply.github.com>
2023-09-23 14:20:24 +0800

    Migrate c-ramp.vue to typescript

--- frontend/src/components/c-ramp.vue
+++ frontend/src/components/c-ramp.vue
@@ -23,7 +23,7 @@
       .ramp__slice(
         draggable="false",
         v-for="(slice, j) in user.commits",
-        v-bind:title="getContributionMessage(slice)",
+        v-bind:title="getContributionMessage(slice, null)",
         v-on:click="openTabZoom(user, slice, $event)",
         v-bind:class="`ramp__slice--color${getSliceColor(slice)}`",
         v-bind:style="{\
@@ -34,11 +34,13 @@
       )
 </template>
 
-<script>
+<script lang='ts'>
+import { defineComponent } from 'vue';
 import brokenLinkDisabler from '../mixin/brokenLinkMixin';
 import User from '../utils/user';
+import { Commit, CommitResult } from '../types/types';
 
-export default {
+export default defineComponent({
   name: 'c-ramp',
   mixins: [brokenLinkDisabler],
   props: {
@@ -85,24 +87,32 @@ export default {
   },
   data() {
     return {
-      rampSize: 0.01,
-      mergeCommitRampSize: this.rampSize * 20,
-      deletesContributionRampSize: this.rampSize * 20,
+      rampSize: 0.01 as number,
     };
   },
 
+  computed: {
+    mergeCommitRampSize(): number {
+      return this.rampSize * 20;
+    },
+    deletesContributionRampSize(): number {
+      return this.rampSize * 20;
+    },
+  },
+
   methods: {
-    getLink(commit) {
+    getLink(commit: CommitResult) {
       return window.getCommitLink(commit.repoId, commit.hash);
     },
-    getContributions(commit) {
+    getContributions(commit: CommitResult | Commit) {
       return commit.insertions + commit.deletions;
     },
-    isDeletesContribution(commit) {
+    isDeletesContribution(commit: CommitResult | Commit) {
       return commit.insertions === 0 && commit.deletions > 0;
     },
-    getWidth(slice) {
-      if (slice.isMergeCommit) {
+    getWidth(slice: CommitResult | Commit) {
+      // Check if slice contains 'isMergeCommit' attribute
+      if ('isMergeCommit' in slice && slice.isMergeCommit) {
         return this.mergeCommitRampSize;
       }
       if (this.getContributions(slice) === 0) {
@@ -111,22 +121,23 @@ export default {
       if (this.isDeletesContribution(slice)) {
         return this.deletesContributionRampSize;
       }
-      const newSize = 100 * (slice.insertions / this.avgsize);
+      // '+' unary operator here attempts to convert this.avgsize to number, if it is not already one
+      const newSize = 100 * (slice.insertions / +this.avgsize);
       return Math.max(newSize * this.rampSize, 0.5);
     },
-    getContributionMessage(slice, commit) {
-      let title = '';
+    getContributionMessage(slice: Commit, commit: CommitResult) {
       if (this.tframe === 'commit') {
         return `[${slice.date}] ${commit.messageTitle}: +${commit.insertions} -${commit.deletions} lines `;
       }
 
-      title = this.tframe === 'day'
-            ? `[${slice.date}] Daily `
-            : `[${slice.date} till ${slice.endDate}] Weekly `;
+      let title = this.tframe === 'day'
+                ? `[${slice.date}] Daily `
+                : `[${slice.date} till ${slice.endDate}] Weekly `;
       title += `contribution: +${slice.insertions} -${slice.deletions} lines`;
       return title;
     },
-    openTabZoom(user, slice, evt) {
+    // Only to be used when this.tframe !== 'Commit'
+    openTabZoom(user: User, slice: Commit, evt: MouseEvent) {
       // prevent opening of zoom tab when cmd/ctrl click
       if (window.isMacintosh ? evt.metaKey : evt.ctrlKey) {
         return;
@@ -155,27 +166,27 @@ export default {
     },
 
     // position for commit granularity
-    getCommitPos(i, total) {
+    getCommitPos(i: number, total: number) {
       return (((total - i - 1) * window.DAY_IN_MS) / total)
           / (this.getTotalForPos(this.sdate, this.udate) + window.DAY_IN_MS);
     },
     // position for day granularity
-    getSlicePos(date) {
+    getSlicePos(date: string) {
       const total = this.getTotalForPos(this.sdate, this.udate);
-      return (new Date(this.udate) - new Date(date)) / (total + window.DAY_IN_MS);
+      return (new Date(this.udate).getMilliseconds() - new Date(date).getMilliseconds()) / (total + window.DAY_IN_MS);
     },
 
     // get duration in miliseconds between 2 date
-    getTotalForPos(sinceDate, untilDate) {
-      return new Date(untilDate) - new Date(sinceDate);
+    getTotalForPos(sinceDate: string, untilDate: string) {
+      return new Date(untilDate).getMilliseconds() - new Date(sinceDate).getMilliseconds();
     },
-    getRampColor(commit, slice) {
+    getRampColor(commit: CommitResult, slice: Commit) {
       if (this.isDeletesContribution(commit)) {
         return '-deletes';
       }
       return this.getSliceColor(slice);
     },
-    getSliceColor(slice) {
+    getSliceColor(slice: Commit) {
       if (this.isDeletesContribution(slice)) {
         return '-deletes';
       }
@@ -187,7 +198,7 @@ export default {
     },
 
     // Prevent browser from switching to new tab when clicking ramp
-    rampClick(evt) {
+    rampClick(evt: MouseEvent) {
       const isKeyPressed = window.isMacintosh ? evt.metaKey : evt.ctrlKey;
       if (isKeyPressed) {
         evt.preventDefault();
@@ -202,7 +213,7 @@ export default {
       return undefined;
     },
   },
-};
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
