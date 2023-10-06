ab9c745eea21af8686f2dd88e8983ee8b9d22562
chan-j-d <chan.j.d.98@gmail.com>
2023-10-06 13:59:14 +0800

    Fix broken test case due to older json file used in test repo delta

--- src/main/java/reposense/model/StandaloneAuthor.java
+++ src/main/java/reposense/model/StandaloneAuthor.java
@@ -2,19 +2,26 @@ package reposense.model;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Represents an author in {@link StandaloneConfig}.
  */
 public class StandaloneAuthor {
+
     private String gitId;
+    // This is for backward compatibility with older standalone author configs that utilize the 'githubId' keyword.
+    private String githubId;
+
     private List<String> emails;
     private String displayName;
     private List<String> authorNames;
     private List<String> ignoreGlobList;
 
     public String getGitId() {
-        return gitId;
+        // This supports a standalone json file containing either keywords, prioritizing 'gitId'.
+        // To remove backward compatibility support, need to update the standalone json configs in test repositories.
+        return Optional.ofNullable(gitId).orElse(githubId);
     }
 
     public List<String> getEmails() {
