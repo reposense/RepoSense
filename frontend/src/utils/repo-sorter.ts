b10e08fd22713903cdafaf9ce0bb6bc45d0c56c7
jq1836 <95712150+jq1836@users.noreply.github.com>
2023-10-23 17:35:42 +0800

    Apply generics to remove any cast

--- frontend/src/utils/repo-sorter.ts
+++ frontend/src/utils/repo-sorter.ts
@@ -1,37 +1,52 @@
 import { User } from '../types/types';
 import { FilterGroupSelection } from '../types/summary';
 
-function getTotalCommits(total: number, group: User) {
+function getTotalCommits(total: number, group: User): number {
   // If group.checkedFileTypeContribution === undefined, then we treat it as 0 contribution
   return total + (group.checkedFileTypeContribution ?? 0);
 }
 
-function getGroupCommitsVariance(total: number, group: User) {
+function getGroupCommitsVariance(total: number, group: User): number {
   return total + group.variance;
 }
 
-function checkKeyAndGetValue(element: object, key: string | undefined) {
+function checkKeyAndGetValue<T extends object>(element: T, key?: string) {
+  // invalid key provided
   if (key === undefined || !(key in element)) {
     return undefined;
   }
-  /** Casting here is safe as guard clause above ensures that element has an attribute with the same name as the string
-   *  represented by key. */
-  return (element as any)[key];
+  return element[key as keyof T];
 }
 
-/** Array is not sorted when sortingOption is not provided. sortingOption is optional to allow it to fit the
- *  SortingFunction<T> interface. */
+/**
+ * Returns an empty string if an invalid/no key is provided or if the value retrieved is not a ComparablePrimitive.
+ * This permits and results in no sorting being done in the above cases. If function is to be made stricter, assertions
+ * or errors may be thrown where empty strings are returned.
+ */
+function getComparablePrimitive(element: User, key?: string): string | number {
+  const val = checkKeyAndGetValue(element, key);
+  // value retrieved is not a comparable primitive
+  if (typeof val !== 'string' && typeof val !== 'number') {
+    return '';
+  }
+  return val;
+}
+
+/**
+ * Array is not sorted when sortingOption is not provided. sortingOption is optional to allow it to fit the
+ * SortingFunction<T> interface.
+ * */
 function sortingHelper(element: User[], sortingOption?: string): string | number {
-  if (sortingOption === 'totalCommits') {
+  switch (sortingOption) {
+  case 'totalCommits':
     return element.reduce(getTotalCommits, 0);
-  }
-  if (sortingOption === 'variance') {
+  case 'variance':
     return element.reduce(getGroupCommitsVariance, 0);
-  }
-  if (sortingOption === 'displayName') {
+  case 'displayName':
     return window.getAuthorDisplayName(element);
+  default:
+    return getComparablePrimitive(element[0], sortingOption);
   }
-  return checkKeyAndGetValue(element[0], sortingOption);
 }
 
 function groupByRepos(
@@ -41,7 +56,7 @@ function groupByRepos(
     sortingWithinOption: string;
     isSortingDsc: string;
     isSortingWithinDsc: string; },
-) {
+): User[][] {
   const sortedRepos: User[][] = [];
   const {
     sortingWithinOption, sortingOption, isSortingDsc, isSortingWithinDsc,
@@ -52,7 +67,7 @@ function groupByRepos(
     if (sortWithinOption === 'totalCommits') {
       users.sort(window.comparator((ele) => ele.checkedFileTypeContribution ?? 0));
     } else {
-      users.sort(window.comparator((ele) => checkKeyAndGetValue(ele, sortWithinOption)));
+      users.sort(window.comparator((ele) => getComparablePrimitive(ele, sortWithinOption)));
     }
 
     if (isSortingWithinDsc) {
@@ -72,7 +87,7 @@ function groupByNone(
   sortingControl: {
     sortingOption: string;
     isSortingDsc: string; },
-) {
+): User[] {
   const sortedRepos: User[] = [];
   const { sortingOption, isSortingDsc } = sortingControl;
   const isSortingGroupTitle = sortingOption === 'groupTitle';
@@ -86,9 +101,9 @@ function groupByNone(
       return `${repo.searchPath}${repo.name}`;
     }
     if (sortingOption === 'totalCommits') {
-      return repo.checkedFileTypeContribution;
+      return repo.checkedFileTypeContribution ?? 0;
     }
-    return checkKeyAndGetValue(repo, sortingOption);
+    return getComparablePrimitive(repo, sortingOption);
   }));
   if (isSortingDsc) {
     sortedRepos.reverse();
@@ -104,7 +119,7 @@ function groupByAuthors(
     sortingWithinOption: string;
     isSortingDsc: string;
     isSortingWithinDsc: string; },
-) {
+): User[][] {
   const authorMap: { [userName: string]: User[] } = {};
   const filtered: User[][] = [];
   const {
@@ -125,7 +140,7 @@ function groupByAuthors(
     if (sortWithinOption === 'totalCommits') {
       authorMap[author].sort(window.comparator((repo) => repo.checkedFileTypeContribution ?? 0));
     } else {
-      authorMap[author].sort(window.comparator((repo) => checkKeyAndGetValue(repo, sortingWithinOption)));
+      authorMap[author].sort(window.comparator((repo) => getComparablePrimitive(repo, sortingWithinOption)));
     }
     if (isSortingWithinDsc) {
       authorMap[author].reverse();
@@ -148,7 +163,7 @@ function sortFiltered(
     sortingWithinOption: string;
     isSortingDsc: string;
     isSortingWithinDsc: string; },
-) {
+): User[][] {
   const { filterGroupSelection } = filterControl;
   let full = [];
 
