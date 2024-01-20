db84b54556969adab08e8f05a4c795401c9c16ac
sopa301 <96387349+sopa301@users.noreply.github.com>
2024-01-20 12:11:22 +0800

    Implement rule to standardise array style

--- frontend/src/utils/segment.ts
+++ frontend/src/utils/segment.ts
@@ -1,11 +1,11 @@
 export default class Segment {
   knownAuthor: string | null;
 
-  lineNumbers: Array<number>;
+  lineNumbers: number[];
 
-  lines: Array<string>;
+  lines: string[];
 
-  constructor(knownAuthor: string | null, lineNumbers: Array<number>, lines: Array<string>) {
+  constructor(knownAuthor: string | null, lineNumbers: number[], lines: string[]) {
     this.knownAuthor = knownAuthor;
     this.lineNumbers = lineNumbers;
     this.lines = lines;
