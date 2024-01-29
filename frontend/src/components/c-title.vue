8cc525fbd0f9922c14ed0622dfa53df84b4c5b4a
sopa301 <96387349+sopa301@users.noreply.github.com>
2024-01-30 01:13:12 +0800

    Add title component

---
+++ frontend/src/components/c-title.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+div(v-html="markdownText")
+</template>
+
+<script lang="ts">
+import MarkdownIt from 'markdown-it';
+import MarkdownItAnchor from 'markdown-it-anchor';
+import MarkdownItHighlightjs from 'markdown-it-highlightjs';
+import MarkdownItTOC from 'markdown-it-toc-done-right';
+import { defineComponent } from 'vue';
+import titleText from '../markdown/title.md';
+
+export default defineComponent({
+  data() {
+    return {
+      markdownText: this.getMarkDownText(),
+    };
+  },
+  methods: {
+    getMarkDownText() {
+      let output = '';
+      try {
+        output = new MarkdownIt()
+          .use(MarkdownItAnchor)
+          .use(MarkdownItHighlightjs)
+          .use(MarkdownItTOC)
+          .render(titleText)
+      } catch (e) {
+        // We don't want to crash the app if the markdown is invalid,
+        // so we just log the error and return an empty string.
+        console.error(e);
+      }
+      return output;
+    },
+  }
+});
+</script>
