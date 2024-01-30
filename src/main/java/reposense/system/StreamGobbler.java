348e2ace2b0c8b2ec0d3ee0582ebbae0fe79c50d
George Tay <george_tay@u.nus.edu>
2024-01-30 14:08:59 +0800

    Fix checkstyle errors and remove unused code

--- src/main/java/reposense/system/StreamGobbler.java
+++ src/main/java/reposense/system/StreamGobbler.java
@@ -1,9 +1,7 @@
 package reposense.system;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
@@ -33,19 +31,6 @@ public class StreamGobbler extends Thread {
     public void run() {
         try {
             StringBuilder sb = new StringBuilder();
-
-//            try (BufferedReader streamReader = new BufferedReader(
-//                    new InputStreamReader(is)
-//            )) {
-//                int c;
-//
-//                while ((c = streamReader.read()) != -1) {
-//                    sb.append((char) c);
-//                }
-//
-//                value = sb.toString();
-//            }
-
             ReadableByteChannel ch = Channels.newChannel(is);
             int len;
             while ((len = ch.read(buffer)) > 0) {
