d2018d246e74fa04ae70d2f1f0f16c9e8e29e187
George Tay <george_tay@u.nus.edu>
2024-02-17 02:58:45 +0800

    [#2076] Refactor RepoConfiguration to simplify constructor complexity (#2078)
    Refactor RepoConfiguration to simplify constructor complexity.
    
    Currently, the constructors for the `RepoConfiguration` class is 
    very complex, involving up to 18 parameters at once, and is 
    not easily extensible.
    
    With the proposed changes, the constructor now follows the 
    Builder pattern, by using a nested class `Builder` that 
    helps to compose config parameters in a way that is much more 
    modular and comprehensible, enhancing code quality and 
    reducing confusion among developers.
    
    However, it must be noted that `Builder` objects are not reusable as of
    now, and that more combinations of arguments are permitted due to the
    removal of constructors for `RepoBuilder`, which previously helped to
    ensure the correctness of parameter combinations.
    
    Let's refactor `RepoConfiguration` to simplify the 
    complexity of the constructor and increase modularity 
    of the code.

---
+++ src/main/java/reposense/parser/ConfigurationBuildException.java
@@ -0,0 +1,8 @@
+package reposense.parser;
+
+/**
+ * Signals that there was an issue building a Configuration (missing parameters, etc.).
+ */
+public class ConfigurationBuildException extends RuntimeException {
+
+}
