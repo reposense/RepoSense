f7d7a6dccb3807b58a48a799b094f8e3d8120976
Ryan Poon <96387349+sopa301@users.noreply.github.com>
2024-02-16 15:04:53 +0800

    Update frontend/src/components/c-title.vue
    Co-authored-by: Charisma Kausar <68203159+ckcherry23@users.noreply.github.com>

--- frontend/src/components/c-title.vue
+++ frontend/src/components/c-title.vue
@@ -32,7 +32,7 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .title {
-  padding: 2rem 1.5rem;
+  padding: 0 1.5rem;
   // This is needed because the parent summary-wrapper center aligns everything
   text-align: initial;
 }
