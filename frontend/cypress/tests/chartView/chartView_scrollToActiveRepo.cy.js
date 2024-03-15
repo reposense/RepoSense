de0db3f5d5eb066b1fc26b822208bc5289cdb427
jonasongg <jonasong2@gmail.com>
2024-03-15 20:07:38 +0800

    add cypress test for scroll to highlighted repo

---
+++ frontend/cypress/tests/chartView/chartView_scrollToActiveRepo.cy.js
@@ -0,0 +1,41 @@
+describe('scroll to active repo', () => {
+  // need to set scrollBehavior to false because the default behavior is to scroll the element into view
+  it('selecting a visible repo should not scroll', { scrollBehavior: false }, () => {
+    cy.get('.icon-button.fa-code')
+      .should('exist')
+      .first()
+      .click();
+
+    cy.get('#summary-wrapper')
+      .first()
+      .then(($el) => {
+        const scrollTop = $el.prop('scrollTop');
+        expect(scrollTop).to.equal(0);
+      });
+  });
+
+  it('selecting a non-visible repo should scroll', () => {
+    cy.get('.icon-button.fa-code')
+      .should('exist')
+      .last()
+      .click();
+
+    cy.get('#summary-wrapper')
+      .first()
+      .then(($el) => {
+        const scrollTop = $el.prop('scrollTop');
+        expect(scrollTop).to.not.equal(0);
+      });
+
+    cy.url()
+      .should('contain', 'tabAuthor=yong24s')
+      .should('contain', 'tabRepo=reposense%2FRepoSense%5Bcypress%5D');
+
+    cy.reload();
+
+    cy.get('.icon-button.fa-code')
+      .should('exist')
+      .last()
+      .should('be.visible');
+  });
+});
