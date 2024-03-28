3f315479e2aed2aa66bb05414d878c4e7d1e0f03
SkyBlaise <zsc990124@gmail.com>
2024-03-28 15:37:48 +0800

    fix failing test case

--- frontend/cypress/tests/codeView/codeView_creditBackgroundColour.cy.js
+++ frontend/cypress/tests/codeView/codeView_creditBackgroundColour.cy.js
@@ -6,12 +6,14 @@ describe('credit background colour', () => {
       .first()
       .click();
 
-    // full credit - #BFF6CF
+    // src/main/java/reposense/model/Author.java
+    // line 9: full credit - #BFF6CF
     cy.get(':nth-child(1) > .file-content > .segment-collection > :nth-child(2) > .java > .code')
       .should('have.css', 'background-color')
       .and('eq', 'rgb(191, 246, 207)');
 
-    // partial credit - #E6FFED
+    // src/main/java/reposense/model/Author.java
+    // line 15: partial credit - #E6FFED
     cy.get(':nth-child(1) > .file-content > .segment-collection > :nth-child(4) > .java > .code')
       .should('have.css', 'background-color')
       .and('eq', 'rgb(230, 255, 237)');
@@ -29,13 +31,23 @@ describe('credit background colour', () => {
       .should('exist')
       .click();
 
-    // full credit - #F0808050
-    cy.get(':nth-child(1) > .file-content > .segment-collection > :nth-child(7) > .scss > .code')
+    // frontend/src/styles/_colors.scss
+    // line 35: full credit - #F0808050
+    cy.get(':nth-child(7) > .scss > :nth-child(1)')
       .should('have.css', 'background-color')
       .and('eq', 'rgba(240, 128, 128, 0.314)');
 
-    // partial credit - #1E90FF20
-    cy.get(':nth-child(3) > .file-content > .segment-collection > :nth-child(10) > .java > .code')
+    // FileInfoExtractor.java is too far away to be loaded, use filter to go to it directly
+    cy.get('#search')
+      .click()
+      .type('FileInfoExtractor.java');
+
+    cy.get('#submit-button')
+      .click();
+
+    // src/main/java/reposense/authorship/FileInfoExtractor.java
+    // line 23: partial credit - #1E90FF20
+    cy.get(':nth-child(10) > .java > :nth-child(1)')
       .should('have.css', 'background-color')
       .and('eq', 'rgba(30, 144, 255, 0.125)');
   });
