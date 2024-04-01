88be92b835d11868a6f4165b5fc1625e9b9710e1
sopa301 <96387349+sopa301@users.noreply.github.com>
2024-04-02 02:42:38 +0800

    Factor out markdown parser

---
+++ frontend/src/components/c-markdown-chunk.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+.markdown(v-html="markdownProcessed", v-if="markdownText != ''")
+</template>
+
+<script lang="ts">
+import MarkdownIt from 'markdown-it';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    markdownText: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    markdownProcessed() {
+      const md = new MarkdownIt({ html: true });
+      return md.render(this.markdownText);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.markdown {
+  overflow-x: auto;
+}
+</style>
