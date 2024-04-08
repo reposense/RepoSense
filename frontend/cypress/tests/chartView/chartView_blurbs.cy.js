48c873b2a8ae9165f9d4be1419f5c0dd8044c27f
sopa301 <96387349+sopa301@users.noreply.github.com>
2024-04-09 00:42:47 +0800

    Add blurbs to frontend files

---
+++ frontend/cypress/tests/chartView/chartView_blurbs.cy.js
@@ -0,0 +1,12 @@
+describe('blurbs', () => {
+  it('shows blurbs', () => {
+    cy.get('.markdown.blurb')
+    .first()
+    .should('contain', 'first blurb');
+  });
+
+  it('has the correct number of valid blurbs', () => {
+    cy.get('.markdown.blurb')
+    .should('have.length', 3);
+  });
+});
