941f4203f7b215ad7d7c4f4a900f1c4b52159754
sopa301 <96387349+sopa301@users.noreply.github.com>
2024-04-09 00:48:42 +0800

    Fix style

--- frontend/cypress/tests/chartView/chartView_blurbs.cy.js
+++ frontend/cypress/tests/chartView/chartView_blurbs.cy.js
@@ -1,12 +1,12 @@
 describe('blurbs', () => {
   it('shows blurbs', () => {
     cy.get('.markdown.blurb')
-    .first()
-    .should('contain', 'first blurb');
+      .first()
+      .should('contain', 'first blurb');
   });
 
   it('has the correct number of valid blurbs', () => {
     cy.get('.markdown.blurb')
-    .should('have.length', 3);
+      .should('have.length', 3);
   });
 });
