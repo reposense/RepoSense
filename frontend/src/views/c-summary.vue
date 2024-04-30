4fe8177d8210a7aa5867358df817f2afb8a7ff94
jonasongg <jonasong2@gmail.com>
2024-04-30 15:19:44 +0800

    remove whitespace

--- frontend/src/views/c-summary.vue
+++ frontend/src/views/c-summary.vue
@@ -441,7 +441,7 @@ export default defineComponent({
       } else {
         removeHash('checkedFileTypes');
       }
-      
+
       if (this.viewRepoTags) {
         addHash('viewRepoTags', 'true');
       } else {
