21f2b69d40cfbe90fc1f83ba733a1976d38e7751
George Tay <george_tay@u.nus.edu>
2024-05-06 19:40:24 +0800

    Implement more test cases

--- src/test/java/reposense/model/BlurbMapTest.java
+++ src/test/java/reposense/model/BlurbMapTest.java
@@ -13,4 +13,21 @@ public class BlurbMapTest {
 
         Assertions.assertEquals(builder.build(), newBuilder.build());
     }
+
+    @Test
+    public void blurbBuilder_testIfBuildsEmpty_success() {
+        BlurbMap map1 = new BlurbMap.Builder().build();
+        BlurbMap map2 = new BlurbMap.Builder().build();
+        Assertions.assertEquals(map1, map2);
+    }
+
+    @Test
+    public void blurbBuilder_testIfUnequal_success() {
+        BlurbMap.Builder builder1 = new BlurbMap.Builder();
+        BlurbMap.Builder builder2 = new BlurbMap.Builder();
+
+        builder1.withRecord("this", "builder");
+        builder2.withRecord("other", "builder");
+        Assertions.assertNotEquals(builder1.build(), builder2.build());
+    }
 }
