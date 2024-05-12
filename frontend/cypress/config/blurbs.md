3e6ec16bf08334185fa77d1da2653e0a74252f18
sopa301 <96387349+sopa301@users.noreply.github.com>
2024-05-12 13:49:17 +0800

    Update tests

--- frontend/cypress/config/blurbs.md
+++ frontend/cypress/config/blurbs.md
@@ -3,3 +3,9 @@ first blurb
 <!--repo-->------------------------------------
 https://gitlab.com/reposense/testrepo-gitlab/-/tree/main
 unseen blurb
+<!--repo-->------------------------------------
+https://github.com/reposense/publish-RepoSense/tree/master
+third blurb
+<!--repo-->------------------------------------
+https://github.com/reposense/RepoSense-auth-helper/tree/master
+second blurb
--- frontend/cypress/tests/chartView/chartView_blurbs.cy.js
+++ frontend/cypress/tests/chartView/chartView_blurbs.cy.js
@@ -3,10 +3,18 @@ describe('blurbs', () => {
     cy.get('.markdown.blurb')
       .first()
       .should('contain', 'first blurb');
+
+    cy.get('.markdown.blurb')
+      .eq(1)
+      .should('contain', 'second blurb');
+
+    cy.get('.markdown.blurb')
+      .eq(2)
+      .should('contain', 'third blurb');
   });
 
   it('has the correct number of valid blurbs', () => {
     cy.get('.markdown.blurb')
-      .should('have.length', 1);
+      .should('have.length', 3);
   });
 });
