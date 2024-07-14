cc151f489c0ca295ee0c16b97596ca3359f23a54
George Tay <george_tay@u.nus.edu>
2024-07-14 17:41:33 +0800

    Revert "Update YAML config file format"
    This reverts commit 587f1528f1d3400683e6c589be85d77f8cb0619f.

--- src/main/java/reposense/model/reportconfig/ReportBranchData.java
+++ src/main/java/reposense/model/reportconfig/ReportBranchData.java
@@ -1,5 +1,6 @@
 package reposense.model.reportconfig;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -9,49 +10,104 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public class ReportBranchData {
     public static final String DEFAULT_BRANCH = "main";
-    public static final String DEFAULT_BLURB = "";
+    public static final List<ReportAuthorDetails> DEFAULT_REPORT_AUTHOR_DETAILS = new ArrayList<>(
+            ReportAuthorDetails.DEFAULT_INSTANCES
+    );
+    public static final List<String> DEFAULT_FILE_FORMATS = List.of(
+            "override:java", "md", "fxml"
+    );
     public static final List<String> DEFAULT_IGNORE_GLOB_LIST = List.of(
             "docs**"
+    );
+    public static final List<String> DEFAULT_IGNORE_COMMITS_LIST = List.of(
+            "2fb6b9b2dd9fa40bf0f9815da2cb0ae8731436c7",
+            "c5a6dc774e22099cd9ddeb0faff1e75f9cf4f151",
+            "cd7f610e0becbdf331d5231887d8010a689f87c7",
+            "768015345e70f06add2a8b7d1f901dc07bf70582"
     );
     public static final List<String> DEFAULT_IGNORE_AUTHORS_LIST = List.of(
             "author1",
             "author2"
     );
+    public static final boolean DEFAULT_IS_FIND_PREVIOUS_AUTHOR = false;
+    public static final boolean DEFAULT_IS_SHALLOW_CLONING = true;
+    public static final boolean DEFAULT_IS_IGNORE_STANDALONE_CONFIG = true;
+
     public static final ReportBranchData DEFAULT_INSTANCE = new ReportBranchData();
 
     static {
         DEFAULT_INSTANCE.branch = ReportBranchData.DEFAULT_BRANCH;
-        DEFAULT_INSTANCE.blurb = ReportBranchData.DEFAULT_BLURB;
+        DEFAULT_INSTANCE.fileFormats = ReportBranchData.DEFAULT_FILE_FORMATS;
         DEFAULT_INSTANCE.ignoreGlobList = ReportBranchData.DEFAULT_IGNORE_GLOB_LIST;
+        DEFAULT_INSTANCE.ignoreCommitList = ReportBranchData.DEFAULT_IGNORE_COMMITS_LIST;
         DEFAULT_INSTANCE.ignoreAuthorList = ReportBranchData.DEFAULT_IGNORE_AUTHORS_LIST;
+        DEFAULT_INSTANCE.isFindPreviousAuthor = ReportBranchData.DEFAULT_IS_FIND_PREVIOUS_AUTHOR;
+        DEFAULT_INSTANCE.isShallowCloning = ReportBranchData.DEFAULT_IS_SHALLOW_CLONING;
+        DEFAULT_INSTANCE.isIgnoreStandaloneConfig = ReportBranchData.DEFAULT_IS_IGNORE_STANDALONE_CONFIG;
+        DEFAULT_INSTANCE.reportAuthorDetails = ReportAuthorDetails.DEFAULT_INSTANCES;
     }
 
     @JsonProperty("branch")
     private String branch;
 
-    @JsonProperty("blurb")
-    private String blurb;
+    @JsonProperty("authors")
+    private List<ReportAuthorDetails> reportAuthorDetails;
+
+    @JsonProperty("file-formats")
+    private List<String> fileFormats;
 
     @JsonProperty("ignore-glob-list")
     private List<String> ignoreGlobList;
 
+    @JsonProperty("ignore-standalone-config")
+    private Boolean isIgnoreStandaloneConfig;
+
+    @JsonProperty("ignore-commits-list")
+    private List<String> ignoreCommitList;
+
     @JsonProperty("ignore-authors-list")
     private List<String> ignoreAuthorList;
+
+    @JsonProperty("is-shallow-cloning")
+    private Boolean isShallowCloning;
+
+    @JsonProperty("is-find-previous-authors")
+    private Boolean isFindPreviousAuthor;
 
     public String getBranch() {
         return branch == null ? DEFAULT_BRANCH : branch;
     }
 
-    public String getBlurb() {
-        return blurb == null ? DEFAULT_BLURB : blurb;
+    public List<ReportAuthorDetails> getReportAuthorDetails() {
+        return reportAuthorDetails == null ? DEFAULT_REPORT_AUTHOR_DETAILS : reportAuthorDetails;
+    }
+
+    public List<String> getFileFormats() {
+        return fileFormats == null ? DEFAULT_FILE_FORMATS : fileFormats;
     }
 
     public List<String> getIgnoreGlobList() {
-        return ignoreGlobList == null ? DEFAULT_IGNORE_GLOB_LIST : ignoreGlobList;
+        return ignoreGlobList == null ? DEFAULT_IGNORE_GLOB_LIST : fileFormats;
+    }
+
+    public boolean getIsIgnoreStandaloneConfig() {
+        return isIgnoreStandaloneConfig == null ? DEFAULT_IS_IGNORE_STANDALONE_CONFIG : isIgnoreStandaloneConfig;
+    }
+
+    public List<String> getIgnoreCommitList() {
+        return ignoreCommitList == null ? DEFAULT_IGNORE_COMMITS_LIST : ignoreCommitList;
     }
 
     public List<String> getIgnoreAuthorList() {
         return ignoreAuthorList == null ? DEFAULT_IGNORE_AUTHORS_LIST : ignoreAuthorList;
+    }
+
+    public boolean getIsShallowCloning() {
+        return isShallowCloning == null ? DEFAULT_IS_SHALLOW_CLONING : isShallowCloning;
+    }
+
+    public boolean getIsFindPreviousAuthor() {
+        return isFindPreviousAuthor == null ? DEFAULT_IS_FIND_PREVIOUS_AUTHOR : isFindPreviousAuthor;
     }
 
     @Override
@@ -63,9 +119,14 @@ public class ReportBranchData {
         if (obj instanceof ReportBranchData) {
             ReportBranchData rbd = (ReportBranchData) obj;
             return this.getBranch().equals(rbd.getBranch())
-                    && this.getBlurb().equals(rbd.getBlurb())
+                    && this.getReportAuthorDetails().equals(rbd.getReportAuthorDetails())
+                    && this.getFileFormats().equals(rbd.getFileFormats())
                     && this.getIgnoreGlobList().equals(rbd.getIgnoreGlobList())
-                    && this.getIgnoreAuthorList().equals(rbd.getIgnoreAuthorList());
+                    && this.getIsIgnoreStandaloneConfig() == rbd.getIsIgnoreStandaloneConfig()
+                    && this.getIgnoreCommitList().equals(rbd.getIgnoreCommitList())
+                    && this.getIgnoreAuthorList().equals(rbd.getIgnoreAuthorList())
+                    && this.getIsShallowCloning() == rbd.getIsShallowCloning()
+                    && this.getIsFindPreviousAuthor() == rbd.getIsFindPreviousAuthor();
         }
 
         return false;
--- src/test/java/reposense/model/reportconfig/ReportBranchDataTest.java
+++ src/test/java/reposense/model/reportconfig/ReportBranchDataTest.java
@@ -10,14 +10,43 @@ public class ReportBranchDataTest {
     }
 
     @Test
+    public void getFileFormats_equalsDefaultReturnValue_success() {
+        Assertions.assertSame(new ReportBranchData().getFileFormats(), ReportBranchData.DEFAULT_FILE_FORMATS);
+    }
+
+    @Test
     public void getIgnoreGlobList_equalsDefaultReturnValue_success() {
         Assertions.assertSame(new ReportBranchData().getIgnoreGlobList(), ReportBranchData.DEFAULT_IGNORE_GLOB_LIST);
+    }
+
+    @Test
+    public void getIsIgnoreStandaloneConfig_equalsDefaultReturnValue_success() {
+        Assertions.assertSame(new ReportBranchData().getIsIgnoreStandaloneConfig(),
+                ReportBranchData.DEFAULT_IS_IGNORE_STANDALONE_CONFIG);
+    }
+
+    @Test
+    public void getIgnoreCommitList_equalsDefaultReturnValue_success() {
+        Assertions.assertSame(new ReportBranchData().getIgnoreCommitList(),
+                ReportBranchData.DEFAULT_IGNORE_COMMITS_LIST);
     }
 
     @Test
     public void getIgnoreAuthorList_equalsDefaultReturnValue_success() {
         Assertions.assertSame(new ReportBranchData().getIgnoreAuthorList(),
                 ReportBranchData.DEFAULT_IGNORE_AUTHORS_LIST);
+    }
+
+    @Test
+    public void getIsShallowCloning_equalsDefaultReturnValue_success() {
+        Assertions.assertSame(new ReportBranchData().getIsShallowCloning(),
+                ReportBranchData.DEFAULT_IS_SHALLOW_CLONING);
+    }
+
+    @Test
+    public void getIsFindPreviousAuthor_equalsDefaultReturnValue_success() {
+        Assertions.assertSame(new ReportBranchData().getIsFindPreviousAuthor(),
+                ReportBranchData.DEFAULT_IS_FIND_PREVIOUS_AUTHOR);
     }
 
     @Test
