3f71d3049b606d8321e67a9abee59be33c7233b4
jedkohjk <jedkoh2001@gmail.com>
2024-08-04 03:20:19 +0800

    Duplicate RepoSense links

---
+++ frontend/src/components/c-footer.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+#footer
+  #app-name
+    span &nbsp;
+    a(
+      v-bind:href="getRepoSenseHomeLink()", target="_blank"
+    )
+      strong RepoSense
+    span &nbsp;|&nbsp;
+    a(
+      v-bind:href="getSpecificCommitLink()", target="_blank"
+    )
+      strong Version
+    span &nbsp;|&nbsp;
+    a(
+      v-bind:href="getUserGuideLink()", target ="_blank"
+    )
+      strong User Guide
+</template>
+
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'c-footer',
+  methods: {
+    getRepoSenseHomeLink(): string {
+      const version = window.repoSenseVersion;
+      if (!version) {
+        return `${window.HOME_PAGE_URL}/RepoSense/`;
+      }
+      return `${window.HOME_PAGE_URL}`;
+    },
+
+    getSpecificCommitLink(): string {
+      const version = window.repoSenseVersion;
+      if (!version) {
+        return `${window.REPOSENSE_REPO_URL}`;
+      }
+      if (version.startsWith('v')) {
+        return `${window.REPOSENSE_REPO_URL}/releases/tag/${version}`;
+      }
+      return `${window.REPOSENSE_REPO_URL}/commit/${version}`;
+    },
+
+    getUserGuideLink(): string {
+      const version = window.repoSenseVersion;
+      if (!version) {
+        return `${window.HOME_PAGE_URL}/RepoSense/ug/index.html`;
+      }
+      return `${window.HOME_PAGE_URL}/ug/index.html`;
+    },
+  },
+});
+
+</script>
