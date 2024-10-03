5ed12bb6e553e3304da3bb2481830df0e0c9dd9a
logical-1985516 <107944536+logical-1985516@users.noreply.github.com>
2024-10-04 00:57:02 +0800

    Add test cases for blameFile

--- src/test/java/reposense/git/GitBlameTest.java
+++ src/test/java/reposense/git/GitBlameTest.java
@@ -1,5 +1,7 @@
 package reposense.git;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -77,5 +79,49 @@ public class GitBlameTest extends GitTestTemplate {
     public void blameLine_nonExistentLine_throwsRunTimeException() {
         Assertions.assertThrows(RuntimeException.class, () -> GitBlame.blameLine(config.getRepoRoot(),
                 FAKE_AUTHOR_BLAME_TEST_FILE_COMMIT_08022018_STRING, "blameTest.java", 5));
+    }
+
+    @Test
+    public void blameFile_allValidArguments_success() {
+        List<GitBlameLineInfo> expectedLineInfos = new ArrayList<>();
+        expectedLineInfos.add(new GitBlameLineInfo("8d0ac2ee20f04dce8df0591caed460bffacb65a4",
+                MAIN_AUTHOR_NAME, "ma.tanghao@dhs.sg", 1517863105));
+        expectedLineInfos.add(new GitBlameLineInfo("8d0ac2ee20f04dce8df0591caed460bffacb65a4",
+                MAIN_AUTHOR_NAME, "ma.tanghao@dhs.sg", 1517863105));
+        expectedLineInfos.add(new GitBlameLineInfo("768015345e70f06add2a8b7d1f901dc07bf70582",
+                FAKE_AUTHOR_NAME, "ma.tanghao@dhs.sg", 1518085550));
+        expectedLineInfos.add(new GitBlameLineInfo("8d0ac2ee20f04dce8df0591caed460bffacb65a4",
+                MAIN_AUTHOR_NAME, "ma.tanghao@dhs.sg", 1517863105));
+        List<GitBlameLineInfo> actualLineInfos = GitBlame.blameFile(config.getRepoRoot(), "blameTest.java", false);
+        Assertions.assertEquals(expectedLineInfos, actualLineInfos);
+    }
+
+    @Test
+    public void blameFile_allValidArgumentsWithPreviousAuthors_success() {
+        config.setBranch(TEST_REPO_BLAME_WITH_PREVIOUS_AUTHORS_BRANCH);
+        GitCheckout.checkoutBranch(config.getRepoRoot(), TEST_REPO_BLAME_WITH_PREVIOUS_AUTHORS_BRANCH);
+        createTestIgnoreRevsFile(AUTHOR_TO_IGNORE_BLAME_COMMIT_LIST_07082021);
+
+        List<GitBlameLineInfo> expectedLineInfos = new ArrayList<>();
+        expectedLineInfos.add(new GitBlameLineInfo("8d0ac2ee20f04dce8df0591caed460bffacb65a4",
+                MAIN_AUTHOR_NAME, "ma.tanghao@dhs.sg", 1517863105));
+        expectedLineInfos.add(new GitBlameLineInfo("8d0ac2ee20f04dce8df0591caed460bffacb65a4",
+                MAIN_AUTHOR_NAME, "ma.tanghao@dhs.sg", 1517863105));
+        expectedLineInfos.add(new GitBlameLineInfo("768015345e70f06add2a8b7d1f901dc07bf70582",
+                FAKE_AUTHOR_NAME, "ma.tanghao@dhs.sg", 1518085550));
+        expectedLineInfos.add(new GitBlameLineInfo("8d0ac2ee20f04dce8df0591caed460bffacb65a4",
+                MAIN_AUTHOR_NAME, "ma.tanghao@dhs.sg", 1517863105));
+        List<GitBlameLineInfo> actualLineInfos = GitBlame.blameFile(config.getRepoRoot(), "blameTest.java", true);
+        Assertions.assertEquals(expectedLineInfos, actualLineInfos);
+    }
+
+    @Test
+    public void blameFile_nonExistentFile_throwsRunTimeException() {
+        Assertions.assertThrows(RuntimeException.class, () -> GitBlame.blameFile(config.getRepoRoot(), "nonExistentFile", false));
+    }
+
+    @Test
+    public void blameFile_nonExistentFileWithPreviousAuthors_throwsRunTimeException() {
+        Assertions.assertThrows(RuntimeException.class, () -> GitBlame.blameFile(config.getRepoRoot(), "nonExistentFile", true));
     }
 }
