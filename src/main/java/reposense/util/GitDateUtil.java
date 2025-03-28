9f4dc07e80fe2b0c9ee6250a39fc2583653caaf6
Jeonghyeon Lee <dlqjdgus99@naver.com>
2025-03-28 16:14:11 -0300

    refactor: deficient encapsulation

---
+++ src/main/java/reposense/util/GitDateUtil.java
@@ -0,0 +1,29 @@
+package reposense.util;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class GitDateUtil {
+    public static final DateTimeFormatter GIT_STRICT_ISO_DATE_FORMAT =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssz");
+
+    private GitDateUtil() {}
+
+    /**
+     * Returns a {@link ZonedDateTime} from a string {@code gitStrictIsoDate}.
+     */
+    public static ZonedDateTime parseGitStrictIsoDate(String gitStrictIsoDate) {
+        return ZonedDateTime.parse(gitStrictIsoDate, GIT_STRICT_ISO_DATE_FORMAT);
+    }
+
+    /**
+     * Returns a {@link LocalDateTime} from a string {@code gitStrictIsoDate}.
+     */
+    public static LocalDateTime parseGitStrictIsoDateWithZone(
+            String gitStrictIsoDate, ZoneId zoneId) {
+        return ZonedDateTime.parse(gitStrictIsoDate, GIT_STRICT_ISO_DATE_FORMAT)
+                .withZoneSameInstant(zoneId).toLocalDateTime();
+    }
+}
