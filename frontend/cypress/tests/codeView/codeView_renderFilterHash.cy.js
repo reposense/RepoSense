d9e2c9f38eb18d331a0bfa52b2478b82804015c7
CYX22222003 <e1155533@u.nus.edu>
2025-04-05 01:45:56 +0800

    Update cypress tests

--- frontend/cypress/tests/codeView/codeView_renderFilterHash.cy.js
+++ frontend/cypress/tests/codeView/codeView_renderFilterHash.cy.js
@@ -1,11 +1,11 @@
 describe('render filter hash', () => {
   it('filter files: url params should persist after change and reload', () => {
-    cy.get('div.mui-textfield.filter_file > input:visible')
+    cy.get('div.mui-textfield.tooltip.filter_file > input:visible')
       .should('be.visible')
       .invoke('val')
       .should('eq', '');
 
-    cy.get('div.mui-textfield.filter_file > input:visible')
+    cy.get('div.mui-textfield.tooltip.filter_file > input:visible')
       .should('be.visible')
       .type('**java**{enter}');
 
