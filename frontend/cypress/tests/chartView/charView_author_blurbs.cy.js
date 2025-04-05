af97fc789b1043120e00877a01761786a416ed56
CYX22222003 <e1155533@u.nus.edu>
2025-04-05 17:22:20 +0800

    Update correct config

--- frontend/cypress/tests/chartView/charView_author_blurbs.cy.js
+++ frontend/cypress/tests/chartView/charView_author_blurbs.cy.js
@@ -1,11 +1,11 @@
 describe('author blurbs', () => {
   it('test display of author blurbs', () => {
     cy.get('#summary .mui-select.grouping select').select('Author')
-    cy.get('.blurbWrapper .markdown blurb')
+    cy.get('.markdown.blurb')
       .first()
       .should('contain', 'first blurb');
 
-    cy.get('.blurbWrapper .markdown blurb')
+    cy.get('.markdown.blurb')
       .eq(1)
       .should('contain', 'second blurb');
 
@@ -16,13 +16,13 @@ describe('author blurbs', () => {
 
   it('test correct number of blurbs', () => {
     cy.get('#summary .mui-select.grouping select').select('Author');
-    cy.get('.blurbWrapper .markdown blurb')
+    cy.get('.markdown.blurb')
       .should('have.length', 3);
   })
 
   it('processes html in blurbs', () => {
     cy.get('#summary .mui-select.grouping select').select('Author');
-    cy.get('.blurbWrapper .markdown blurb')
+    cy.get('.markdown.blurb')
       .eq(1)
       .find('h1')
       .contains('dummy text for the second blurb');
@@ -30,7 +30,7 @@ describe('author blurbs', () => {
 
   it('processes markdown in blurbs', () => {
     cy.get('#summary .mui-select.grouping select').select('Author');
-    cy.get('.blurbWrapper .markdown blurb')
+    cy.get('.markdown.blurb')
       .eq(2)
       .find('h2')
       .contains('h2 tag for the third blurb');
