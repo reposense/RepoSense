f5f90363d105cbe174ce55215fbdece16861db55
gladwintan <gladwintan@hotmail.sg>
2025-06-30 14:00:01 +0800

    Remove trailing whitespace

--- frontend/cypress/tests/portfolioView/portfolioView_sorting.cy.js
+++ frontend/cypress/tests/portfolioView/portfolioView_sorting.cy.js
@@ -7,14 +7,14 @@ describe('sorting',
         // map last 3 repos in repo-config.csv to corresponding display title in summary charts
         const repoNames = lines.slice(-3).map((line) => {
           const values = line.split(',');
-          
+
           const match = values[0].match(/^https?:\/\/(www\.)?github.com\/([^/]+)\/([^/.]+)(\.git)?/i)
           const repoName = `${match[2]}/${match[3]}`;
-          
+
           return `${repoName}[${values[1]}]`;
         });
         cy.wrap(repoNames).as('repoNames')
-      });     
+      });
     })
 
     it('repo order should follow order of rows in repo-config.csv', () => {
@@ -22,7 +22,7 @@ describe('sorting',
         .then(($els) => {
           // last 3 summary chart title
           const repoTitles = Cypress._.map($els, (el) => el.textContent.trim()).slice(-3);
-  
+
           cy.get('@repoNames').then((repoNames) => {
             repoNames.forEach((expectedRepoName, index) => {
               expect(repoTitles[index]).to.eq(expectedRepoName);
