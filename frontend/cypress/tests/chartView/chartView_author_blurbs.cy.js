58fdcb36f4739c0204e1fec1671d5dc00c17aa64
gekjunxu <E1122727@u.nus.edu>
2025-07-26 01:29:55 +0800

    Fix indentation

--- frontend/cypress/tests/chartView/chartView_author_blurbs.cy.js
+++ frontend/cypress/tests/chartView/chartView_author_blurbs.cy.js
@@ -1,61 +1,61 @@
 describe('author blurbs', () => {
-  it('test display of author blurbs', () => {
-    cy.get('#summary .mui-select.grouping select').select('Author')
-    cy.get('.markdown.blurb')
-      .first()
-      .should('contain', 'first blurb');
+    it('test display of author blurbs', () => {
+        cy.get('#summary .mui-select.grouping select').select('Author')
+        cy.get('.markdown.blurb')
+            .first()
+            .should('contain', 'first blurb');
 
-    cy.get('.markdown.blurb')
-        .eq(1)
-        .should('contain', 'first chart blurb');
+        cy.get('.markdown.blurb')
+            .eq(1)
+            .should('contain', 'first chart blurb');
 
-    cy.get('.markdown.blurb')
-        .eq(2)
-        .should('contain', 'second blurb');
+        cy.get('.markdown.blurb')
+            .eq(2)
+            .should('contain', 'second blurb');
 
-    cy.get('.markdown.blurb')
-        .eq(3)
-        .should('contain', 'third blurb');
+        cy.get('.markdown.blurb')
+            .eq(3)
+            .should('contain', 'third blurb');
 
-    cy.get('.markdown.blurb')
-        .eq(4)
-        .should('contain', 'second chart blurb');
+        cy.get('.markdown.blurb')
+            .eq(4)
+            .should('contain', 'second chart blurb');
 
-    cy.get('.markdown.blurb')
-        .eq(5)
-        .should('contain', 'third chart blurb');
+        cy.get('.markdown.blurb')
+            .eq(5)
+            .should('contain', 'third chart blurb');
 
-  });
+    });
 
-  it('test correct number of blurbs', () => {
-    cy.get('#summary .mui-select.grouping select').select('Author');
-    cy.get('.markdown.blurb')
-      .should('have.length', 6);
-  })
+    it('test correct number of blurbs', () => {
+        cy.get('#summary .mui-select.grouping select').select('Author');
+        cy.get('.markdown.blurb')
+            .should('have.length', 6);
+    })
 
-  it('processes html in blurbs', () => {
-    cy.get('#summary .mui-select.grouping select').select('Author');
-    cy.get('.markdown.blurb')
-      .eq(2)
-      .find('h1')
-      .contains('dummy text for the second blurb');
+    it('processes html in blurbs', () => {
+        cy.get('#summary .mui-select.grouping select').select('Author');
+        cy.get('.markdown.blurb')
+            .eq(2)
+            .find('h1')
+            .contains('dummy text for the second blurb');
 
-    cy.get('.markdown.blurb')
-        .eq(4)
-        .find('h2')
-        .contains('second chart blurb');
-  })
+        cy.get('.markdown.blurb')
+            .eq(4)
+            .find('h2')
+            .contains('second chart blurb');
+    })
 
-  it('processes markdown in blurbs', () => {
-    cy.get('#summary .mui-select.grouping select').select('Author');
-    cy.get('.markdown.blurb')
-      .eq(3)
-      .find('h2')
-      .contains('h2 tag for the third blurb');
+    it('processes markdown in blurbs', () => {
+        cy.get('#summary .mui-select.grouping select').select('Author');
+        cy.get('.markdown.blurb')
+            .eq(3)
+            .find('h2')
+            .contains('h2 tag for the third blurb');
 
-    cy.get('.markdown.blurb')
-        .eq(5)
-        .find('h3')
-        .contains('h3 third chart blurb');
-  })
+        cy.get('.markdown.blurb')
+            .eq(5)
+            .find('h3')
+            .contains('h3 third chart blurb');
+    })
 })
