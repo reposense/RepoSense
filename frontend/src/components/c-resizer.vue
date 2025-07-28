870e903faf399a5445e0224f16d9f9639076a9a5
gekjunxu <E1122727@u.nus.edu>
2025-07-28 18:10:55 +0800

    Revert "Attempt to adjust backend logic"
    This reverts commit 1d9b829cc4cf885eb0c5262505181ae5661fb584.

--- frontend/src/components/c-resizer.vue
+++ frontend/src/components/c-resizer.vue
@@ -1,34 +1,39 @@
 <template lang="pug">
-  #app-wrapper(
-    :style="appStyles",
-    @mousemove="mouseMove",
-    @mouseup="deregisterMouseMove",
-    @mouseleave="deregisterMouseMove"
+#app-wrapper(
+  :style="appStyles",
+  @mousemove="mouseMove",
+  @mouseup="deregisterMouseMove",
+  @mouseleave="deregisterMouseMove"
+)
+  #tab-resize-guide(:style="guideStyles")
+  .left-resize-container
+    slot(name="left")
+  #tab-resize(
+    @mousedown.left="registerMouseMove",
+    v-show="isTabActive"
   )
-    #tab-resize-guide(:style="guideStyles")
-    .left-resize-container
-      slot(name="left")
-    #tab-resize(
-      @mousedown.left="registerMouseMove",
-      v-show="isTabActive"
-    )
-      .tab-close(@click="closeTab")
-        i.fas.fa-caret-right
-    .right-resize-container(
-      :style="rightContainerStyles",
-      v-if="isTabActive"
-    )
-      slot(name="right")
+    .tab-close(@click="closeTab")
+      i.fas.fa-caret-right
+  .right-resize-container(
+    :style="rightContainerStyles",
+    v-if="isTabActive"
+  )
+    slot(name="right")
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import { mapState } from 'vuex';
 import { defineComponent } from 'vue';
 
-const DRAG_BAR_THICKNESS = 13.25;
+const DRAG_BAR_WIDTH = 13.25;
 const SCROLL_BAR_WIDTH = 17;
 const GUIDE_BAR_WIDTH = 2;
 
+/** The following eslint suppression suppresses a rare false positive case where event cannot be accessed due to
+ *  handler being a lambda function parameter. The explicit lambda function here allows us to easily discern handler's
+ *  parameters, i.e. an event of type MouseEvent.
+ */
+// eslint-disable-next-line no-unused-vars
 const throttledEvent = (delay: number, handler: (event: MouseEvent) => unknown): ((event: MouseEvent) => void) => {
   let lastCalled = 0;
   return (event: MouseEvent): void => {
@@ -42,73 +47,54 @@ const throttledEvent = (delay: number, handler: (event: MouseEvent) => unknown):
 export default defineComponent({
   name: 'c-resizer',
 
-  data() {
-    const isPortrait = window.innerHeight > window.innerWidth;
+  data(): {
+    guideWidth: number,
+    flexWidth: number,
+    isResizing: boolean
+  } {
     return {
-      isPortrait,
-      guideRatio: 0.5,
-      flexRatio: 0.5,
-      isResizing: false
+      guideWidth: (0.5 * window.innerWidth - (GUIDE_BAR_WIDTH / 2)) / window.innerWidth,
+      flexWidth: 0.5,
+      isResizing: false,
     };
   },
 
   computed: {
-    ...mapState(['isTabActive']),
-
     appStyles(): string {
       return this.isResizing
-        ? `user-select: none; cursor: ${this.isPortrait ? 'row-resize' : 'col-resize'};`
+        ? 'user-select: none; cursor: col-resize;'
         : '';
     },
 
     guideStyles(): string {
-      if (!this.isResizing) return '';
-      const percent = this.guideRatio * 100;
-      return this.isPortrait
-        ? `display: block; bottom: ${percent}%;`
-        : `display: block; right: ${percent}%;`;
+      return this.isResizing
+        ? `display: block; right: ${this.guideWidth * 100}%;`
+        : '';
     },
 
     rightContainerStyles(): string {
-      const percent = this.flexRatio * 100;
-      return this.isPortrait
-        ? `flex: 0 0 ${percent}%; height: ${percent}%;`
-        : `flex: 0 0 ${percent}%;`;
+      return `flex: 0 0 ${this.flexWidth * 100}%;`;
     },
 
     mouseMove(): Function {
-      if (!this.isResizing) return () => {};
+      if (this.isResizing) {
+        return throttledEvent(25, (event: MouseEvent) => {
+          this.guideWidth = (
+            Math.min(
+              Math.max(
+                window.innerWidth - event.clientX,
+                SCROLL_BAR_WIDTH + DRAG_BAR_WIDTH,
+              ),
+              window.innerWidth - SCROLL_BAR_WIDTH,
+            )
+            - (GUIDE_BAR_WIDTH / 2)
+          ) / window.innerWidth;
+        });
+      }
+      return () => {};
+    },
 
-      return throttledEvent(25, (event: MouseEvent) => {
-        if (this.isPortrait) {
-          const available = window.innerHeight;
-          const offset = Math.min(
-            Math.max(
-              available - event.clientY,
-              SCROLL_BAR_WIDTH + DRAG_BAR_THICKNESS
-            ),
-            available - SCROLL_BAR_WIDTH
-          );
-          this.guideRatio = (offset - GUIDE_BAR_WIDTH / 2) / available;
-        } else {
-          const available = window.innerWidth;
-          const offset = Math.min(
-            Math.max(
-              available - event.clientX,
-              SCROLL_BAR_WIDTH + DRAG_BAR_THICKNESS
-            ),
-            available - SCROLL_BAR_WIDTH
-          );
-          this.guideRatio = (offset - GUIDE_BAR_WIDTH / 2) / available;
-        }
-      });
-    }
-  },
-
-  mounted() {
-    window.addEventListener('resize', () => {
-      this.isPortrait = window.innerHeight > window.innerWidth;
-    });
+    ...mapState(['isTabActive']),
   },
 
   methods: {
@@ -117,16 +103,14 @@ export default defineComponent({
     },
 
     deregisterMouseMove(): void {
-      if (!this.isResizing) return;
-
       this.isResizing = false;
-      const available = this.isPortrait ? window.innerHeight : window.innerWidth;
-      this.flexRatio = (this.guideRatio * available + GUIDE_BAR_WIDTH / 2) / available;
+      this.flexWidth = (this.guideWidth * window.innerWidth + (GUIDE_BAR_WIDTH / 2))
+        / window.innerWidth;
     },
 
     closeTab(): void {
       this.$store.commit('updateTabState', false);
-    }
-  }
+    },
+  },
 });
 </script>
