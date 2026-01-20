56a69cb3266b99fa8da81c4341ba7a18d3ad8d4c
ashmitahaldar <67264881+ashmitahaldar@users.noreply.github.com>
2026-01-20 17:58:24 +0800

    Refactor title.md references to intro.md in ReportGenerator and associated tests

--- src/test/java/reposense/report/ReportGeneratorTest.java
+++ src/test/java/reposense/report/ReportGeneratorTest.java
@@ -27,70 +27,71 @@ import reposense.parser.SummaryJsonParser;
 import reposense.util.TimeUtil;
 
 class ReportGeneratorTest {
-    private static final Path OUTPUT_PATH = loadResource(ReportGeneratorTest.class, "ReportGeneratorTest");
-    private static final Path ASSETS_PATH = loadResource(ReportGeneratorTest.class, "ReportGeneratorTest/test_assets");
-    private static final Path ORIGINAL_SUMMARY_JSON_PATH = loadResource(ReportGeneratorTest.class,
-            "ReportGeneratorTest/original_files/summary.json");
-    private static final Path SUMMARY_JSON_PATH = loadResource(ReportGeneratorTest.class,
-            "ReportGeneratorTest/summary.json");
-    private static final Path ORIGINAL_TITLE_MD_PATH = loadResource(ReportGeneratorTest.class,
-            "ReportGeneratorTest/original_files/title.md");
-    private static final Path TITLE_MD_PATH = loadResource(ReportGeneratorTest.class, "ReportGeneratorTest/title.md");
-    private static final Path TEST_TITLE_MD_PATH = loadResource(ReportGeneratorTest.class,
-            "ReportGeneratorTest/test_assets/title.md");
+        private static final Path OUTPUT_PATH = loadResource(ReportGeneratorTest.class, "ReportGeneratorTest");
+        private static final Path ASSETS_PATH = loadResource(ReportGeneratorTest.class, "ReportGeneratorTest/test_assets");
+        private static final Path ORIGINAL_SUMMARY_JSON_PATH = loadResource(ReportGeneratorTest.class,
+                        "ReportGeneratorTest/original_files/summary.json");
+        private static final Path SUMMARY_JSON_PATH = loadResource(ReportGeneratorTest.class,
+                        "ReportGeneratorTest/summary.json");
+        private static final Path ORIGINAL_INTRO_MD_PATH = loadResource(ReportGeneratorTest.class,
+                        "ReportGeneratorTest/original_files/intro.md");
+        private static final Path INTRO_MD_PATH = loadResource(ReportGeneratorTest.class,
+                        "ReportGeneratorTest/intro.md");
+        private static final Path TEST_INTRO_MD_PATH = loadResource(ReportGeneratorTest.class,
+                        "ReportGeneratorTest/test_assets/intro.md");
 
-    private static final String REPORT_GENERATED_TIME = "Wed, 1 Jan 2025 00:00:00 SGT";
-    private static final String GITHUB_API_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+        private static final String REPORT_GENERATED_TIME = "Wed, 1 Jan 2025 00:00:00 SGT";
+        private static final String GITHUB_API_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
-    @Test
-    void generateReposReport_isOnlyTextRefreshedTrue_success() throws Exception {
-        RepoBlurbMap repoBlurbMap = new RepoBlurbMap();
-        repoBlurbMap.withRecord("https://github.com/reposense/testrepo-Delta/tree/master", "This is a test blurb");
-        AuthorBlurbMap authorBlurbMap = new AuthorBlurbMap();
-        authorBlurbMap.withRecord("nbriannl", "Test for author-blurbs.md");
-        ChartBlurbMap chartBlurbMap = new ChartBlurbMap();
-        chartBlurbMap.withRecord("https://github.com/reposense/testrepo-Delta/tree/master|nbriannl",
-                "This is a test blurb for chart-blurbs.md");
-        TimeUtil.startTimer();
+        @Test
+        void generateReposReport_isOnlyTextRefreshedTrue_success() throws Exception {
+                RepoBlurbMap repoBlurbMap = new RepoBlurbMap();
+                repoBlurbMap.withRecord("https://github.com/reposense/testrepo-Delta/tree/master", "This is a test blurb");
+                AuthorBlurbMap authorBlurbMap = new AuthorBlurbMap();
+                authorBlurbMap.withRecord("nbriannl", "Test for author-blurbs.md");
+                ChartBlurbMap chartBlurbMap = new ChartBlurbMap();
+                chartBlurbMap.withRecord("https://github.com/reposense/testrepo-Delta/tree/master|nbriannl",
+                                "This is a test blurb for chart-blurbs.md");
+                TimeUtil.startTimer();
 
-        List<Path> reportFoldersAndFiles = new ReportGenerator().generateReposReport(List.of(), OUTPUT_PATH.toString(),
-                ASSETS_PATH.toString(), new ReportConfiguration(), REPORT_GENERATED_TIME,
-                LocalDateTime.parse("2025-02-16T00:00:00", DateTimeFormatter.ofPattern(GITHUB_API_DATE_FORMAT)),
-                LocalDateTime.parse("2025-03-16T23:59:59", DateTimeFormatter.ofPattern(GITHUB_API_DATE_FORMAT)),
-                false, false, 4, 12, TimeUtil::getElapsedTime,
-                ZoneId.of("Asia/Singapore"), false, false, 0.51,
-                repoBlurbMap, authorBlurbMap, chartBlurbMap, false, true);
+                List<Path> reportFoldersAndFiles = new ReportGenerator().generateReposReport(List.of(), OUTPUT_PATH.toString(),
+                                ASSETS_PATH.toString(), new ReportConfiguration(), REPORT_GENERATED_TIME,
+                                LocalDateTime.parse("2025-02-16T00:00:00", DateTimeFormatter.ofPattern(GITHUB_API_DATE_FORMAT)),
+                                LocalDateTime.parse("2025-03-16T23:59:59", DateTimeFormatter.ofPattern(GITHUB_API_DATE_FORMAT)),
+                                false, false, 4, 12, TimeUtil::getElapsedTime,
+                                ZoneId.of("Asia/Singapore"), false, false, 0.51,
+                                repoBlurbMap, authorBlurbMap, chartBlurbMap, false, true);
 
-        SummaryJson actualSummaryJson = new SummaryJsonParser().parse(SUMMARY_JSON_PATH);
+                SummaryJson actualSummaryJson = new SummaryJsonParser().parse(SUMMARY_JSON_PATH);
 
-        assertNull(reportFoldersAndFiles);
-        assertEquals(repoBlurbMap, actualSummaryJson.getRepoBlurbs());
-        assertEquals(authorBlurbMap, actualSummaryJson.getAuthorBlurbs());
-        assertTrue(compareFileContents(TITLE_MD_PATH, TEST_TITLE_MD_PATH));
-    }
+                assertNull(reportFoldersAndFiles);
+                assertEquals(repoBlurbMap, actualSummaryJson.getRepoBlurbs());
+                assertEquals(authorBlurbMap, actualSummaryJson.getAuthorBlurbs());
+                assertTrue(compareFileContents(INTRO_MD_PATH, TEST_INTRO_MD_PATH));
+        }
 
-    @Test
-    void generateReposReport_isOnlyTextRefreshedTrueButInvalidPath_throwsIoException() throws Exception {
-        ReportGenerator reportGenerator = new ReportGenerator();
-        TimeUtil.startTimer();
-        RepoBlurbMap repoBlurbMap = new RepoBlurbMap();
-        repoBlurbMap.withRecord("https://github.com/reposense/testrepo-Delta/tree/master", "This is a test blurb");
-        AuthorBlurbMap authorBlurbMap = new AuthorBlurbMap();
-        ChartBlurbMap chartBlurbMap = new ChartBlurbMap();
-        Assertions.assertThrows(
-                IOException.class, () -> reportGenerator.generateReposReport(List.of(), ASSETS_PATH.toString(),
-                        ASSETS_PATH.toString(), new ReportConfiguration(), REPORT_GENERATED_TIME,
-                        LocalDateTime.parse("2025-02-16T00:00:00", DateTimeFormatter.ofPattern(GITHUB_API_DATE_FORMAT)),
-                        LocalDateTime.parse("2025-03-16T23:59:59", DateTimeFormatter.ofPattern(GITHUB_API_DATE_FORMAT)),
-                        false, false, 4, 12, TimeUtil::getElapsedTime,
-                        ZoneId.of("Asia/Singapore"), false, false, 0.51,
-                        repoBlurbMap, authorBlurbMap, chartBlurbMap, false, true)
-        );
-    }
+        @Test
+        void generateReposReport_isOnlyTextRefreshedTrueButInvalidPath_throwsIoException() throws Exception {
+                ReportGenerator reportGenerator = new ReportGenerator();
+                TimeUtil.startTimer();
+                RepoBlurbMap repoBlurbMap = new RepoBlurbMap();
+                repoBlurbMap.withRecord("https://github.com/reposense/testrepo-Delta/tree/master", "This is a test blurb");
+                AuthorBlurbMap authorBlurbMap = new AuthorBlurbMap();
+                ChartBlurbMap chartBlurbMap = new ChartBlurbMap();
+                Assertions.assertThrows(
+                                IOException.class, () -> reportGenerator.generateReposReport(List.of(), ASSETS_PATH.toString(),
+                                                ASSETS_PATH.toString(), new ReportConfiguration(), REPORT_GENERATED_TIME,
+                                                LocalDateTime.parse("2025-02-16T00:00:00", DateTimeFormatter.ofPattern(GITHUB_API_DATE_FORMAT)),
+                                                LocalDateTime.parse("2025-03-16T23:59:59", DateTimeFormatter.ofPattern(GITHUB_API_DATE_FORMAT)),
+                                                false, false, 4, 12, TimeUtil::getElapsedTime,
+                                                ZoneId.of("Asia/Singapore"), false, false, 0.51,
+                                                repoBlurbMap, authorBlurbMap, chartBlurbMap, false, true)
+                                        );
+        }
 
-    @AfterEach
-    void reset() throws IOException {
-        Files.copy(ORIGINAL_SUMMARY_JSON_PATH, SUMMARY_JSON_PATH, StandardCopyOption.REPLACE_EXISTING);
-        Files.copy(ORIGINAL_TITLE_MD_PATH, TITLE_MD_PATH, StandardCopyOption.REPLACE_EXISTING);
-    }
+        @AfterEach
+        void reset() throws IOException {
+                Files.copy(ORIGINAL_SUMMARY_JSON_PATH, SUMMARY_JSON_PATH, StandardCopyOption.REPLACE_EXISTING);
+                Files.copy(ORIGINAL_INTRO_MD_PATH, INTRO_MD_PATH, StandardCopyOption.REPLACE_EXISTING);
+        }
 }
