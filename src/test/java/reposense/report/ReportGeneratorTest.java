cd6a6067c546b046d5452683f9179abef67ae5ce
YuLetian <yu.letian66@gmail.com>
2026-02-06 11:06:36 +0800

    Delete deprecated test

--- src/test/java/reposense/report/ReportGeneratorTest.java
+++ src/test/java/reposense/report/ReportGeneratorTest.java
@@ -70,25 +70,6 @@ class ReportGeneratorTest {
         assertTrue(compareFileContents(INTRO_MD_PATH, TEST_INTRO_MD_PATH));
     }
 
-    @Test
-    void generateReposReport_isOnlyTextRefreshedTrueButInvalidPath_throwsIoException() throws Exception {
-        ReportGenerator reportGenerator = new ReportGenerator();
-        TimeUtil.startTimer();
-        RepoBlurbMap repoBlurbMap = new RepoBlurbMap();
-        repoBlurbMap.withRecord("https://github.com/reposense/testrepo-Delta/tree/master", "This is a test blurb");
-        AuthorBlurbMap authorBlurbMap = new AuthorBlurbMap();
-        ChartBlurbMap chartBlurbMap = new ChartBlurbMap();
-        Assertions.assertThrows(
-                IOException.class, () -> reportGenerator.generateReposReport(List.of(), ASSETS_PATH.toString(),
-                ASSETS_PATH.toString(), new ReportConfiguration(), REPORT_GENERATED_TIME,
-                LocalDateTime.parse("2025-02-16T00:00:00", DateTimeFormatter.ofPattern(GITHUB_API_DATE_FORMAT)),
-                LocalDateTime.parse("2025-03-16T23:59:59", DateTimeFormatter.ofPattern(GITHUB_API_DATE_FORMAT)),
-                false, false, 4, 12, TimeUtil::getElapsedTime,
-                ZoneId.of("Asia/Singapore"), false, false, 0.51,
-                repoBlurbMap, authorBlurbMap, chartBlurbMap, false, true)
-        );
-    }
-
     @AfterEach
     void reset() throws IOException {
         Files.copy(ORIGINAL_SUMMARY_JSON_PATH, SUMMARY_JSON_PATH, StandardCopyOption.REPLACE_EXISTING);
