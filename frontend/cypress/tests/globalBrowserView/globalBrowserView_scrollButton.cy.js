665d497ab18d216ec0b1f5ea2c4af45191229a7d
YuLetian <yu.letian66@gmail.com>
2026-03-16 18:15:16 +0800

    Fix: linter

--- frontend/cypress/tests/globalBrowserView/globalBrowserView_scrollButton.cy.js
+++ frontend/cypress/tests/globalBrowserView/globalBrowserView_scrollButton.cy.js
@@ -1,28 +1,28 @@
 describe('scroll button in global browser view', () => {
-    beforeEach(() => {
-        cy.get('#summary .mui-select.filter-mode select')
-            .select('Global');
+  beforeEach(() => {
+    cy.get('#summary .mui-select.filter-mode select')
+      .select('Global');
 
-        cy.get('#tab-file-browser', { timeout: 10000 })
-            .should('be.visible');
-    });
+    cy.get('#tab-file-browser', { timeout: 10000 })
+      .should('be.visible');
+  });
 
-    it('button is not shown when only scroll 100 pixels', () => {
-        cy.get('#tabs-wrapper').scrollTo(0, 100);
-        cy.get('#tab-file-browser #go-back-button').should('not.be.visible');
-        cy.get('#tabs-wrapper').scrollTo(0, 0);
-    });
+  it('button is not shown when only scroll 100 pixels', () => {
+    cy.get('#tabs-wrapper').scrollTo(0, 100);
+    cy.get('#tab-file-browser #go-back-button').should('not.be.visible');
+    cy.get('#tabs-wrapper').scrollTo(0, 0);
+  });
 
-    it('shows the button when scroll more than 200 pixels', () => {
-        cy.get('#tabs-wrapper').scrollTo(0, 300);
-        cy.get('#tab-file-browser #go-back-button').should('be.visible');
-        cy.get('#tabs-wrapper').scrollTo(0, 0);
-    });
+  it('shows the button when scroll more than 200 pixels', () => {
+    cy.get('#tabs-wrapper').scrollTo(0, 300);
+    cy.get('#tab-file-browser #go-back-button').should('be.visible');
+    cy.get('#tabs-wrapper').scrollTo(0, 0);
+  });
 
-    it('button scrolls back to top when clicked', () => {
-        cy.get('#tabs-wrapper').scrollTo(0, 300);
-        cy.get('#tab-file-browser #go-back-button').should('be.visible');
-        cy.get('#tab-file-browser #go-back-button').click();
-        cy.get('#tabs-wrapper').invoke('scrollTop').should('equal', 0);
-    });
+  it('button scrolls back to top when clicked', () => {
+    cy.get('#tabs-wrapper').scrollTo(0, 300);
+    cy.get('#tab-file-browser #go-back-button').should('be.visible');
+    cy.get('#tab-file-browser #go-back-button').click();
+    cy.get('#tabs-wrapper').invoke('scrollTop').should('equal', 0);
+  });
 });
