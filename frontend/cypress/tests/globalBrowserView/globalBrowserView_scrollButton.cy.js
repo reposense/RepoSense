6c4226d79923bf8bc0ef9fee5f88d2bd98c4add7
YuLetian <yu.letian66@gmail.com>
2026-03-30 18:38:50 +0800

    Change selector

--- frontend/cypress/tests/globalBrowserView/globalBrowserView_scrollButton.cy.js
+++ frontend/cypress/tests/globalBrowserView/globalBrowserView_scrollButton.cy.js
@@ -9,20 +9,20 @@ describe('scroll button in global browser view', () => {
 
   it('button is not shown when only scroll 100 pixels', () => {
     cy.get('#tabs-wrapper').scrollTo(0, 100);
-    cy.get('#tab-file-browser .go-back-button').should('not.be.visible');
+    cy.get('#tabs-wrapper .go-back-button').should('not.be.visible');
     cy.get('#tabs-wrapper').scrollTo(0, 0);
   });
 
   it('shows the button when scroll more than 200 pixels', () => {
     cy.get('#tabs-wrapper').scrollTo(0, 300).trigger('scroll');
-    cy.get('#tab-file-browser .go-back-button').should('be.visible');
+    cy.get('#tabs-wrapper .go-back-button').should('be.visible');
     cy.get('#tabs-wrapper').scrollTo(0, 0);
   });
 
   it('button scrolls back to top when clicked', () => {
     cy.get('#tabs-wrapper').scrollTo(0, 300).trigger('scroll');
-    cy.get('#tab-file-browser .go-back-button').should('be.visible');
-    cy.get('#tab-file-browser .go-back-button').click();
+    cy.get('#tabs-wrapper .go-back-button').should('be.visible');
+    cy.get('#tabs-wrapper .go-back-button').click();
     cy.get('#tabs-wrapper').invoke('scrollTop').should('equal', 0);
   });
 });
