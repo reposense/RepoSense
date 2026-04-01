e6b265d610e263a493df6de7c0e49e0f26c8928a
ashmitahaldar <67264881+ashmitahaldar@users.noreply.github.com>
2026-04-01 23:45:25 +0800

    feat: update config wizard other flags handling

--- src/main/java/reposense/parser/ArgsParser.java
+++ src/main/java/reposense/parser/ArgsParser.java
@@ -103,8 +103,10 @@ public class ArgsParser {
             "\"Since Date\" cannot be later than \"Until Date\".";
     private static final String MESSAGE_SINCE_DATE_LATER_THAN_TODAY_DATE =
             "\"Since Date\" must not be later than today's date.";
-    private static final String MESSAGE_CONFIG_WIZARD_INCOMPATIBLE_FLAGS =
-            "--config-wizard cannot be used with any other flags.";
+    private static final String MESSAGE_CONFIG_WIZARD_VIEW_INCOMPATIBLE =
+            "--config-wizard cannot be used with --view.";
+    private static final String MESSAGE_CONFIG_WIZARD_FLAGS_IGNORED =
+            "Other flags are ignored in --config-wizard mode.";
     private static final String MESSAGE_AUTHOR_DEDUP_MODE_WITHOUT_CONFIG =
             "--author-dedup-mode flag is used without --config flag. The flag will be ignored.";
     private static final String MESSAGE_AUTHOR_CONFIG_FILE_NOT_FOUND =
@@ -339,11 +341,14 @@ public class ArgsParser {
         boolean isConfigWizard = results.get(CONFIG_WIZARD_FLAGS[0]);
 
         if (isConfigWizard) {
+            if (reportFolderPath != null && !reportFolderPath.equals(EMPTY_PATH)) {
+                throw new ParseException(MESSAGE_CONFIG_WIZARD_VIEW_INCOMPATIBLE);
+            }
             boolean hasOtherFlags = Arrays.stream(args)
-                    .filter(arg -> arg.startsWith("--"))
+                    .filter(arg -> arg.startsWith("-"))
                     .anyMatch(arg -> !arg.equals(CONFIG_WIZARD_FLAGS[0]));
             if (hasOtherFlags) {
-                throw new ParseException(MESSAGE_CONFIG_WIZARD_INCOMPATIBLE_FLAGS);
+                logger.warning(MESSAGE_CONFIG_WIZARD_FLAGS_IGNORED);
             }
         }
 
