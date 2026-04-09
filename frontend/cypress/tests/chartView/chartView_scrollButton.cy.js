5f5a25a6ec2d9355e72adf9b02786d0e64d927e9
YuLetian <yu.letian66@gmail.com>
2026-04-09 20:44:42 +0800

    Fix

--- frontend/cypress/tests/chartView/chartView_scrollButton.cy.js
+++ frontend/cypress/tests/chartView/chartView_scrollButton.cy.js
@@ -7,14 +7,14 @@ describe('scroll button', () => {
     cy.get('#summary-wrapper .summary-charts').should('exist');
     cy.get('#summary-wrapper').scrollTo(0, 100);
     cy.get('#summary-wrapper .go-back-button').should('not.be.visible');
-    cy.get('#tabs-wrapper').scrollTo(0, 0);
+    cy.get('#summary-wrapper').scrollTo(0, 0);
   });
 
   it('shows the button when scroll more than 200 pixels', () => {
     cy.get('#summary-wrapper .summary-charts').should('exist');
     cy.get('#summary-wrapper').scrollTo(0, 300);
     cy.get('#summary-wrapper .go-back-button').should('be.visible');
-    cy.get('#tabs-wrapper').scrollTo(0, 0);
+    cy.get('#summary-wrapper').scrollTo(0, 0);
   });
 
   it('button scrolls back to top when clicked', () => {
@@ -23,6 +23,6 @@ describe('scroll button', () => {
     cy.get('#summary-wrapper .go-back-button').should('be.visible');
     cy.get('#summary-wrapper .go-back-button').click();
     cy.get('#summary-wrapper').invoke('scrollTop').should('equal', 0);
-    cy.get('#tabs-wrapper').scrollTo(0, 0);
+    cy.get('#summary-wrapper').scrollTo(0, 0);
   });
 });
